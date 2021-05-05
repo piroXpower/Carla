3a078fc092a60dba8f28348adf1bc31860d5b196
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-05 22:45:32 +0530

    Update __init__.py

--- Carla/modules/__init__.py
+++ Carla/modules/__init__.py
@@ -6,40 +6,40 @@ SUDO_USERS = []
 ELITES = []
 
 
-async def can_promote_users(chat_id, user_id):
- perm = await tbot.get_permissions(chat_id, user_id)
+async def can_promote_users(event, user_id):
+ perm = await tbot.get_permissions(event.chat_id, user_id)
  if not perm.is_admin:
   return await event.reply("You need to be an admin to do this.")
  if not perm.add_admins:
   return await event.reply("You are missing the following rights to use this command: CanPromoteUsers.")
  return True
 
-async def cb_can_promote_users(chat_id, user_id):
- perm = await tbot.get_permissions(chat_id, user_id)
+async def cb_can_promote_users(event, user_id):
+ perm = await tbot.get_permissions(event.chat_id, user_id)
  if not perm.is_admin:
   return await event.answer("You need to be an admin to do this.")
  if not perm.add_admins:
   return await event.edit("You are missing the following rights to use this command: CanPromoteUsers.")
  return True
 
-async def can_change_info(chat_id, user_id):
- perm = await tbot.get_permissions(chat_id, user_id)
+async def can_change_info(event, user_id):
+ perm = await tbot.get_permissions(event.chat_id, user_id)
  if not perm.is_admin:
   return await event.reply("You need to be an admin to do this.")
  if not perm.change_info:
   return await event.reply("You are missing the following rights to use this command: CanChangeInfo.")
  return True
 
-async def can_pin_messages(chat_id, user_id):
- perm = await tbot.get_permissions(chat_id, user_id)
+async def can_pin_messages(event, user_id):
+ perm = await tbot.get_permissions(event.chat_id, user_id)
  if not perm.is_admin:
   return await event.reply("You need to be an admin to do this.")
  if not perm.pin_messages:
   return await event.reply("You are missing the following rights to use this command: CanPinMessages.")
  return True
 
-async def can_ban_users(chat_id, user_id):
- perm = await tbot.get_permissions(chat_id, user_id)
+async def can_ban_users(event, user_id):
+ perm = await tbot.get_permissions(event.chat_id, user_id)
  if not perm.is_admin:
   return await event.reply("You need to be an admin to do this.")
  if not perm.ban_users:
