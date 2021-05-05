a9b6165b8a915a50870bebe51dc96a3f35c7a97a
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-05 17:29:31 +0530

    Update __init__.py

--- Carla/modules/__init__.py
+++ Carla/modules/__init__.py
@@ -1,5 +1,5 @@
 from Carla import tbot
-
+from telethon import events, Button
 
 async def can_promote_users(chat_id, user_id):
  perm = await tbot.get_permissions(chat_id, user_id)
@@ -24,3 +24,28 @@ async def can_ban_users(chat_id, user_id):
  if not perm.ban_users:
   return await event.reply("You are missing the following rights to use this command: CanBanUsers.")
  return True
+
+async def get_user(event):
+    args = event.pattern_match.group(1).split(" ", 1)
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        user_obj = await tbot.get_entity(previous_message.sender_id)
+        extra = event.pattern_match.group(1)
+    elif args:
+        extra = ""
+        user = args[0]
+        if len(args) == 2:
+            extra = args[1]
+        if user.isnumeric():
+            user = int(user)
+        if not user:
+            await event.reply("I don't know who you're talking about, you're going to need to specify a user...!")
+            return
+        try:
+            user_obj = await tbot.get_entity(user)
+        except (TypeError, ValueError) as err:
+            await event.reply(str(err))
+            return
+
+    return user_obj, extra
+
