0b75f6f0318f87873468167bbe639a9485cc5148
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-05 22:23:06 +0530

    Create Reporting.py

---
+++ Carla/modules/Reporting.py
@@ -0,0 +1,15 @@
+from Carla import tbot, OWNER_ID
+from . import ELITES
+from Carla.events import Cbot
+
+Ron = """
+Reports are currently enabled in this chat.
+Users can use the /report command, or mention @admin, to tag all admins.
+
+To change this setting, try this command again, with one of the following args: yes/no/on/off
+"""
+Roff = """
+Reports are currently disabled in this chat.
+
+To change this setting, try this command again, with one of the following args: yes/no/on/off
+"""
