669a45debf4b2a4ebcccb012da6c0b8723328ead
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-05 21:03:10 +0530

    Update Pin.py

--- Carla/modules/Pin.py
+++ Carla/modules/Pin.py
@@ -5,6 +5,8 @@ from Carla.events import Cbot
 
 @Cbot(pattern="^/pinned")
 async def _(event):
+ if event.is_private:
+  return #connect
  x = await event.reply("`Getting the pinned message..`")
  try:
      message = await tbot.get_messages(event.chat_id, ids=types.InputMessagePinned())
@@ -17,3 +19,32 @@ async def _(event):
      chat_id = str(event.chat_id)
      chat_id = chat_id.replace('-100', '')
      await x.edit(f"The pinned message in **{event.chat.title}** is [here](http://t.me/c/{chat_id}/{id}).", link_preview=False)
+
+@Cbot(pattern="^/pin ?(.*)")
+async def _(event):
+ virulent = ["silent", "violent", "notify", "loud", "quiet"]
+ if event.text.startswith("?pinned") or event.text.startswith("!pinned") or event.text.startswith("/pinned"):
+  return
+ if event.is_private:
+  return #connect
+ if not event.sender_id == OWNER_ID or event.sender_id in ELITES:
+    await can_pin_messages(event.chat_id, event.sender_id)
+ if not event.reply_to_msg_id:
+   return await event.reply("You need to reply to a message to pin it!")
+ reply_msg = await event.get_reply_message()
+ options = event.pattern_match.group(1)
+ if options and options not in virulent:
+        return await event.reply(f"'{options}' was not recognised as a valid pin option. Please use one of: loud/violent/notify/silent/quiet")
+ is_silent = True
+ if options == 'silent' or options == 'quiet':
+   is_silent = False
+ chat = (str(event.chat_id)).replace('-100', '')
+ text = f"I have pinned [this message](t.me/c/{chat}/{reply_msg.id})."
+ if options == 'notify':
+  text = f"I have pinned [this message](t.me/c/{chat}/{reply_msg.id}). and notified all members."
+ try:
+      await tbot.pin_message(event.chat_id, reply_msg.id, notify=is_silent)
+      if not is_silent:
+       await event.respond(text)
+ except:
+      await msg.reply(f"Unable to pin the message Probably due to too many requests.")
