0402366d321369814dd6524f03840bdf823a608f
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-05 20:38:36 +0530

    Create Pin.py

---
+++ Carla/modules/Pin.py
@@ -0,0 +1,17 @@
+from telethon import Button, events, types
+from Carla import tbot, BOT_ID, OWNER_ID
+from . import is_admin, can_pin_messages, ELITES
+from Carla.events import Cbot
+
+@Cbot(pattern="^/pinned")
+async def _(event):
+ x = await event.reply("`Getting the pinned message..`")
+ try:
+     message = await tbot.get_messages(event.chat_id, ids=types.InputMessagePinned())
+     id = message.id
+ except AttributeError:
+     return await x.edit("There are no pinned messages in this chat.")
+ if event.chat.username:
+     await x.edit(f"The pinned message in **{event.chat.title}** is **[Here]**(http://t.me/{event.chat.username}/{id}).", link_preview=False)
+ else:
+     await x.edit(f"The pinned message in **{event.chat.title}** is [here](http://t.me/c/{id}).", link_preview=False)
