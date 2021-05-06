84ce81e8aa68d53dd5654b77589c6853f0420855
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-06 15:16:23 +0530

    Update Antiflood.py

--- Carla/modules/Antiflood.py
+++ Carla/modules/Antiflood.py
@@ -1,1 +1,19 @@
-#.
+from Carla import tbot, OWNER_ID
+from . import ELITES, can_change_info
+from typing import Optional, List
+import re, time
+from .sql import antiflood_sql as sql
+
+@Cbot(pattern="^/setfloodmode ?(.*)")
+async def _(event):
+ if event.is_private:
+   return #connect
+ if not can_change_info(event, event.sender_id):
+   return
+ options = event.pattern_match.group(1)
+ if not options:
+   return await event.reply("You need to specify an action to take upon flooding. Current modes are: ban/kick/mute/tban/tmute")
+ supported = ["ban", "mute", "kick", "tban [0-9]", "tmute [0-9]"]
+ if not options in supported:
+   return await event.reply(f"Unknown type '{options}'. Please use one of: ban/kick/mute/tban/tmute")
+ 
