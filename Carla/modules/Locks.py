76b1341eb90674467d9fd4ed43ccbe1fa5bf9a42
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-06 10:28:44 +0530

    Update Locks.py

--- Carla/modules/Locks.py
+++ Carla/modules/Locks.py
@@ -1,6 +1,8 @@
 from Carla import tbot
 from telethon import Button, events
-from . import is_admin
+from . import is_admin, can_change_info
+
+supported = ["all", "album", "audio", "bot", "button", "command", "contact", "document", "email", "emojigame", "forward", "forwardbot", "forwardchannel", "game", "gif", "inline", "invitelink", "location", "phone", "photo", "poll", "rtl", "sticker", "text", "url", "video", voicenote", "voice"]
 
 ltext = """
 The available locktypes are:
@@ -44,3 +46,14 @@ async def _(event):
    if not await is_admin(event.chat_id, event.sender_id):
         return await event.reply("You need to be an admin to do this.")
    await event.reply(ltext)
+
+@Cbot(pattern="^/lock ?(.*)")
+async def _(event):
+ if event.is_private:
+   return #connect
+ if not await can_change_info(event, event.sender_id):
+   return
+ args = event.pattern_match.group(1)
+ if not args in supported:
+      return await event.reply(f"Unknown lock types:\n- {args}\nCheck /locktypes!")
+ 
