aa123d65e13b96d81fe8f1ead098ceb1b0806885
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-06 10:37:41 +0530

    Update Locks.py

--- Carla/modules/Locks.py
+++ Carla/modules/Locks.py
@@ -2,7 +2,7 @@ from Carla import tbot
 from telethon import Button, events
 from . import is_admin, can_change_info
 
-supported = ["all", "album", "audio", "bot", "button", "command", "contact", "document", "email", "emojigame", "forward", "forwardbot", "forwardchannel", "game", "gif", "inline", "invitelink", "location", "phone", "photo", "poll", "rtl", "sticker", "text", "url", "video", "voicenote", "voice"]
+supported = ["all", "album", "media", "audio", "bot", "button", "command", "contact", "document", "email", "emojigame", "forward", "forwardbot", "forwardchannel", "game", "gif", "inline", "invitelink", "location", "phone", "photo", "poll", "rtl", "sticker", "text", "url", "video", "voicenote", "voice"]
 
 ltext = """
 The available locktypes are:
@@ -26,6 +26,7 @@ The available locktypes are:
 - inline
 - invitelink
 - location
+- media
 - phone
 - photo
 - poll
@@ -49,6 +50,8 @@ async def _(event):
 
 @Cbot(pattern="^/lock ?(.*)")
 async def _(event):
+ if event.text.startswith("!locktypes") or event.text.startswith("/locktypes") or event.text.startswith("?locktypes"):
+   return
  if event.is_private:
    return #connect
  if not await can_change_info(event, event.sender_id):
@@ -56,4 +59,16 @@ async def _(event):
  args = event.pattern_match.group(1)
  if not args in supported:
       return await event.reply(f"Unknown lock types:\n- {args}\nCheck /locktypes!")
- 
+ chat_rights = ["text", "gif", "sticker", "game", "inline", "media", "rtl"]
+ if args in chat_rights:
+   await chat_event(event, args)
+ else:
+   await db_lock(event, args)
+
+async def chat_event(event, args)
+ await event.reply("Chat Event")
+
+
+
+async def db_lock(event, args)
+ await event.reply("Db Event")
