d6126bb9a0aa506cbf63ae9928584fa9a4f4109c
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-07 20:45:57 +0530

    Create Misc.py

---
+++ Carla/modules/Misc.py
@@ -0,0 +1,20 @@
+from Carla import tbot
+from Carla.events import Cbot
+import requests, os
+
+@Cbot(pattern="^/sshot ?(.*)")
+async def _(event):
+ BASE = 'https://render-tron.appspot.com/screenshot/'
+ url = event.pattern_match.group(1)
+ if not url:
+   return await event.reply('Please provide the URL.')
+ path = 'target.jpg'
+ response = requests.get(BASE + url, stream=True)
+ if response.status_code == 200:
+    with open(path, 'wb') as file:
+        for chunk in response:
+            file.write(chunk)
+ else:
+   return await event.respond('Invalid URL Provided.')
+ await tbot.send_file(event.chat_id, path)
+ os.remove('target.jpg')
