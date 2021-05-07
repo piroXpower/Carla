155e5bb95dc83e75ac0501a0be0546d06744736e
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-07 12:46:17 +0530

    Update Blocklists.py

--- Carla/modules/Blocklists.py
+++ Carla/modules/Blocklists.py
@@ -20,5 +20,35 @@ async def _(event):
  text = "Added blocklist filter '{}'!".format(trigger)
  await event.respond(text)
  sql.add_to_blacklist(event.chat_id, trigger)
- trigger = trigger.split(" ", 1)
- await event.respond(trigger[0] + "/n" + trigger[1])
+
+@Cbot(pattern="^/addblacklist ?(.*)")
+async def _(event):
+ if event.is_private:
+     return #connect
+ if not await can_change_info(event, event.sender_id):
+     return
+ if event.reply_to_msg_id:
+     msg = await event.get_reply_message()
+     trigger = msg.message
+ elif event.pattern_match.group(1):
+     trigger = event.pattern_match.group(1)
+ else:
+     return await event.reply("You need to provide a blocklist trigger!\neg: `/addblocklist the admins suck`.")
+ text = "Added blocklist filter '{}'!".format(trigger)
+ await event.respond(text)
+ sql.add_to_blacklist(event.chat_id, trigger)
+ 
+@Cbot(pattern="^/blocklist")
+async def _(event):
+ if event.is_private:
+     return #connect
+ if not await is_admin(event.chat_id, event.sender_id):
+     return await event.reply("You need to be an admin to do this.")
+ all_blacklisted = sql.get_chat_blacklist(event.chat_id)
+ if len(all_blacklisted) == 0:
+    text = 'No blocklist filters active in {}!'.format(event.chat.title)
+ else:
+    text = 'The following blocklist filters are currently active in {}:'.format(event.chat.title)
+    for i in all_blacklisted:
+          text += f"\n- `{trigger}`"
+ await event.reply(text)
