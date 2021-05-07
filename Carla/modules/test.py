dabb7266ea16c7ab73e4db2160b906ad7919c644
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-07 19:37:17 +0530

    Update test.py

--- Carla/modules/test.py
+++ Carla/modules/test.py
@@ -15,4 +15,4 @@ async def h(e: events.InlineQuery.Event):
         sup = []
         for i in s:
          sup.append(await e.builder.document(i, type='sticker'))
-        await e.answer(sup)
+        await e.answer(sup, gallery=True)
