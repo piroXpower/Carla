08ae383670e89903f8c0b1c472b06c6e83c64a31
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-07 18:40:26 +0530

    Update test.py

--- Carla/modules/test.py
+++ Carla/modules/test.py
@@ -1,10 +1,17 @@
 from Carla import tbot
 from telethon import events
-sticker = 'CAADBQAD0gEAAjAnSjiO_2DuZwa5tgI'
+from Carla.events import Cbot
+s = None
+@Cbot(pattern="^/suz")
+async def _(event):
+ global s
+ k = await tbot.get_reply_message()
+ s = k
 
 @tbot.on(events.InlineQuery)
 async def h(e: events.InlineQuery.Event):
+        global s
         builder = e.builder
         await e.answer(results=[
-            builder.document(sticker, type='sticker')
+            builder.document(s, type='sticker')
         ])
