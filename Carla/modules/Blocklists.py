7a73979e2b8e6d97a8480dbe2518c482a979a4fe
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-07 17:47:34 +0530

    Update Blocklists.py

--- Carla/modules/Blocklists.py
+++ Carla/modules/Blocklists.py
@@ -176,3 +176,15 @@ async def on_new_message(event):
            elif mode == 'ban':
                  await tbot.edit_permissions(event.chat_id, event.sender_id, until_date=None, view_messages=False)
                  await event.respond('banned')
+           elif mode == 'kick':
+                 await tbot.kick_participant(event.chat_id, event.sender_id)
+           elif mode == 'mute':
+                 await tbot.edit_permissions(event.chat_id, event.sender_id, until_date=None, send_messages=False)
+           elif mode == 'tban':
+                 tt = sql.get_time(event.chat_id)
+                 await tbot.edit_permissions(event.chat_id, event.sender_id, until_date=time.time() + int(tt), view_messages=False)
+           elif mode == 'tmute':
+                 tt = sql.get_time(event.chat_id)
+                 await tbot.edit_permissions(event.chat_id, event.sender_id, until_date=time.time() + int(tt), send_messages=False)
+           elif mode == 'warn':
+                 print(99)
