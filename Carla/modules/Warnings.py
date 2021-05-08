f5cf79c9a0d4f3faa91bdf9439c8023ac72d912d
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-08 11:08:18 +0530

    Update Warnings.py

--- Carla/modules/Warnings.py
+++ Carla/modules/Warnings.py
@@ -14,4 +14,10 @@ async def _(event):
  if not args:
   settings = sql.get_limit(event.chat_id)
   await event.reply(f"Current warn limit is {settings}\n\nTo change this send the command with the new limit.")
- 
+ elif args.is_digit():
+  if int(args) > 20:
+   return await event.reply("Max limit is 20.\nTry lowering the limit.")
+  k = sql.set_warn_limit(event.chat_id, args)
+  await event.reply(f"Sucessfully updated warn limit to {args}")
+ else:
+  await event.reply(f"Expected an integer, got '{args}'.")
