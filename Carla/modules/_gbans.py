1489b9247a66bca92e5b54b96a9c558183e70464
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-08 12:52:55 +0530

    Update _gbans.py

--- Carla/modules/_gbans.py
+++ Carla/modules/_gbans.py
@@ -1,6 +1,7 @@
 from Carla import tbot, OWNER_ID
 from . import ELITES, SUDO_USERS, is_admin, get_user, db
 from Carla.events import Cbot
+from telethon import Button
 
 gbanned = db.gbanned
 def get_reason(id):
