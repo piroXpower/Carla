7480625759830d1bf92bf4b361a944695b421ff9
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-08 12:21:10 +0530

    Update _gbans.py

--- Carla/modules/_gbans.py
+++ Carla/modules/_gbans.py
@@ -5,7 +5,11 @@ from Carla.events import Cbot
 a = """
 __Your request sent to DEVS waiting for approval. Till that send proofs to DEVS.__
 """
+b = """
+You don't seem to be referring to a user or the ID specified is incorrect..
+"""
 
 @Cbot(pattern="^/gban ?(.*)")
 async def _(event):
- await event.reply(a)
+ if not event.reply_to_msg_id and not event.pattern_match.group(1):
+   return await event.reply(b)
