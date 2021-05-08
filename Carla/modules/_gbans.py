9b4724a858b5674d838f32aacdcd30e9fc41c22a
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-08 12:43:32 +0530

    Update _gbans.py

--- Carla/modules/_gbans.py
+++ Carla/modules/_gbans.py
@@ -27,7 +27,7 @@ Fool! You can't ban my master. noob!😑
 
 @Cbot(pattern="^/gban ?(.*)")
 async def _(event):
- if not event.sender_id == OWNER_ID or not event.sender_id in ELITES or not event.sender_id in SUDO_USERS:
+ if not event.sender_id == OWNER_ID and not event.sender_id in ELITES and not event.sender_id in SUDO_USERS:
    return
  if not event.reply_to_msg_id and not event.pattern_match.group(1):
    return await event.reply(b)
