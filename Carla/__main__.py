e90a621ce9ec75c7cf46ce74a2d6eca0c846e7c1
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-09 17:36:03 +0530

    Update __main__.py

--- Carla/__main__.py
+++ Carla/__main__.py
@@ -11,7 +11,7 @@ except Exception:
     exit(1)
 
 async def start_log():
- await tbot.send_message(-1001326741686, "**Bot Started!**")
+ await tbot.send_message(-1001273171524, "**Bot Started!**")
 
 tbot.loop.run_until_complete(start_log())
 tbot.run_until_disconnected()
