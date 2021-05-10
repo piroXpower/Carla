61b78812a9d887a6c67ad0c0f5acf5453f806104
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-10 09:49:34 +0530

    Update Misc.py

--- Carla/modules/Misc.py
+++ Carla/modules/Misc.py
@@ -43,37 +43,13 @@ async def _(event):
    user, extra = await get_user(event)
   except TypeError:
    pass
- user_id = user.id
- first_name = user.first_name
- last_name = user.last_name
- username = user.username
- text = "<b>User Information:</b>\n"
- text += f"<b>ID:</b> <code>{user_id}</code>\n"
- if first_name:
-   text += f"<b>First Name:</b> {first_name}\n"
- if last_name:
-   text += f"<b>Last Name:</b> {last_name}\n"
- if username:
-   text += f"<b>Username:</b> @{username}\n"
- text += f'<b>User link:</b> <a href="tg://user?id={user_id}">{first_name}</a>'
- await event.reply(text, parse_mode="html")
-
-@Cbot(pattern="^/whois ?(.*)")
-async def _(event):
- if not event.reply_to_msg_id and not event.pattern_match.group(1):
-   user = await tbot.get_entity(event.sender_id)
- else:
-  try:
-   user, extra = await get_user(event)
-  except TypeError:
-   pass
+ photo = None
  try:
-  o_user = tbot(GetFullUserRequest(user.username))
- except:
-  o_user = tbot(GetFullUserRequest(user.id))
- photo = await event.client.download_profile_photo(
+  photo = await event.client.download_profile_photo(
         user.id, './' + str(user.id), download_big=True
     )
+ except:
+  pass
  user_id = user.id
  first_name = user.first_name
  last_name = user.last_name
@@ -87,5 +63,5 @@ async def _(event):
  if username:
    text += f"<b>Username:</b> @{username}\n"
  text += f'<b>User link:</b> <a href="tg://user?id={user_id}">{first_name}</a>'
- await event.respond(text, file=photo, parse_mode='html', file_name="RoseLoverX")
- os.remove('./' + str(user.id) + ".jpg")
+ await event.reply(text, file=photo, parse_mode="html", force_document=True)
+
