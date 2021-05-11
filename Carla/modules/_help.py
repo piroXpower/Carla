9759cdf062cb9bba68e4e8e7bc0b7cdbfd45ba26
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-11 22:03:44 +0530

    Update _help.py

--- Carla/modules/_help.py
+++ Carla/modules/_help.py
@@ -6,13 +6,13 @@ from telethon import Button
 string = "Contact me in PM for help!"
 
 c_button = Button.inline("Back", data="go_back")
-bl_button = Button.inline('Block
+bl_button = Button.inline('Blocklist Command Examples', data='bl_cmd')
 welcome = """
-Greetings
+**Greetings**
 
 Give your members a warm welcome with the greetings module! Or a sad goodbye... Depends!
 
-Admin commands:
+**Admin commands:**
 - /welcome <yes/no/on/off>: Enable/disable welcomes messages.
 - /goodbye <yes/no/on/off>: Enable/disable goodbye messages.
 - /setwelcome <text>: Set a new welcome message. Supports markdown, buttons, and fillings.
@@ -22,13 +22,13 @@ Admin commands:
 - /cleanservice <yes/no/on/off>: Delete all service messages. Those are the annoying 'x joined the group' notifications you see when people join.
 - /cleanwelcome <yes/no/on/off>: Delete old welcome messages. When a new person joins, or after 5 minutes, the previous message will get deleted.
 
-Examples:
+**Examples:**
 - Get the welcome message without any formatting
 -> /welcome noformat
 """
 wlc_btn = [Button.inline("CAPTCHA", data='captcha'), Button.inline('Formatting', data='formatting')], [Button.inline('Back', data="go_back")]
 antiflood = """
-Antiflood
+**Antiflood**
 
 You know how sometimes, people join, send 100 messages, and ruin your chat? With antiflood, that happens no more!
 
@@ -39,7 +39,27 @@ Admin commands:
 - /setflood <number/off/no>: Set the number of messages after which to take action on a user. Set to '0', 'off', or 'no' to disable.
 - /setfloodmode <action type>: Choose which action to take on a user who has been flooding. Options: ban/kick/mute/tban/tmute
 """
+blocklist = """
+**Blocklists**
 
+Want to stop people asking stupid questions? or ban anyone saying censored words? Blocklists is the module for you!
+
+From blocking rude words, filenames/extensions, to specific emoji, everything is possible.
+
+Admin commands:
+- /addblocklist <blocklist trigger> <reason>: Add a blocklist trigger. You can blocklist an entire sentence by putting it in "quotes".
+- /rmblocklist <blocklist trigger>: Remove a blocklist trigger.
+- /unblocklistall: Remove all blocklist triggers - chat creator only.
+- /blocklist: List all blocklisted items.
+- /blocklistmode <blocklist mode>: Set the desired action to take when someone says a blocklisted item. Available: nothing/ban/mute/kick/warn/tban/tmute.
+- /blocklistdelete <yes/no/on/off>: Set whether blocklisted messages should be deleted. Default: (on)
+- /setblocklistreason <reason>: Set the default blocklist reason to warn people with.
+- /resetblocklistreason: Reset the default blocklist reason to default - nothing.
+
+Top tip:
+Blocklists allow you to use some modifiers to match "unknown" characters. For example, you can use the ? character to match a single occurrence of any non-whitespace character.
+You could also use the * modifier, which matches any number of any character. If you want to blocklist urls, this will allow you to match the full thing. It matches every character except spaces. This is cool if you want to block, for example, url shorteners.
+"""
 
 @Cbot(pattern="^/help ?(.*)")
 async def help(event):
@@ -59,4 +79,4 @@ async def hh(event):
  elif plugin_name == 'antiflood':
    await event.reply(antiflood, buttons=c_button)
  elif plugin_name == 'blocklist' or plugin_name == 'blacklist' or plugin_name == 'blocklists' or plugin_name == 'blacklists':
-   await event.reply(blocklist, buttons=c_button)
+   await event.reply(blocklist, buttons=bl_button)
