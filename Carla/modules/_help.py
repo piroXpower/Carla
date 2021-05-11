ee82941f367c2778dc9d20dc86bd6a186890ad59
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-11 22:13:29 +0530

    Update _help.py

--- Carla/modules/_help.py
+++ Carla/modules/_help.py
@@ -5,6 +5,7 @@ from telethon import Button
 
 string = "Contact me in PM for help!"
 
+f_button = [Button.inline("Fed Admin", data="f_ad"), Button.inline("Fed Owner", data="f_ow")], [Button.inline("Fed User", data="f_us")], [Button.inline("Back", data="go_back")]
 c_button = Button.inline("Back", data="go_back")
 bl_button = [Button.inline('Blocklist Command Examples', data='bl_cmd')], [Button.inline("Back", data="go_back")]
 welcome = """
@@ -60,6 +61,40 @@ Top tip:
 Blocklists allow you to use some modifiers to match "unknown" characters. For example, you can use the ? character to match a single occurrence of any non-whitespace character.
 You could also use the * modifier, which matches any number of any character. If you want to blocklist urls, this will allow you to match the full thing. It matches every character except spaces. This is cool if you want to block, for example, url shorteners.
 """
+locks = """
+**Locks**
+
+Do stickers annoy you? or want to avoid people sharing links? or pictures? You're in the right place!
+
+The locks module allows you to lock away some common items in the telegram world; the bot will automatically delete them!
+
+**Admin commands:**
+- /lock <item(s)>: Lock one or more items. Now, only admins can use this type!
+- /unlock <item(s)>: Unlock one or more items. Everyone can use this type again!
+- /locks: List currently locked items.
+- /lockwarns <yes/no/on/off>: Enabled or disable whether a user should be warned when using a locked item.
+- /locktypes: Show the list of all lockable items.
+- /allowlist <url/id/@channelname(s)>: Allowlist a URL, group ID, channel @, or bot @ to stop them being deleted by URL, forward, invitelink, and inline locks. Separate with a space to add multiple items at once. If no arguments are given, returns the current allowlist.
+- /rmallowlist <url/id/@channelname(s)>: Remove an item from the allowlist - url, invitelink, and forward locking will now take effect on messages containing it again. Separate with a space to remove multiple items.
+- /rmallowlistall: Remove all allowisted items.
+
+**Examples:**
+- Lock stickers with:
+-> /lock sticker
+- You can lock/unlock multiple items by chaining them:
+-> /lock sticker photo gif video
+- To allow forwards from a specific channel, eg @CarlaSupport, you can allowlist it. You can also use the ID, or invitelink:
+-> /allowlist @CarlaSupport
+"""
+fedz = """
+**Federations**
+
+Ah, group management. It's all fun and games, until you start getting spammers in, and you need to ban them. Then you need to start banning more, and more, and it gets painful.
+But then you have multiple groups, and you don't want these spammers in any of your groups - how can you deal? Do you have to ban them manually, in all your groups?
+
+No more! With federations, you can make a ban in one chat overlap to all your other chats.
+You can even appoint federation admins, so that your trustworthiest admins can ban across all the chats that you want to protect.
+"""
 
 @Cbot(pattern="^/help ?(.*)")
 async def help(event):
@@ -76,7 +111,11 @@ async def hh(event):
  plugin_name = (event.pattern_match.group(1)).lower()
  if plugin_name == 'welcome' or plugin_name == 'greetings':
    await event.reply(welcome, buttons=wlc_btn)
- elif plugin_name == 'antiflood':
+ elif plugin_name == 'antiflood' or plugin_name == 'antispam':
    await event.reply(antiflood, buttons=c_button)
  elif plugin_name == 'blocklist' or plugin_name == 'blacklist' or plugin_name == 'blocklists' or plugin_name == 'blacklists':
    await event.reply(blocklist, buttons=bl_button)
+ elif plugin_name == 'lock' or plugin_name == 'locks':
+   await event.reply(locks, buttons=c_button)
+ elif plugin_name in ['fed', 'feds', 'federation', 'federations']:
+   await event.reply(fedz, buttons=f_button)
