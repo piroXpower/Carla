605a8846005fdd7b7ffe75ca6d375b0f1cf3bd1f
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-13 22:57:37 +0530

    Create Extras.py

---
+++ Carla/modules/Extras.py
@@ -0,0 +1,8 @@
+from Carla.modules.sql.nightmode_sql import add_nightmode, rmnightmode, get_all_chat_id, is_nightmode_indb
+from Carla import tbot
+from Carla.events import Cbot
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+
+enable = ['enable', 'on', 'y', 'yes']
+disable = ['disable', 'off', 'n' 'no']
+
