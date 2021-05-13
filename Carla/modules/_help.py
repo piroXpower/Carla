435e6a238f8d6a072cedb13d6c2803fb95e54be2
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-13 10:00:37 +0530

    Update _help.py

--- Carla/modules/_help.py
+++ Carla/modules/_help.py
@@ -161,6 +161,24 @@ That's what approvals are for - approve of trustworthy users to allow them to se
 - /approved: List all approved users.
 - /unapproveall: Unapprove ALL users in a chat. This cannot be undone.
 """
+note = """
+**Notes**
+
+Save data for future users with notes!
+
+Notes are great to save random tidbits of information; a phone number, a nice gif, a funny picture - anything!
+
+**User commands:**
+- /get `<notename>`: Get a note.
+- #notename: Same as /get.
+
+**Admin commands:**
+- /save `<notename> <note text>`: Save a new note called "word". Replying to a message will save that message. Even works on media!
+- /clear `<notename>`: Delete the associated note.
+- /notes: List all notes in the current chat.
+- /saved: Same as /notes.
+- /clearall: Delete ALL notes in a chat. This cannot be undone.
+- /privatenotes: Whether or not to send notes in PM. Will send a message with a button which users can click to get the note in PM.
 
 @Cbot(pattern="^/help ?(.*)")
 async def help(event):
@@ -193,3 +211,5 @@ async def hh(event):
    await event.reply(pin, buttons=c_button)
  elif plugin_name in ['approve', 'approval']:
    await event.reply(approve, buttons=c_button)
+ elif plugin_name in ['note', 'notes']:
+   await event.reply(note, buttons=c_button)
