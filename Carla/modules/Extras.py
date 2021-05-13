2d67eb75c105f02fff97d3de419620e8d4f13a69
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-13 23:03:51 +0530

    Update Extras.py

--- Carla/modules/Extras.py
+++ Carla/modules/Extras.py
@@ -1,8 +1,31 @@
 from Carla.modules.sql.nightmode_sql import add_nightmode, rmnightmode, get_all_chat_id, is_nightmode_indb
 from Carla import tbot
 from Carla.events import Cbot
+from . import can_change_info
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+import pytz
+from datetime import datetime
 
 enable = ['enable', 'on', 'y', 'yes']
 disable = ['disable', 'off', 'n' 'no']
 
+@Cbot(pattern="^/nightmode ?(.*)")
+async def lilz(event):
+ if event.is_private:
+    return
+ if not await can_change_info(event, event.sender_id):
+    return
+ args = event.pattern_match.group(1)
+ if not args:
+   if is_nightmode_indb(event.chat_id):
+      await event.reply("**NightMode** is currently **enabled** for this chat.")
+   else:
+      await event.reply("**NightMode** is currently **disabled** for this chat.")
+ elif args in enable:
+      await event.reply("Enabled nightmode for this.\n\nGroup closes at 12Am and opens at 6Am IST")
+      add_nightmode(event.chat_id)
+ elif args in disable:
+      await event.reply("Disabled nightmode for this chat.")
+      rmnightmode(event.chat_id)
+
+#__soon__
