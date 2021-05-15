225bae3693805744c9b250bb9627a8d11f5fe070
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-15 16:55:29 +0530

    Update Bans.py

--- Carla/modules/Bans.py
+++ Carla/modules/Bans.py
@@ -1,6 +1,6 @@
 from Carla import tbot
 from Carla.events import Cbot
-from . import can_ban_users
+from . import can_ban_users, g_time
 
 
 async def excecute_operation(event, user_id, name, mode, reason="", tt=""):
@@ -20,10 +20,14 @@ async def excecute_operation(event, user_id, name, mode, reason="", tt=""):
                      reason = f"\nReason: <code>{reason}</code>"
                  await event.respond(f'<b>Muted <a href="tg://user?id={user_id}">{name}</a></b>!{reason}', parse_mode='html')
            elif mode == 'tban':
-                 tt = sql.get_time(event.chat_id)
+                 if reason:
+                     reason = f"\nReason: <code>{reason}</code>"
+                 time = g_time(tt)
+                 await event.respond(f'<b>Banned <a href="tg://user?id={user_id}">{name}</a></b> for {time}!{reason}', parse_mode='html')
                  await tbot.edit_permissions(event.chat_id, event.sender_id, until_date=time.time() + int(tt), view_messages=False)
            elif mode == 'tmute':
                  if reason:
                      reason = f"\nReason: <code>{reason}</code>"
-                 await event.respond(f'<b>Muted <a href="tg://user?id={user_id}">{name}</a></b> for !{reason}', parse_mode='html')
+                 time = g_time(tt)
+                 await event.respond(f'<b>Muted <a href="tg://user?id={user_id}">{name}</a></b> for {time}!{reason}', parse_mode='html')
                  await tbot.edit_permissions(event.chat_id, event.sender_id, until_date=time.time() + int(tt), send_messages=False)
