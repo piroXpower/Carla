e4e4685cb4506044976623efe08566229a77e204
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-15 16:45:34 +0530

    Update Bans.py

--- Carla/modules/Bans.py
+++ Carla/modules/Bans.py
@@ -2,13 +2,14 @@ from Carla import tbot
 from Carla.events import Cbot
 from . import can_ban_users
 
-
+text += f'\nChecked by <b><a href="tg://user?id={event.sender_id}">{event.sender.first_name}</a></b>'
+ await 
 async def excecute_operation(event, user_id, name, mode, reason=""):
            if mode == 'ban':
                  await tbot.edit_permissions(event.chat_id, user_id, until_date=None, view_messages=False)
                  if reason:
                      reason = f"\n<b>Reason:</b> <code>{reason}</code>"
-                 await event.respond(f"<b>Banned {name}</b>{reason}")
+                 await event.respond(f'<b>Banned <a href="tg://user?id={user_id}">{name}</a></b>{reason}', parse_mode='html')
            elif mode == 'kick':
                  await tbot.kick_participant(event.chat_id, event.sender_id)
            elif mode == 'mute':
