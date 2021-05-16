05baa01bc1ba0e687ec176ff0237bef114a275c3
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-16 15:24:57 +0530

    Update Extras.py
    Oo

--- Carla/modules/Extras.py
+++ Carla/modules/Extras.py
@@ -302,7 +302,6 @@ async def gps(event):
       latitude = geoloc.latitude
       gm = "https://www.google.com/maps/search/{},{}".format(
             latitude, longitude)
-      await tbot.send_file(event.chat_id, file=types.InputMediaGeoPoint(types.InputGeoPoint(float(latitude), float(longitude))), caption="hi")
+      await tbot.send_file(event.chat_id, file=InputMediaGeoPoint(InputGeoPoint(float(latitude), float(longitude))), caption="hi")
   except Exception as e:
-        print(e)
-        await event.reply("Unable to locate that place.")
+        await event.reply("Unable to locate that place. " + str(e))
