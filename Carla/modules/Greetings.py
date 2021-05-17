c4022a33314c2091a629909837b08d54b6e10912
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-17 19:08:51 +0530

    Update Greetings.py

--- Carla/modules/Greetings.py
+++ Carla/modules/Greetings.py
@@ -1,5 +1,5 @@
 from Carla import tbot, OWNER_ID
-from . import can_change_info, ELITES
+from . import can_change_info, ELITES, button_parser
 import re
 from Carla.events import Cbot
 import Carla.modules.sql.welcome_sql as sql
@@ -70,4 +70,29 @@ async def _(event):
 async def ca(event):
  if not event.user_joined:
     return
- 
+ if not sql.is_chat(event.chat_id):
+    return
+ if event.user_id in ELITES:
+    return await event.reply("An ELITE level disaster just joined. beware.")
+ elif event.user_id == OWNER_ID:
+    return await event.reply("Wow, my owner just joined!")
+ cws = get_current_welcome_settings(event.chat_id)
+ if not cws:
+    welcome_text = f"Hey **{event.user.first_name}**, How are you."
+    buttons = []
+    file = None
+ else:
+    welcome_text, buttons = button_parser(cws.custom_welcome_message)
+    first_name = event.user.first_name
+    last_name = event.user.last_name
+    full_name = first_name + " " + last_name
+    user_id = event.user_id
+    chat_title = event.chat.title
+    chat_id = event.chat_id
+    chat_username = event.chat.username
+    username = event.user.username
+    mention = f"[{first_name}](tg://user?id={user_id})"
+    welcome_text = welcome_text.format(mention=mention, first_name=first_name, last_name=last_name, username=username, chat_id=chat_id, chat_username=chat_username, full_name=full_name)
+    file = None
+ await event.reply(welcome_text, buttons=buttons, file=file)
+
