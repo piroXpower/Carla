fcfef55f8f8507f58c8d71db381fa6a8edc8f223
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-17 16:42:18 +0530

    Update btn.py

--- Carla/modules/btn.py
+++ Carla/modules/btn.py
@@ -36,11 +36,129 @@ def split_quotes(text: str) -> List:
     else:
         return text.split(None, 1)
 
+def parser(text, keyword):
+    if "buttonalert" in text:
+        text = text.replace("\n", "\\n").replace("\t", "\\t")
+    buttons = []
+    note_data = ""
+    prev = 0
+    i = 0
+    alerts = []
+    for match in BTN_URL_REGEX.finditer(text):
+        # Check if btnurl is escaped
+        n_escapes = 0
+        to_check = match.start(1) - 1
+        while to_check > 0 and text[to_check] == "\\":
+            n_escapes += 1
+            to_check -= 1
+
+        # if even, not escaped -> create button
+        if n_escapes % 2 == 0:
+            note_data += text[prev : match.start(1)]
+            prev = match.end(1)
+            if match.group(3) == "buttonalert":
+                # create a thruple with button label, url, and newline status
+                if bool(match.group(5)) and buttons:
+                    buttons[-1].append(
+                        Button.inline(
+                            match.group(2),
+                            data=f"alertmessage:{i}:{keyword}",
+                        )
+                    )
+                else:
+                    buttons.append(
+                        [
+                            Button.inline(
+                                match.group(2),
+                                data=f"alertmessage:{i}:{keyword}",
+                            )
+                        ]
+                    )
+                i = i + 1
+                alerts.append(match.group(4))
+            else:
+                if bool(match.group(5)) and buttons:
+                    buttons[-1].append(
+                        Button.url(
+                            match.group(2), match.group(4).replace(" ", "")
+                        )
+                    )
+                else:
+                    buttons.append(
+                        [
+                            Button.url(
+                                match.group(2), match.group(4).replace(" ", "")
+                            )
+                        ]
+                    )
+
+        # if odd, escaped -> move along
+        else:
+            note_data += text[prev:to_check]
+            prev = match.start(1) - 1
+    else:
+        note_data += text[prev:]
+
+    try:
+        return note_data, buttons, alerts
+    except:
+        return note_data, buttons, None
+BUTTONS = {}
+
+
+def get_reply_msg_btns_text(message):
+    text = ""
+    for column in message.reply_markup.rows:
+        btn_num = 0
+        for btn in column.buttons:
+            btn_num += 1
+            name = btn.text
+
+            if btn.url:
+                url = btn.url
+                if "?start=" in url:
+                    raw_btn = url.split("?start=")[1]
+                    text += parse_button(raw_btn, name)
+                else:
+                    text += f"\n[{btn.text}](btnurl:{btn.url}*!repl!*)"
+            elif btn.data:
+                text += parse_button(btn.data, name)
+
+            if btn_num > 1:
+                text = text.replace("*!repl!*", ":same")
+            else:
+                text = text.replace("*!repl!*", "")
+    return text
+
+def parse_button(data, name):
+    raw_button = data.split("_")
+    raw_btn_type = raw_button[0]
+
+    pattern = re.match(r"btn(.+)(sm|cb|start)", raw_btn_type)
+    if not pattern:
+        return ""
+
+    action = pattern.group(1)
+    args = raw_button[1]
+
+    if action in BUTTONS:
+        text = f"\n[{name}](btn{action}:{args}*!repl!*)"
+    else:
+        if args:
+            text = f"\n[{name}].(btn{action}:{args})"
+        else:
+            text = f"\n[{name}].(btn{action})"
+
+    return text
+
 @Cbot(pattern="^/btn ?(.*)")
 async def tt(event):
  text = event.pattern_match.group(1)
- txt = split_quotes(text)
- await event.respond(str(txt))
+ try:
+  brb = get_reply_msg_btns_text((await event.get_reply_message())
+ except Exception as e:
+  return await event.respond(str(e))
+ await event.respond(str(brb))
 
 def remove_escapes(text: str) -> str:
     counter = 0
