4d9a7c7ac14fbda8798b3b02646d46881fc8dede
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-19 21:45:44 +0530

    Update CAPTCHA.py

--- Elsie/modules/CAPTCHA.py
+++ Elsie/modules/CAPTCHA.py
@@ -249,9 +249,14 @@ async def _(event):
 async def captcha_to_welcome(event, text, file):
     style = sql.get_style(event.chat_id)
     await tbot.edit_permissions(event.chat_id, event.user_id, send_messages=False)
-    text = (
+    if style in ["math", "text"]:
+     text = (
         text
         + f" [Click here to prove human](btnurl://t.me/MissElsie_Bot?start=captcha_{event.chat_id}&{style})"
     )
-    welcome_text, buttons = button_parser(text)
+     welcome_text, buttons = button_parser(text)
+    else:
+     welcome_text, buttons = button_parser(text)
+     buttons.append([Button.url("Click to prove human", data="humanv_{}&{}".format(event.chat_id, event.user_id))])
     await event.reply(welcome_text, file=file, buttons=buttons)
+
