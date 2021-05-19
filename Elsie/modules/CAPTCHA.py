1cc556008c93ce04be7aa5e5fa34c5d34e30e893
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-05-19 15:40:06 +0000

    RoseLoverX: auto fixes

--- Elsie/modules/CAPTCHA.py
+++ Elsie/modules/CAPTCHA.py
@@ -247,11 +247,11 @@ async def _(event):
 
 
 async def captcha_to_welcome(event, welcome_text, file, buttons):
- style = sql.get_style(event.chat_id)
- await tbot.edit_permissions(event.chat_id, event.user_id, send_messages=False)
- if style == 'math':
-    await math_captcha(event, welcome_text, file, buttons)
+    style = sql.get_style(event.chat_id)
+    await tbot.edit_permissions(event.chat_id, event.user_id, send_messages=False)
+    if style == "math":
+        await math_captcha(event, welcome_text, file, buttons)
+
 
 async def math_captcha(event, welcome_text, file, buttons):
- await event.respond(str(buttons))
-    
+    await event.respond(str(buttons))
