b1c8f203f157e18f7b4293e3ee9be816dab76a0e
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-19 13:42:29 +0530

    Update Warnings.py

--- Elsie/modules/Warnings.py
+++ Elsie/modules/Warnings.py
@@ -84,36 +84,40 @@ async def warn_user(event):
     buttons = [Button.inline("Remove warn", data=f"rm_warn-{user.id}")]
     await event.respond(text, buttons=buttons, parse_mode='html')
  else:
-    print(6)
+    tt = 0
+    mode = sql.get_warn_strength(event.chat_id)
+    if mode in ['tban', 'tmute']:
+       tt = sql.get_ban_time(event.chat_id)
+    await excecute_warn(event, user.id, user.first_name, mode, reason, tt, limit)
 
 
-async def excecute_warn(event, user_id, name, mode, reason="", tt=0):
+async def excecute_warn(event, user_id, name, mode, reason="", tt=0, limit=3):
            if mode == 'ban':
                  await tbot.edit_permissions(event.chat_id, user_id, until_date=None, view_messages=False)
                  if reason:
-                     reason = f"\nReason: <code>{reason}</code>"
-                 await event.respond(f'<b>Banned <a href="tg://user?id={user_id}">{name}</a></b>!{reason}', parse_mode='html')
+                     reason = f"\nReason: <i>{reason}</i>"
+                 await event.respond(f'Thats {limit}/{limit} Warnings, <a href="tg://user?id={user_id}">{name}</a> Has been Banned.{reason}', parse_mode='html')
            elif mode == 'kick':
                  await tbot.kick_participant(event.chat_id, event.sender_id)
                  if reason:
-                     reason = f"\nReason: <code>{reason}</code>"
-                 await event.respond(f'<b>Kicked <a href="tg://user?id={user_id}">{name}</a></b>!{reason}', parse_mode='html')
+                     reason = f"\nReason: <i>{reason}</i>"
+                 await event.respond(f'Thats {limit}/{limit} Warnings, <a href="tg://user?id={user_id}">{name}</a> has been Kicked!{reason}', parse_mode='html')
            elif mode == 'mute':
                  await tbot.edit_permissions(event.chat_id, event.sender_id, until_date=None, send_messages=False)
                  if reason:
-                     reason = f"\nReason: <code>{reason}</code>"
-                 await event.respond(f'<b>Muted <a href="tg://user?id={user_id}">{name}</a></b>!{reason}', parse_mode='html')
+                     reason = f"\nReason: <i>{reason}</i>"
+                 await event.respond(f'Thats {limit}/{limit} Warnings, <a href="tg://user?id={user_id}">{name}</a> has been Muted!{reason}', parse_mode='html')
            elif mode == 'tban':
                  if reason:
-                     reason = f"\nReason: <code>{reason}</code>"
+                     reason = f"\nReason: <i>{reason}</i>"
                  time = g_time(tt)
-                 await event.respond(f'<b>Banned <a href="tg://user?id={user_id}">{name}</a></b> for {time}!{reason}', parse_mode='html')
+                 await event.respond(f'Thats {limit}/{limit} Warnings, <a href="tg://user?id={user_id}">{name}</a> has been Banned for {time}!{reason}', parse_mode='html')
                  await tbot.edit_permissions(event.chat_id, event.sender_id, until_date=time.time() + int(tt), view_messages=False)
            elif mode == 'tmute':
                  if reason:
-                     reason = f"\nReason: <code>{reason}</code>"
+                     reason = f"\nReason: <i>{reason}</i>"
                  time = g_time(tt)
-                 await event.respond(f'<b>Muted <a href="tg://user?id={user_id}">{name}</a></b> for {time}!{reason}', parse_mode='html')
+                 await event.respond(f'Thats {limit}/{limit} Warnings, <a href="tg://user?id={user_id}">{name}</a> has been Muted for {time}!{reason}', parse_mode='html')
                  await tbot.edit_permissions(event.chat_id, event.sender_id, until_date=time.time() + int(tt), send_messages=False)
 
 
