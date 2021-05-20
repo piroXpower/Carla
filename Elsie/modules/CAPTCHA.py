d67a10fb9135f1200597388e76d0d01d20469eee
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-20 14:05:22 +0530

    Update CAPTCHA.py

--- Elsie/modules/CAPTCHA.py
+++ Elsie/modules/CAPTCHA.py
@@ -319,12 +319,16 @@ async def math_captcha(chat_id, user_id):
             Button.inline(no3, data=f"math_{no3}"),
         ],
         [
+            Button.inline(no4, data=f"math_{no4}"),
+            Button.inline(no5, data=f"math_{no5}"),
+            Button.inline(no6, data=f"math_{no6}"),
+        ],
+        [
             Button.inline(no7, data=f"math_{no7}"),
             Button.inline(no8, data=f"math_{no8}"),
             Button.inline(answer, data=f"math_{answer}"),
         ],
     )
-    shuffle(kek)
     await asyncio.sleep(0.2)
     await event.respond(
         "Click the correct answer to get verified.", file=pic, buttons=kek
