ab833d87e72be1dde71ba2847d6f0270ef42d415
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-05-21 07:02:06 +0000

    RoseLoverX: auto fixes

--- Elsie/modules/CAPTCHA.py
+++ Elsie/modules/CAPTCHA.py
@@ -1,5 +1,4 @@
 from asyncio import sleep
-from random import shuffle
 
 from telethon import Button, events
 
@@ -312,11 +311,7 @@ async def math_captcha(event, chat_id, user_id):
     question, answer = gen_math_question()
     no1, no2, no3, no4, no5, no6, no7, no8 = rand_no()
     math_captcha_pic(question)
-    ba = [[
-            Button.inline("{}".format(no1), data="ca_{}".format(no1)),
-            Button.inline("{}".format(no2), data="ca_{}".format(no2)),
-            Button.inline("{}".format(no3), data="ca_{}".format(no3)),
-        ],
+    ba = [
         [
             Button.inline("{}".format(no1), data="ca_{}".format(no1)),
             Button.inline("{}".format(no2), data="ca_{}".format(no2)),
@@ -326,7 +321,13 @@ async def math_captcha(event, chat_id, user_id):
             Button.inline("{}".format(no1), data="ca_{}".format(no1)),
             Button.inline("{}".format(no2), data="ca_{}".format(no2)),
             Button.inline("{}".format(no3), data="ca_{}".format(no3)),
-        ]]
+        ],
+        [
+            Button.inline("{}".format(no1), data="ca_{}".format(no1)),
+            Button.inline("{}".format(no2), data="ca_{}".format(no2)),
+            Button.inline("{}".format(no3), data="ca_{}".format(no3)),
+        ],
+    ]
     await sleep(0.2)
     await event.respond("Hello", buttons=ba)
 
