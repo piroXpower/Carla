fa3505253bcbefa3b8b008ff3f1f1a8aaca2f839
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-21 10:34:13 +0530

    Update __init__.py

--- Elsie/modules/__init__.py
+++ Elsie/modules/__init__.py
@@ -356,7 +356,7 @@ def math_captcha_pic(text):
     )
     draw.text(
         (
-            (image_widthz - w + 300) / 2,
+            (image_widthz - w + 150) / 2,
             (image_heightz - h - 3) / 2,
         ),
         text[:3][2:],
@@ -365,7 +365,7 @@ def math_captcha_pic(text):
     )
     draw.text(
         (
-            (image_widthz - w + 600) / 2,
+            (image_widthz - w + 300) / 2,
             (image_heightz - h - 3) / 2,
         ),
         text[:5][3:],
@@ -374,7 +374,7 @@ def math_captcha_pic(text):
     )
     draw.text(
         (
-            (image_widthz - w + randint(990, 1000)) / 2,
+            (image_widthz - w + randint(460, 500)) / 2,
             (image_heightz - h - 3) / 2,
         ),
         text[:6][5:],
@@ -383,7 +383,7 @@ def math_captcha_pic(text):
     )
     draw.text(
         (
-            (image_widthz - w + randint(1080, 1200)) / 2,
+            (image_widthz - w + randint(540, 600)) / 2,
             (image_heightz - h - 3) / 2,
         ),
         text[:7][6:],
