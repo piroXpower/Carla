ae65a8a7b48a56b78ccfe8061dc0bb662b28ee9e
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-05-21 07:14:54 +0000

    RoseLoverX: auto fixes

--- Elsie/modules/CAPTCHA.py
+++ Elsie/modules/CAPTCHA.py
@@ -313,20 +313,20 @@ async def math_captcha(event, chat_id, user_id):
     math_captcha_pic(question)
     buttons = []
     A = [
-            Button.inline("{}".format(no1), data="ca_{}".format(no1)),
-            Button.inline("{}".format(no2), data="ca_{}".format(no2)),
-            Button.inline("{}".format(no3), data="ca_{}".format(no3)),
-        ]
+        Button.inline("{}".format(no1), data="ca_{}".format(no1)),
+        Button.inline("{}".format(no2), data="ca_{}".format(no2)),
+        Button.inline("{}".format(no3), data="ca_{}".format(no3)),
+    ]
     B = [
-            Button.inline("{}".format(no1), data="ca_{}".format(no1)),
-            Button.inline("{}".format(no2), data="ca_{}".format(no2)),
-            Button.inline("{}".format(no3), data="ca_{}".format(no3)),
-        ]
+        Button.inline("{}".format(no1), data="ca_{}".format(no1)),
+        Button.inline("{}".format(no2), data="ca_{}".format(no2)),
+        Button.inline("{}".format(no3), data="ca_{}".format(no3)),
+    ]
     C = [
-            Button.inline("{}".format(no1), data="ca_{}".format(no1)),
-            Button.inline("{}".format(no2), data="ca_{}".format(no2)),
-            Button.inline("{}".format(no3), data="ca_{}".format(no3)),
-        ]
+        Button.inline("{}".format(no1), data="ca_{}".format(no1)),
+        Button.inline("{}".format(no2), data="ca_{}".format(no2)),
+        Button.inline("{}".format(no3), data="ca_{}".format(no3)),
+    ]
     shuffle(A)
     shuffle(B)
     shuffle(C)
