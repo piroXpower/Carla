4b11365bf8243546b404244823ff101886631072
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-25 12:46:01 +0530

    Update Bans.py

--- Evelyn/modules/Bans.py
+++ Evelyn/modules/Bans.py
@@ -11,7 +11,7 @@ async def excecute_operation(event, user_id, name, mode, reason="", tt=0):
         if reason:
             reason = f"\nReason: <code>{reason}</code>"
         await event.respond(
-            f'<b>Banned <a href="tg://user?id={user_id}">{name}</a></b>!{reason}',
+            f'Another one bites the dust...! Banned <a href="tg://user?id={user_id}">{name}</a></b>.{reason}',
             parse_mode="html",
         )
     elif mode == "kick":
@@ -19,7 +19,7 @@ async def excecute_operation(event, user_id, name, mode, reason="", tt=0):
         if reason:
             reason = f"\nReason: <code>{reason}</code>"
         await event.respond(
-            f'<b>Kicked <a href="tg://user?id={user_id}">{name}</a></b>!{reason}',
+            f'I"ve kicked <a href="tg://user?id={user_id}">{name}</a></b>.{reason}',
             parse_mode="html",
         )
     elif mode == "mute":
@@ -60,3 +60,28 @@ async def excecute_operation(event, user_id, name, mode, reason="", tt=0):
             until_date=time.time() + int(tt),
             send_messages=False,
         )
+    elif mode == "unmute":
+        if reason:
+           reason = f"\nReason: <code>{reason}</code>"
+        unmute = await tbot.edit_permissions(
+            event.chat_id, event.sender_id, until_date=None, send_messages=True
+        )
+        if unmute:
+         await event.respond(
+            f'I shall allow <a href="tg://user?id={user_id}">{name}</a></b> to text! {reason}',
+            parse_mode="html",
+        )
+        else:
+         await event.reply("This person can already speak freely!")
+    elif mode == "unban":
+        if reason:
+           reason = f"\nReason: <code>{reason}</code>"
+        unban = await tbot.edit_permissions(
+            event.chat_id, event.sender_id, until_date=None, view_messages=True
+        )
+        if unban:
+         await event.respond(
+            f'Fine, they can join again.'
+        )
+        else:
+         await event.reply("This person hasn't been banned... how am I meant to unban them?")
