70050d32322fe2bfe788ce41deb2ee1b061738a7
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-05-25 07:35:15 +0000

    RoseLoverX: auto fixes

--- Evelyn/modules/Bans.py
+++ Evelyn/modules/Bans.py
@@ -1,9 +1,9 @@
-from Evelyn import tbot, OWNER_ID
-
-from . import g_time
 import time
-from Evelyn import Cbot
-from . import can_ban_users, is_admin, is_owner, ELITES, get_user
+
+from Evelyn import OWNER_ID, Cbot, tbot
+
+from . import ELITES, can_ban_users, g_time, get_user, is_admin, is_owner
+
 
 async def excecute_operation(
     event, user_id, name, mode, reason="", tt=0, attributes=""
@@ -93,26 +93,31 @@ async def excecute_operation(
                 "This person hasn't been banned... how am I meant to unban them?"
             )
 
+
 @Cbot(pattern="^/ban ?(.*)")
 async def ban(event):
- if event.is_private:
-   return
- if event.from_id:
-  if event.is_group:
-   if not event.sender_id == OWNER_ID and not event.sender_id in ELITES:
-      if not await can_ban_users(event, event.sender_id):
-         return
-  reason = ""
-  try:
-   user, reason = await get_user(event)
-  except TypeError:
-   pass
-  if not user:
-   return
-  user_id = user.id
-  mode = "ban"
-  if await is_admin(event.chat_id, user_id):
-   return await event.reply("Why would I ban an admin? That sounds like a pretty dumb idea.")
-  if await is_owner(event, user_id):
-   return await event.reply("Why would I ban the creator? That sounds like a pretty dumb idea.")
-  await excecute_operation(event, user_id, user.first_name, mode, reason, 0, "")
+    if event.is_private:
+        return
+    if event.from_id:
+        if event.is_group:
+            if not event.sender_id == OWNER_ID and not event.sender_id in ELITES:
+                if not await can_ban_users(event, event.sender_id):
+                    return
+        reason = ""
+        try:
+            user, reason = await get_user(event)
+        except TypeError:
+            pass
+        if not user:
+            return
+        user_id = user.id
+        mode = "ban"
+        if await is_admin(event.chat_id, user_id):
+            return await event.reply(
+                "Why would I ban an admin? That sounds like a pretty dumb idea."
+            )
+        if await is_owner(event, user_id):
+            return await event.reply(
+                "Why would I ban the creator? That sounds like a pretty dumb idea."
+            )
+        await excecute_operation(event, user_id, user.first_name, mode, reason, 0, "")
