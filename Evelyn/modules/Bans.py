c7add61298f58818415edc884946374bb32afab0
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-05-25 13:56:07 +0000

    RoseLoverX: auto fixes

--- Evelyn/modules/Bans.py
+++ Evelyn/modules/Bans.py
@@ -148,8 +148,8 @@ async def anon_admins(event):
     input = data.split("_", 1)[1]
     input = input.split("|", 3)
     mode = input[0]
-    user_id = input[1]
-    time = input[2]
+    input[1]
+    input[2]
     await event.edit(str(mode))
 
 
