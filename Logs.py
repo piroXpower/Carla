b6ce0e79616aa367a95b7c947f7e4dc405bdd6d4
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-05-26 05:41:16 +0000

    RoseLoverX: auto fixes

--- Logs.py
+++ Logs.py
@@ -1,4 +1,5 @@
-import os, sys
+import os
+import sys
 
 API_KEY = os.environ.get("API_KEY")
 API_HASH = os.environ.get("API_HASH")
@@ -8,16 +9,18 @@ TOKEN = "1842654376:AAHXjoWnlKx_jvazdqJremZ40B0M29ufd2Q"
 OWNER_ID = os.environ.get("OWNER_ID")
 
 from telethon import TelegramClient, events
+
 wbot = TelegramClient(None, API_KEY, API_HASH)
 
 try:
- wbot.start(bot_token=TOKEN)
+    wbot.start(bot_token=TOKEN)
 except Exception as e:
- print("Logger Failed to Start." + str(e))
- sys.exit(1)
+    print("Logger Failed to Start." + str(e))
+    sys.exit(1)
+
 
 @wbot.on(events.NewMessage(pattern="^[!.?/]logs$"))
 async def logs(event):
- if not event.sender_id == OWNER_ID:
-    return
- await event.respond("Logger Test")
+    if not event.sender_id == OWNER_ID:
+        return
+    await event.respond("Logger Test")
