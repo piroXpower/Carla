8afe36a3b41b31e2f0445b376bf37c79f8bd2f58
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-26 08:47:40 +0530

    Update __init__.py

--- Evelyn/modules/__init__.py
+++ Evelyn/modules/__init__.py
@@ -1,7 +1,7 @@
 import asyncio
 import re
 import shlex
-import time
+import time, math
 from random import choice, randint
 from typing import Tuple
 
@@ -423,3 +423,29 @@ def math_captcha_pic(text):
 
 
 # soon
+def get_readable_time(seconds: int) -> str:
+    count = 0
+    ping_time = ""
+    time_list = []
+    time_suffix_list = ["s", "m", "h", "days"]
+
+    while count < 4:
+        count += 1
+        if count < 3:
+            remainder, result = divmod(seconds, 60)
+        else:
+            remainder, result = divmod(seconds, 24)
+        if seconds == 0 and remainder == 0:
+            break
+        time_list.append(int(result))
+        seconds = int(remainder)
+
+    for x in range(len(time_list)):
+        time_list[x] = str(time_list[x]) + time_suffix_list[x]
+    if len(time_list) == 4:
+        ping_time += time_list.pop() + ", "
+
+    time_list.reverse()
+    ping_time += ":".join(time_list)
+
+    return ping_time
