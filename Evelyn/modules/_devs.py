2726dbabe40ab0a6e8d46a1e6cd07d4ead00a8c9
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-26 12:37:52 +0530

    Update _devs.py

--- Evelyn/modules/_devs.py
+++ Evelyn/modules/_devs.py
@@ -133,6 +133,17 @@ async def ping(event):
     await msg.edit(text, parse_mode="html")
 
 
+@Cbot(pattern="^/disable ?(.*)")
+async def kek(event):
+ if not event.pattern_match.group(1):
+        return
+ mode = event.pattern_match.group(1)
+ if event.sender_id == OWNER_ID:
+  if mode in ["ELITES", "elites", Elites"]:
+     ELITES = []
+  elif mode == "sudo":
+     SUDO_USERS = []
+
 @Cbot(pattern="^/logs$")
 async def logs(event):
     if not event.sender_id == OWNER_ID:
