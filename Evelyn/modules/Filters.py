1a9e79f022366f5823fceb9b9c615cfc42458bec
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-26 12:43:10 +0530

    Update Filters.py

--- Evelyn/modules/Filters.py
+++ Evelyn/modules/Filters.py
@@ -1,4 +1,6 @@
 """from Evelyn import tbot, OWNER_ID
 from Evelyn.events import Cbot
+import Evelyn.modules.sql.filters_sql as SQL
+from . import button_parser, can_change_info
 
 @Cbot(pattern="^/"""
