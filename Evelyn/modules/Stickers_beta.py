50d027da6bb074e819724ec4fa89549ca0a969d8
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-27 17:23:45 +0530

    Create Stickers_beta.py

--- Evelyn/modules/Stickers_beta.py
+++ Evelyn/modules/Stickers_beta.py
@@ -1,8 +1,11 @@
-from telethon.tl.functions.stickers import CreateStickerSetRequest as create_set
-from telethon.tl.types import InputDocument, InputStickerSetItem, MaskCoords
+import os
 
-from Evelyn import tbot
 from Evelyn.events import Cbot
+from Evelyn import tbot
+from telethon.tl.functions.stickers import CreateStickerSetRequest as create_set
+from telethon.tl.types import MaskCoords, InputDocument, InputStickerSetItem
+
+
 
 
 @Cbot(pattern="^/kang ?(.*)")
@@ -25,33 +28,11 @@ async def kang(event):
     file_reference = msg.media.document.file_reference
     pack_name = event.sender.first_name + "'s Kang pack"
     short_name = event.sender.first_name + "'s pack1"
-    event.sender_id
+    user_id = event.sender_id
     try:
-        result = tbot(
-            create_set(
-                user_id=user_id,
-                title=pack_name,
-                short_name=short_name,
-                stickers=[
-                    InputStickerSetItem(
-                        document=InputDocument(
-                            id=sticker_id,
-                            access_hash=access_hash,
-                            file_reference=file_reference,
-                        ),
-                        emoji=emoji,
-                        mask_coords=MaskCoords(n=42, x=7.13, y=7.13, zoom=7.13),
-                    )
-                ],
-                masks=True,
-                animated=False,
-                thumb=InputDocument(
-                    id=sticker_id,
-                    access_hash=access_hash,
-                    file_reference=file_reference,
-                ),
-            )
-        )
+        result = tbot(create_set(user_id=user_id, title=pack_name, short_name=short_name,
+             stickers=[InputStickerSetItem(document=InputDocument(id=sticker_id, access_hash=access_hash, file_reference=file_reference), 
+             emoji=emoji, mask_coords=MaskCoords(n=42,x=7.13,y=7.13,zoom=7.13))], masks=True, animated=False, thumb=InputDocument(id=sticker_id, access_hash=access_hash, file_reference=file_reference)))
     except Exception as e:
-        return await event.respond(str(e))
+         return await event.respond(str(e))
     await event.respond(str(result)[:200])
