50f2fd7a37d8aa33d0f8d2cc6baa6a623a1a15a3
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-05-28 15:30:03 +0000

    RoseLoverX: auto fixes

--- Evelyn/modules/Reporting.py
+++ Evelyn/modules/Reporting.py
@@ -1,9 +1,9 @@
+from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
 
 from Evelyn import OWNER_ID, tbot
 from Evelyn.events import Cbot
 from Evelyn.modules.sql import reporting_sql as sql
-from telethon import events
 
 from . import ELITES, can_change_info, is_admin
 
