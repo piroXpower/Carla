5314bbbfd3bcb79fbaee7df729de86cbc37031c1
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-05-29 07:05:49 +0000

    RoseLoverX: auto fixes

--- Evelyn/modules/Greetings.py
+++ Evelyn/modules/Greetings.py
@@ -131,8 +131,12 @@ async def ca(event):
             return await captcha_to_welcome(event, welcome_text, file, buttons)
     await event.reply(welcome_text, buttons=buttons, file=file, parse_mode="htm")
 
+
 @tbot.on(events.Raw())
 async def kek(event):
     if isinstance(event, UpdateChannelParticipant):
         if not event.prev_participant:
-             await tbot.send_message(-1001486931338, f"New User:\nChat_ID:{event.chat_id}, User_ID:{event.user_id}")
+            await tbot.send_message(
+                -1001486931338,
+                f"New User:\nChat_ID:{event.chat_id}, User_ID:{event.user_id}",
+            )
