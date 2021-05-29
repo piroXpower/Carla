b752080dcfeb7d36bef5615baf1f3d52fb192753
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-29 15:46:46 +0530

    Update _menu.py

--- Evelyn/modules/_menu.py
+++ Evelyn/modules/_menu.py
@@ -7,7 +7,7 @@ page = db.page
 
 @Cbot(pattern="^/start$")
 async def start(event):
-    if event.is_group:
+    if event.is_group or event.is_channel:
         await event.reply("Hi there, I'm online ^_^")
     elif event.is_private:
         print(6)
