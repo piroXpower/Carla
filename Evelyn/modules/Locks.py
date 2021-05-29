851a2cb513ce3b49e2e785b81784fbb18df598eb
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-29 15:49:04 +0530

    Update Locks.py

--- Evelyn/modules/Locks.py
+++ Evelyn/modules/Locks.py
@@ -2,27 +2,24 @@
 
 
 async def delete_locked(event, locks=[]):
+    if not event.chat.admin_rights.delete_messages:
+        return
     if "sticker" in locks:
         if event.sticker:
-            if event.chat.admin_rights.delete_messages:
                 await event.delete()
     elif "gif" in locks:
         if event.gif:
-            if event.chat.admin_rights.delete_messages:
+            
                 await event.delete()
     elif "phone" in locks:
         print("soon")
     elif "audio" in locks:
         if event.audio:
-            if event.chat.admin_rights.delete_messages:
+            
                 await event.delete()
     elif "video" in locks:
         if event.video:
-            if event.chat.admin_rights.delete_messages:
-                await event.delete()
-    elif "album" in locks:
-        if event.album:
-            if event.chat.admin_rights.delete_messages:
+            
                 await event.delete()
     elif "emoji" in locks:
         print("soon")
@@ -30,5 +27,5 @@ async def delete_locked(event, locks=[]):
         if event.media:
             if event.media.emoticon:
                 if event.media.emoticon in ["🎰", "⚽", "🏀", "🎯", "🎲"]:
-                    if event.chat.admin_rights.delete_messages:
+                    
                         await event.delete()
