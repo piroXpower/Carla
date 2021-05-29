1458cb5632a2e080775c1b44bd9c6dfe70ce4a3c
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-05-29 10:19:28 +0000

    RoseLoverX: auto fixes

--- Evelyn/modules/Locks.py
+++ Evelyn/modules/Locks.py
@@ -6,26 +6,26 @@ async def delete_locked(event, locks=[]):
         return
     if "sticker" in locks:
         if event.sticker:
-                await event.delete()
+            await event.delete()
     elif "gif" in locks:
         if event.gif:
-            
-                await event.delete()
+
+            await event.delete()
     elif "phone" in locks:
         print("soon")
     elif "audio" in locks:
         if event.audio:
-            
-                await event.delete()
+
+            await event.delete()
     elif "video" in locks:
         if event.video:
-            
-                await event.delete()
+
+            await event.delete()
     elif "emoji" in locks:
         print("soon")
     elif "emojigame" in locks:
         if event.media:
             if event.media.emoticon:
                 if event.media.emoticon in ["🎰", "⚽", "🏀", "🎯", "🎲"]:
-                    
-                        await event.delete()
+
+                    await event.delete()
