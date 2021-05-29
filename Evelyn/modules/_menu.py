6f5dcc12e12aa7c5c4a05a920d9520d0522158cc
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-05-29 10:12:22 +0000

    RoseLoverX: auto fixes

--- Evelyn/modules/_menu.py
+++ Evelyn/modules/_menu.py
@@ -1,12 +1,13 @@
-from Evelyn import tbot
 from Evelyn.events import Cbot
+
 from . import db
 
 page = db.page
 
+
 @Cbot(pattern="^/start$")
 async def start(event):
- if event.is_group:
-  await event.reply("Hi there, I'm online ^_^")
- elif event.is_private:
-  print(6)
+    if event.is_group:
+        await event.reply("Hi there, I'm online ^_^")
+    elif event.is_private:
+        print(6)
