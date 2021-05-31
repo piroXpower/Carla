dd62d30537fae8fe69e0c13322da2590a21b0b33
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-31 20:11:50 +0530

    Update Chatbot.py

--- Evelyn/modules/Chatbot.py
+++ Evelyn/modules/Chatbot.py
@@ -1,24 +1,8 @@
-"""from Evelyn import tbot
 from telethon import events
+from Evelyn import tbot
 from Evelyn.events import Cbot
+
 import requests
 
-url = "https://alexa-bot-api-web-server.vercel.app/api/alexa?stimulus=${}?lang=$en"
+url = "https://acobot-brainshop-ai-v1.p.rapidapi.com/get"
 
-BOT_ID = 1705574750
-
-@tbot.on(events.NewMessage(pattern=None))
-async def su(event):
- if event.text.startswith("/") or event.text.startswith(".") or event.text.startswith("?") or event.text.startswith("!"):
-   return
- if event.reply_to_msg_id:
-   if (await event.get_reply_message()).sender_id != BOT_ID:
-      return
- else:
-    return
- if event.media:
-   return
- text = event.text
- response = requests.request("GET", url.format(text))
- await event.reply(str(response.text))
-"""
