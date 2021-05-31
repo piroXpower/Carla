4fcd3b18d11c2d3af30c2835d497d7c55996b17f
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-31 20:08:31 +0530

    Create chatbot_sql.py

---
+++ Evelyn/modules/sql/chatbot_sql.py
@@ -0,0 +1,29 @@
+from . import BASE, SESSION
+from sqlalchemy import Column, String, Boolean
+
+class ChatBot(BASE):
+    __tablename__ = "chatbot"
+    chat_id = Column(String(14), primary_key=True)
+    mode = Column(Boolean, default=True)
+
+    def __init__(self, chat_id, mode=True):
+        self.chat_id = chat_id
+        self.mode = mode
+
+ChatBot.__table__.create(checkfirst=True)
+
+def set_chatbot_mode(chat_id: str, mode):
+    bot = SESSION.query(ChatBot).get(str(chat_id))
+    if bot:
+        bot.mode = mode
+    else:
+        bot = ChatBot(chat_id, mode)
+    SESSION.add(bot)
+    SESSION.commit()
+
+
+def chatbot_mode(chat_id: str):
+    bot = SESSION.query(ChatBot).get(str(chat_id))
+    if bog:
+        return bot.mode
+    return False
