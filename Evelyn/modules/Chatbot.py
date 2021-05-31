1305a51946a0f93852153121005b760455222e00
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-05-31 14:52:54 +0000

    RoseLoverX: auto fixes

--- Evelyn/modules/Chatbot.py
+++ Evelyn/modules/Chatbot.py
@@ -1,29 +1,30 @@
-from telethon import events
-from Evelyn import tbot
-from Evelyn.events import Cbot
 import Evelyn.modules.sql.chatbot_sql as sql
-from requests import get
+from Evelyn.events import Cbot
+
 from . import can_change_info
+
 """
 url = "https://acobot-brainshop-ai-v1.p.rapidapi.com/get"
 """
+
+
 @Cbot(pattern="^/chatbot ?(.*)")
 async def cb(event):
- if event.is_group:
-   if not await can_change_info(event, event.sender_id):
-      return
- args = event.pattern_match.group(1)
- if not args:
-  mode = sql.chatbot_mode(event.chat_id)
-  if mode:
-     await event.reply("ChatBot is currently **enabled** for this chat.")
-  else:
-     await event.reply("ChatBot is currently **disabled** for this chat.")
- elif args in ['on', 'y', 'yes']:
-     await event.reply("**Enabled** ChatBot for this chat.")
-     sql.set_chatbot_mode(event.chat_id, True)
- elif args in ['off', 'n', 'no']:
-     await event.reply("**Disabled** ChatBot for this chat.")
-     sql.set_chatbot_mode(event.chat_id, False)
- else:
-    await event.reply("Your input was not recognised as one of: yes/no/y/n/on/off")
+    if event.is_group:
+        if not await can_change_info(event, event.sender_id):
+            return
+    args = event.pattern_match.group(1)
+    if not args:
+        mode = sql.chatbot_mode(event.chat_id)
+        if mode:
+            await event.reply("ChatBot is currently **enabled** for this chat.")
+        else:
+            await event.reply("ChatBot is currently **disabled** for this chat.")
+    elif args in ["on", "y", "yes"]:
+        await event.reply("**Enabled** ChatBot for this chat.")
+        sql.set_chatbot_mode(event.chat_id, True)
+    elif args in ["off", "n", "no"]:
+        await event.reply("**Disabled** ChatBot for this chat.")
+        sql.set_chatbot_mode(event.chat_id, False)
+    else:
+        await event.reply("Your input was not recognised as one of: yes/no/y/n/on/off")
