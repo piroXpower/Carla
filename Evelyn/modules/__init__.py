002d99d581ea6b15daf953a3ce25a31f35c73770
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-31 16:29:09 +0530

    Update __init__.py

--- Evelyn/modules/__init__.py
+++ Evelyn/modules/__init__.py
@@ -120,17 +120,25 @@ async def is_owner(event, user_id):
         await event.reply("Only admins can execute this command!")
         return False
     if not perm.is_creator:
-        await event.reply("You need to be the chat Creator to do this!")
+        await event.reply(f"You need to be the chat owner of {event.chat.title} to do this.")
         return False
     return True
 
+async def cb_is_owner(event, user_id):
+    perm = await tbot.get_permissions(event.chat_id, user_id)
+    if not perm.is_admin:
+        await event.answer("Only admins can execute this command!")
+        return False
+    if not perm.is_creator:
+        await event.edit(f"You need to be the chat owner of {event.chat.title} to do this.")
+        return False
+    return True
 
 async def check_owner(event, user_id):
     perm = await tbot.get_permissions(event.chat_id, user_id)
     if not perm.is_creator:
         return False
     return True
-
 
 async def can_del_msg(event, user_id):
     perm = await tbot.get_permissions(event.chat_id, user_id)
