7c19c8fa772c494cdd66a4479cdc54ff5a9bcb87
Amarnath C <amarnathcharichilil@gmail.com>
2021-05-31 17:18:25 +0530

    Update Notes.py

--- Evelyn/modules/Notes.py
+++ Evelyn/modules/Notes.py
@@ -4,200 +4,125 @@ import Evelyn.modules.sql.notes_sql as sql
 from Evelyn import tbot
 from Evelyn.events import Cbot
 
-from . import can_change_info, is_admin
-
-
-def gen_button_from_text(text):
-    print(6)
-
-
-def get_markup(ev):
-    print(7)
-
+from . import can_change_info, is_admin, get_reply_msg_btns_text, button_parser
 
 @Cbot(pattern="^/save ?(.*)")
 async def save(event):
-    if event.is_private:
-        return
-    if not await can_change_info(event, event.sender_id):
-        return
-    if not event.reply_to_msg_id and not event.pattern_match.group(1):
+  if event.is_private:
+      return
+  if not event.from_id:
+      return await anonymous_save(event)
+  if event.is_group:
+         if not await can_change_info(event, event.sender_id):
+            return
+  if not event.pattern_match.group(1) and not event.is_reply:
+      return await event.reply("You need to give the note a name!")
+  elif event.is_reply:
+      if not event.pattern_match.group(1):
         return await event.reply("You need to give the note a name!")
-    elif event.reply_to_msg_id:
-        if not event.pattern_match.group(1):
-            return await event.reply("You need to give the note a name!")
-        msg = await event.get_reply_message()
-        if msg.media:
-            file = msg.file.id
-            note = msg.text
-        else:
-            note = msg.text
-            if msg.reply_markup:
-                finale = get_markup(msg.reply_markup)
-                note += "|"
-                note += finale
-            file = None
-        keyword = event.pattern_match.group(1)
-    elif not event.reply_to_msg_id and event.pattern_match.group(1):
-        file = None
-        args = event.pattern_match.group(1)
-        args = args.split(" ", 1)
-        if len(args) == 1:
-            return await event.reply("You need to give the note some content!")
-        keyword = args[0]
-        note = args[1]
-    try:
-        sql.add_note(event.chat_id, keyword, note, file)
-    except Exception as e:
-        return await event.respond(str(e))
-    await event.reply(f"Saved note '{keyword}'.")
-
-
-@tbot.on(events.NewMessage(pattern=r"\#(\S+)"))
-async def nt(event):
-    name = event.pattern_match.group(1)
-    if not name:
-        return
-    note = sql.get_notes(event.chat_id, name)
-    if not note:
-        return
-    reply_w = note.reply
-    reply_w = reply_w.replace("{noprivate}", "")
-    reply_w = reply_w.replace("{admin}", "")
-    reply_w = reply_w.replace("{private}", "")
-    if "{admin}" in note.reply:
-        if not await is_admin(event.chat_id, event.sender_id):
-            return await event.reply("This note is for admins only.")
-    butto = None
-    if "|" in note.reply:
-        reply_w, butto = gen_button_from_text(note.reply)
-    mode = sql.get_mode(event.chat_id)
-    if mode == False or "{noprivate}" in note.reply:
-        if note.file:
-            try:
-                await event.reply(reply_w, file=note.file, buttons=butto)
-            except:
-                await event.reply(reply_w, file=note.file)
-        else:
-            try:
-                await event.reply(reply_w, buttons=butto)
-            except:
-                await event.reply(reply_w)
-    elif mode == True or "{private}" in note.reply:
-        text = f"Tap here to view '{name}' in your private chat."
-        luv = f"{event.chat_id}_{name}"
-        buttons = Button.url(
-            "Click me!", "t.me/MissEvelyn_bot?start=notes_{}".format(luv)
-        )
-        await event.reply(text, buttons=buttons)
-
-
-@Cbot(pattern="^/get ?(.*)")
-async def getnote(event):
-    name = event.pattern_match.group(1)
-    if not name:
-        return await event.reply("Not enough arguments!")
-    note = sql.get_notes(event.chat_id, name)
-    if not note:
-        return await event.reply("Note not found.")
-    reply_w = note.reply
-    if "{admin}" in note.reply:
-        reply_w = note.reply.replace("{admin}", "")
-        if not await is_admin(event.chat_id, event.sender_id):
-            return await event.reply("This note is for admins only.")
-    mode = sql.get_mode(event.chat_id)
-    if mode == False:
-        if note.file:
-            await event.reply(reply_w, file=note.file)
-        else:
-            await event.reply(reply_w)
-    elif mode == True:
-        text = f"Tap here to view '{name}' in your private chat."
-        luv = f"{event.chat_id}_{name}"
-        buttons = Button.url(
-            "Click me!", "t.me/MissEvelyn_bot?start=notes_{}".format(luv)
-        )
-        await event.reply(text, buttons=buttons)
-
-
-pos = ["y", "yes", "on"]
-neg = ["n", "no", "off"]
-
+      name = event.pattern_match.group(1)
+      msg = await event.get_reply_message()
+      if msg.media and not msg.text:
+       file = msg.file.id
+       reply = ""
+      elif msg.media and msg.text:
+       file = msg.file.id
+       reply = msg.text
+      elif msg.text:
+       file = None
+       reply = msg.text
+      if msg.reply_markup:
+           buttons = get_reply_msg_btns_text(msg)
+           reply = reply + " " + str(buttons)
+  elif event.pattern_match.group(1):
+    final = event.text.split(None, 1)[0]
+    final = final.split(" ", 2)
+    name = final[0]
+    reply = final[1]
+    file = None
+  await event.reply(f"Saved note `{name}`")
+  sql.add_note(event.chat_id, name, reply, file)
 
 @Cbot(pattern="^/privatenotes ?(.*)")
-async def pn(event):
-    if event.is_private:
-        return  # conn
+async def pnotes(event):
+ if event.is_private:
+   return
+ if not event.from_id:
+      return #for now
+ if event.is_group:
+     if not await can_change_info(event, event.sender_id):
+         return
+ arg = event.pattern_match.group(1)
+ if not arg:
+   mode = sql.get_mode(event.chat_id)
+   if mode:
+     text = "Your notes are currently being sent in private. Evelyn will send a small note with a button which redirects to a private chat."
+   else:
+     text = "Your notes are currently being sent in the group."
+   await event.reply(text)
+ elif arg in ['y', 'yes', 'on', 'true']:
+   await event.reply("Evelyn will now send a message to your chat with a button redirecting to PM, where the user will receive the note.")
+   sql.set_mode(event.chat_id, True)
+ elif arg in ['n', 'no', 'off', 'false']:
+   await event.reply("Evelyn will now send notes straight to the group.")
+   sql.set_mode(event.chat_id, False)
+ else:
+   await event.reply(f"failed to get boolean value from input: expected one of y/yes/on or n/no/off; got: {arg}")
+  
+@Cbot(pattern="^/clear ?(.*)")
+async def clear(event):
+ if event.is_private:
+    return
+ if not event.from_id:
+      return #for now
+ if event.is_group:
     if not await can_change_info(event, event.sender_id):
-        return
-    args = event.pattern_match.group(1)
-    if not args:
-        mode = sql.get_mode(event.chat_id)
-        if mode == False:
-            await event.reply("Your notes are currently being sent in the group.")
-        else:
-            await event.reply(
-                "Your notes are currently being sent in private. Evelyn will send a small note with a button which redirects to a private chat."
-            )
-    elif args in pos:
-        await event.reply(
-            "Evelyn will now send a message to your chat with a button redirecting to PM, where the user will receive the note."
-        )
-        sql.set_mode(event.chat_id, True)
-    elif args in neg:
-        await event.reply("Evelyn will now send notes straight to the group.")
-        sql.set_mode(event.chat_id, False)
-    else:
-        await event.reply(
-            f"failed to get boolean value from input: expected one of y/yes/on or n/no/off; got: {args}"
-        )
+       return
+ args = event.pattern_match.group(1)
+ if not args:
+   return await event.reply("Not enough arguments!")
+ notes = sql.get_all_notes(event.chat_id)
+ for note in notes:
+   if note.keyword == args:
+      await event.reply("Note '{}' deleted!".format(args))
+      return sql.remove_note(event.chat_id, args)
+ await event.reply("You haven't saved any notes with this name yet!")
+
+@tbot.on(events.NewMessage(pattern=r"\#(\S+)"))
+async def nottrig(event):
+ name = event.pattern_match.group(1)
+ note = get_notes(event.chat_id, name)
+ if not note:
+    return
+ if event.is_reply:
+  reply_to = event.reply_to_msg_id
+ else:
+  reply_to = event.id
+ mode = sql.get_mode(event.chat_id)
+ reply = note.reply
+ if "{private}" in reply:
+   reply = (reply).replace("{private}", "")
+   mode = True
+ if "{noprivate}" in reply:
+   reply = (reply).replace("{noprivate}", "")
+   mode = False
+ if mode:
+  if "{admin}" in reply:
+    reply = (reply).replace("{admin}", "")
+    if not await is_admin(event.chat_id, event.sender_id):
+       return
+  text = "Tap here to view '{}' in your private chat.".format(name)
+  buttons = Button.url("Click Me", f"t.me/MissEvelyn_bot?start=notes_{event.chat_id}&{name}")
+  await event.reply(text, buttons=buttons)
+ else:
+  reply_text, buttons = button_parser(reply)
+  if note.file:
+    file = note.file
+  else:
+    file = None
+  await event.reply(reply_text, buttons=buttons, file=file)
 
 
-@Cbot(pattern="^/start notes_(.*)")
-async def kp(event):
-    name = event.pattern_match.group(1)
-    chat, name = name.split("_", 1)
-    chat = int(chat.strip())
-    name = name.strip()
-    note = sql.get_notes(chat, name)
-    if not note:
-        return
-    reply_w = note.reply
-    if "|" in note.reply:
-        reply_w, butto = gen_button_from_text(note.reply)
-    if note.file:
-        try:
-            await event.reply(reply_w, file=note.file, buttons=butto)
-        except:
-            await event.reply(reply_w, file=note.file)
-    else:
-        try:
-            await event.reply(f"**{name}:**\n\n" + reply_w, buttons=butto)
-        except:
-            await event.reply(f"**{name}:**\n\n" + reply_w)
+ 
 
-
-@Cbot(pattern="^/notes")
-async def bin(event):
-    if event.is_private:
-        return
-    mode = sql.get_mode(event.chat_id)
-    txt = f"List of notes in {event.chat.title}"
-    if mode == False:
-        all_notes = sql.get_all_notes(event.chat_id)
-        if len(all_notes) > 0:
-            for a_note in all_notes:
-                add = ""
-                if "{admin}" in a_note.reply:
-                    add = " __{admin}__"
-                elif "{private}" in a_note.reply:
-                    add = " __{private}__"
-                elif "{noprivate}" in a_note.reply:
-                    add = " __{noprivate}__"
-                txt += f"\n- `{a_note.keyword}` {add}"
-            txt += "\nYou can retrieve these notes\nby using `/get notename`, or \n`#notename`"
-        else:
-            txt = f"No notes in {event.chat.title}!"
-        await event.reply(txt)
-    else:
-        print(6)
+  
