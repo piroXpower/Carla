fda590ca2274614768d523a73581b3e6b1a0cee9
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-03 15:30:14 +0530

    Update Inline.py

--- Evelyn/modules/Inline.py
+++ Evelyn/modules/Inline.py
@@ -73,7 +73,7 @@ async def pypi(event):
             summary = result["info"]["summary"]
             release_url = result["info"]["release_url"]
             requires_dist = result["info"]["requires_dist"]
-            py = f"<b><h1>{name}</h1></b>"
+            py = f"<b>{name}</b>"
             py += f"\n\n<b>Author:</b> {author}"
             py += f"\n<b>Latest Version:</b> <code>{version}</code>"
             if summary:
