8e14445fdd53bcc4ff6200f85b9f97fa2c01816b
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-03 14:59:39 +0530

    Update Inline.py

--- Evelyn/modules/Inline.py
+++ Evelyn/modules/Inline.py
@@ -8,15 +8,32 @@ async def cq(event: events.InlineQuery.Event):
     builder = event.builder
     query = event.pattern_match.group(1)
     if query == "" or len(query) > 4096:
-        content = "**Send whisper messages through inline mode**\n\nUsage: `@ezWhisperBot [@username] text`"
-        des = "Usage: @ezWhisperBot [@username] text"
+        content = "**Send whisper messages through inline mode**\n\nUsage: `@MissEvelyn_Bot [@username] text`"
+        des = "Usage: @MissEvelyn_Bot [@username] text"
         icon_url = "https://www.freeiconspng.com/uploads/whisper-icon-0.png"
         resultm = builder.article(
             title="🔥 Write a whisper message",
             description=des,
             text=content,
             buttons=[
-                [Button.switch_inline("Search Again", query="cq ", same_peer=True)],
+                [Button.switch_inline("Make New", query="cq ", same_peer=True)],
             ],
         )
         await event.answer([resultm])
+    elif not query.startswith("@"):
+        title = "👀 Whisper once to the first one who open it"
+        content = (
+            "👀 The first one who open the whisper can read it"
+        )
+        des = f"🤫 {query}"
+        buttons=[
+                [Button.inline("👀 show message", data="show_whisper")],
+            ]
+        resultm = builder.article(
+            title="🔥 Write a whisper message",
+            description=des,
+            text=content,
+            buttons=buttons,
+        )
+        msg = await event.answer([resultm])
+
