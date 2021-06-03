8a6c2e14b27e565c6b1703778ceae7349674eaa3
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-03 18:51:54 +0530

    Create elevated_users_sql.py

---
+++ Evelyn/modules/sql/elevated_users_sql.py
@@ -0,0 +1,49 @@
+from . import SESSION, BASE
+from sqlalchemy import Integer, UnicodeText, Column
+
+class SUDO(BASE):
+ __tablename__ = "sudousers"
+ user_id = Column(Integer, primary_key=True)
+ first_name = Column(UnicodeText)
+
+ def __init__(self, user_id, first_name:
+   self.user_id = user_id
+   self.first_name = first_name
+
+class ELITE(BASE):
+ __tablename__ = "eliteusers"
+ user_id = Column(Integer, primary_key=True)
+ first_name = Column(UnicodeText)
+
+ def __init__(self, user_id, first_name:
+   self.user_id = user_id
+   self.first_name = first_name
+
+SUDO.__table__.create(checkfirst=True)
+ELITE.__table__.create(checkfirst=True)
+
+def add_sudo(user_id, first_name):
+ sudo = SESSION.query(SUDO).get(str(user_id))
+ if sudo:
+   return False
+ sudo = SUDO(user_id, first_name)
+ SESSION.add(sudo)
+ SESSION.commit()
+ return True
+
+def remove_sudo(user_id):
+ sudo = SESSION.query(SUDO).get(str(user_id))
+ if sudo:
+   SESSION.delete(sudo)
+   SESSION.commit()
+   return True
+ return False
+
+def get_all_sudos():
+ try:
+  return SESSION.query(SUDO).all()
+ except BaseException:
+  return None
+ finally:
+      SESSION.close()
+
