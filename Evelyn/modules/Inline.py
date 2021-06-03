4c66236a1406931f95f3728fbb2257a3fcab4213
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-06-03 09:07:02 +0000

    RoseLoverX: auto fixes

--- Evelyn/modules/Inline.py
+++ Evelyn/modules/Inline.py
@@ -1,23 +1,22 @@
-from telethon import events, Button
+from telethon import Button, events
 
 from Evelyn.events import Cquery
 
 
 @Cquery(pattern="cq ?(.*)")
 async def cq(event: events.InlineQuery.Event):
-        builder = event.builder
-        query = event.pattern_match.group(1)
-        if query == '' or len(query) > 4096:
-            title = "🔥 Write a whisper message"
-            content = "**Send whisper messages through inline mode**\n\nUsage: `@ezWhisperBot [@username] text`"
-            des = "Usage: @ezWhisperBot [@username] text"
-            icon_url = "https://www.freeiconspng.com/uploads/whisper-icon-0.png"
-            resultm = builder.article(
-                title="No Results.",
-                description="Try Again With correct Spelling",
-                text="**No Matching Found**",
-                buttons=[
-                    [Button.switch_inline("Search Again", query="yt ", same_peer=True)],
-                ],
-            )
-            await event.answer([resultm])
+    builder = event.builder
+    query = event.pattern_match.group(1)
+    if query == "" or len(query) > 4096:
+        content = "**Send whisper messages through inline mode**\n\nUsage: `@ezWhisperBot [@username] text`"
+        des = "Usage: @ezWhisperBot [@username] text"
+        icon_url = "https://www.freeiconspng.com/uploads/whisper-icon-0.png"
+        resultm = builder.article(
+            title="No Results.",
+            description="Try Again With correct Spelling",
+            text="**No Matching Found**",
+            buttons=[
+                [Button.switch_inline("Search Again", query="yt ", same_peer=True)],
+            ],
+        )
+        await event.answer([resultm])
