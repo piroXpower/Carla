3eb654964562c6400f12eb0c49d0f2205dfa6f22
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-06-03 08:59:48 +0000

    RoseLoverX: auto fixes

--- Evelyn/modules/Inline.py
+++ Evelyn/modules/Inline.py
@@ -5,17 +5,14 @@ from Evelyn.events import Cquery
 
 @Cquery(pattern="cq ?(.*)")
 async def cq(event: events.InlineQuery.Event):
-        builder = event.builder
-        query = event.pattern_match.group(1)
-        if query == '' or len(query) > 4096:
-            title = "🔥 Write a whisper message"
-            content = "**Send whisper messages through inline mode**\n\nUsage: `@ezWhisperBot [@username] text`"
-            des = "Usage: @ezWhisperBot [@username] text"
-            icon_url = "https://www.freeiconspng.com/uploads/whisper-icon-0.png"
-            result = builder.article(
-                title=title,
-                description=content,
-                text=des,
-                thumb=icon_url
-            )
-            await event.answer([result])
+    builder = event.builder
+    query = event.pattern_match.group(1)
+    if query == "" or len(query) > 4096:
+        title = "🔥 Write a whisper message"
+        content = "**Send whisper messages through inline mode**\n\nUsage: `@ezWhisperBot [@username] text`"
+        des = "Usage: @ezWhisperBot [@username] text"
+        icon_url = "https://www.freeiconspng.com/uploads/whisper-icon-0.png"
+        result = builder.article(
+            title=title, description=content, text=des, thumb=icon_url
+        )
+        await event.answer([result])
