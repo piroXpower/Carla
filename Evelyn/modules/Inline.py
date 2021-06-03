6875d54e0864e2ca162fbc99b2c98d9343942793
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-06-03 08:55:54 +0000

    RoseLoverX: auto fixes

--- Evelyn/modules/Inline.py
+++ Evelyn/modules/Inline.py
@@ -1,23 +1,24 @@
-from Evelyn.events import Cquery
 from telethon import events
+
+from Evelyn.events import Cquery
 
 
 @Cquery(pattern="cq ?(.*)")
 async def cq(event: events.InlineQuery.Event):
-   try:
-    builder = event.builder
-    query = event.pattern_match.group(1)
-    if query == None or len(query) > 4096:
-        title = "🔥 Write a whisper message"
-        content = "**Send whisper messages through inline mode**\n\nUsage: `@ezWhisperBot [@username] text`"
-        des = "Usage: @ezWhisperBot [@username] text"
-        icon_url = "https://www.freeiconspng.com/uploads/whisper-icon-0.png"
-        result = builder.article(
-            title=title,
-            description=content,
-            text=des,
-            thumb=icon_url,
-        )
-        await event.answer([result])
-   except Exception as e:
+    try:
+        builder = event.builder
+        query = event.pattern_match.group(1)
+        if query == None or len(query) > 4096:
+            title = "🔥 Write a whisper message"
+            content = "**Send whisper messages through inline mode**\n\nUsage: `@ezWhisperBot [@username] text`"
+            des = "Usage: @ezWhisperBot [@username] text"
+            icon_url = "https://www.freeiconspng.com/uploads/whisper-icon-0.png"
+            result = builder.article(
+                title=title,
+                description=content,
+                text=des,
+                thumb=icon_url,
+            )
+            await event.answer([result])
+    except Exception as e:
         print(e)
