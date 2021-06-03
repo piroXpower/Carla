9681d856aa81ce9cb680bb71db3b2f58aa81ec3a
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-03 14:17:42 +0530

    Update Inline.py

--- Evelyn/modules/Inline.py
+++ Evelyn/modules/Inline.py
@@ -1,6 +1,26 @@
 from Evelyn.events import Cquery
+from telethon import events, Button
 
 
-@Cquery(pattern="cq")
+@Cquery(pattern="cq ?(.*)")
 async def cq(event):
-    await event.answer("Hello")
+    builder = event.builder
+    query = event.pattern_match.group(1)
+    if query == '' or len(query) > 4096:
+      title = "🔥 Write a whisper message"
+      content = ("**Send whisper messages through inline mode**\n\n"
+                   "Usage: `@ezWhisperBot [@username] text`")
+      description = "Usage: @ezWhisperBot [@username] text"
+      icon_url = "https://www.freeiconspng.com/uploads/whisper-icon-0.png"
+      switch_pm_text = "ℹ️ Learn how to send whispers"
+      switch_pm_parameter = "learn"
+      result = builder.article(
+                title=title,
+                description=content,
+                text=description,
+                thumb=icon_url,
+                buttons=[
+                    [Button.switch_inline("Learn more", query="yt ", same_peer=True)],
+                ],
+            )
+      await event.answer([result])
