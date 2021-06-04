35f4e30201211b3be5aa53288a987ed79c1f01a0
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-04 17:28:13 +0530

    Update __main__.py

--- Evelyn/__main__.py
+++ Evelyn/__main__.py
@@ -1,7 +1,8 @@
 from sys import exit
+from pyrogram import Client
 
 import Evelyn.events  # pylint:disable=E0602
-from Evelyn import TOKEN, tbot
+from Evelyn import TOKEN, tbot, API_KEY, API_HASH
 
 try:
     tbot.start(bot_token=TOKEN)
@@ -9,10 +10,11 @@ except Exception:
     print("Token Invalid.")
     exit(1)
 
+sbot = Client("wbb", bot_token=TOKEN, api_id=API_KEY, api_hash=API_HASH)
 
 async def start_log():
     await tbot.send_message(-1001273171524, "**Bot Started!**")
-
+sbot.start()
 
 tbot.loop.run_until_complete(start_log())
 tbot.run_until_disconnected()
