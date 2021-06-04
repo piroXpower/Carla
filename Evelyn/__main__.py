0793f84f07ff305e08d2d3c3e1937c71c6b54ab6
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-06-04 11:58:48 +0000

    RoseLoverX: auto fixes

--- Evelyn/__main__.py
+++ Evelyn/__main__.py
@@ -1,8 +1,9 @@
 from sys import exit
+
 from pyrogram import Client
 
 import Evelyn.events  # pylint:disable=E0602
-from Evelyn import TOKEN, tbot, API_KEY, API_HASH
+from Evelyn import API_HASH, API_KEY, TOKEN, tbot
 
 try:
     tbot.start(bot_token=TOKEN)
@@ -12,8 +13,11 @@ except Exception:
 
 sbot = Client("wbb", bot_token=TOKEN, api_id=API_KEY, api_hash=API_HASH)
 
+
 async def start_log():
     await tbot.send_message(-1001273171524, "**Bot Started!**")
+
+
 sbot.start()
 
 tbot.loop.run_until_complete(start_log())
