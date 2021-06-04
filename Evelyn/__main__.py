fcebc0d68bbdc7879aaf2df92a2fa706fbb9c48f
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-06-04 12:30:41 +0000

    RoseLoverX: auto fixes

--- Evelyn/__main__.py
+++ Evelyn/__main__.py
@@ -9,8 +9,10 @@ except Exception:
     print("Token Invalid.")
     exit(1)
 
+
 async def start_log():
     await tbot.send_message(-1001273171524, "**Bot Re-Started!**")
+
 
 tbot.loop.run_until_complete(start_log())
 tbot.run_until_disconnected()
