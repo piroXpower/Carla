468b65914e32ae3ede62db6ea8857c877d30046c
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-06-05 11:07:16 +0000

    RoseLoverX: auto fixes

--- Evelyn/modules/sql/locks_sql.py
+++ Evelyn/modules/sql/locks_sql.py
@@ -1,54 +1,101 @@
-from . import SESSION, BASE
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Boolean, Column, String
+
+from . import BASE, SESSION
+
 
 class LOCKS(BASE):
- __tablename__: "locks"
- chat_id = Column(String(14), primary_key=True)
- media = Column(Boolean, default=False)
- audio = Column(Boolean, default=False)
- video = Column(Boolean, default=False)
- sticker = Column(Boolean, default=False)
- gif = Column(Boolean, default=False)
- inline = Column(Boolean, default=False)
- all = Column(Boolean, default=False)
- emojigame = Column(Boolean, default=False)
- phone = Column(Boolean, default=False)
- photo = Column(Boolean, default=False)
- url = Column(Boolean, default=False)
- location = Column(Boolean, default=False)
- invitelink = Column(Boolean, default=False)
- forward = Column(Boolean, default=False)
- document = Column(Boolean, default=False)
- contact = Column(Boolean, default=False)
- command  = Column(Boolean, default=False)
- button = Column(Boolean, default=False)
- bot = Column(Boolean, default=False)
+    __tablename__: "locks"
+    chat_id = Column(String(14), primary_key=True)
+    media = Column(Boolean, default=False)
+    audio = Column(Boolean, default=False)
+    video = Column(Boolean, default=False)
+    sticker = Column(Boolean, default=False)
+    gif = Column(Boolean, default=False)
+    inline = Column(Boolean, default=False)
+    all = Column(Boolean, default=False)
+    emojigame = Column(Boolean, default=False)
+    phone = Column(Boolean, default=False)
+    photo = Column(Boolean, default=False)
+    url = Column(Boolean, default=False)
+    location = Column(Boolean, default=False)
+    invitelink = Column(Boolean, default=False)
+    forward = Column(Boolean, default=False)
+    document = Column(Boolean, default=False)
+    contact = Column(Boolean, default=False)
+    command = Column(Boolean, default=False)
+    button = Column(Boolean, default=False)
+    bot = Column(Boolean, default=False)
 
- def __init__(self, chat_id, media, audio, video, sticker, gif, inline, all, emojigame, phone, photo, url, location, invitelink, forward, document, contact, command, button, bot):
-   self.chat_id = chat_id
-   self.media = media
-   self.audio = audio
-   self.video = video
-   self.sticker = sticker
-   self.gif = gif
-   self.inline = inline
-   self.all = all
-   self.emojigame = emojigame
-   self.phone = phone
-   self.photo = photo
-   self.url = url
-   self.location = location
-   self.invitelink = invitelink
-   self.forward = forward
-   self.document = document
-   self.contact = contact
-   self.command = command
-   self.button = button
-   self.bot = bot
- 
+    def __init__(
+        self,
+        chat_id,
+        media,
+        audio,
+        video,
+        sticker,
+        gif,
+        inline,
+        all,
+        emojigame,
+        phone,
+        photo,
+        url,
+        location,
+        invitelink,
+        forward,
+        document,
+        contact,
+        command,
+        button,
+        bot,
+    ):
+        self.chat_id = chat_id
+        self.media = media
+        self.audio = audio
+        self.video = video
+        self.sticker = sticker
+        self.gif = gif
+        self.inline = inline
+        self.all = all
+        self.emojigame = emojigame
+        self.phone = phone
+        self.photo = photo
+        self.url = url
+        self.location = location
+        self.invitelink = invitelink
+        self.forward = forward
+        self.document = document
+        self.contact = contact
+        self.command = command
+        self.button = button
+        self.bot = bot
+
+
 LOCKS.__table__.create(checkfirst=True)
 
-def change_mode(chat_id, media=False, audio=False, video=False, sticker=False, gif=False, inline=False, all=False, emojigame=False, phone=False, photo=False, url=False, location=False, invitelink=False, forward=False, document=False, contact=False, command=False, button=False, bot=False):
-  curr = SESSION.query(LOCKS).get(str(chat_id))
-  if curr:
-    print(6)
+
+def change_mode(
+    chat_id,
+    media=False,
+    audio=False,
+    video=False,
+    sticker=False,
+    gif=False,
+    inline=False,
+    all=False,
+    emojigame=False,
+    phone=False,
+    photo=False,
+    url=False,
+    location=False,
+    invitelink=False,
+    forward=False,
+    document=False,
+    contact=False,
+    command=False,
+    button=False,
+    bot=False,
+):
+    curr = SESSION.query(LOCKS).get(str(chat_id))
+    if curr:
+        print(6)
