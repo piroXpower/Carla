b332a714a3cb25ee20350d61dd282d8fae52956c
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-05 18:11:25 +0530

    Update Locks.py

--- Evelyn/modules/Locks.py
+++ Evelyn/modules/Locks.py
@@ -1,5 +1,6 @@
 # soon
 from Evelyn.events import Cbot
+from Evelyn.modules.sql.locks_sql import get_chat_locks
 
 
 @Cbot(pattern="^/locktypes")
@@ -29,6 +30,40 @@ The available locktypes are:
 - voice
 """
     await event.reply(text)
+
+ok_locks = """
+These are the current lock settings:
+- all = {}
+- album =false
+- audio = {}
+- bot = {}
+- button = {}
+- command = {}
+- contact = {}
+- document = {}
+- email = false
+- emojigame = {}
+- forward = {}
+- game = false
+- gif = {}
+- inline = {}
+- invitelink = {}
+- location = {}
+- phone = {}
+- photo = {}
+- poll = false
+- sticker = {}
+- text =false
+- url = {}
+- video = {}
+- videonote = false
+- voice = false
+"""
+@Cbot(pattern="^/locks")
+async def locks(event):
+ c = get_chat_locks(event.chat_id)
+ final_y = ok_locks.format(c.all, c.audio, c.bot, c.button, c.command, c.contact, c.document, c.emojigame, c.forward, c.gif, c.inline, c.invitelink, c.location, c.phone, c.photo, c.sticker, c.url, c.video)
+ await event.reply(final_y)
 
 
 async def delete_locked(event, locks=[]):
