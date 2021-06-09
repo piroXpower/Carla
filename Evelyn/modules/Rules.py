57cb8e7af17e91b8052eda5aa31850147b16f036
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-06-09 16:57:54 +0000

    RoseLoverX: auto fixes

--- Evelyn/modules/Rules.py
+++ Evelyn/modules/Rules.py
@@ -1,31 +1,36 @@
-from Evelyn import tbot
-import Evelyn.modules.sql.rules_sql as SQL
 from Evelyn.events import Cbot
 
 from . import can_change_info
+
 pos = ["on", "yes", "u"]
 neg = ["off", "no", "n"]
 
+
 @Cbot(pattern="^/privaterules ?(.*)")
 async def pr(event):
- if event.is_private:
-  return
- if not await can_change_info(event, event.sender_id):
-    return
- args = event.pattern_match.group(1)
- if not args:
-  mode = sql.get_private(event.chat_id)
-  if mode:
-    await event.reply("Use of /rules will send the rules to the user's PM.")
-  else:
-    await event.reply(f"All /rules commands will send the rules to {event.chat.title}.")
- elif args in pos:
-   await event.reply("Use of /rules will send the rules to the user's PM.")
-   sql.set_private_rules(event.chat_id, True)
- elif args in neg:
-   await event.reply(f"All /rules commands will send the rules to {event.chat.title}.")
-   sql.set_private_rules(event.chat_id, False)
- else:
-   await event.reply("I only understand the following: yes/no/on/off")
+    if event.is_private:
+        return
+    if not await can_change_info(event, event.sender_id):
+        return
+    args = event.pattern_match.group(1)
+    if not args:
+        mode = sql.get_private(event.chat_id)
+        if mode:
+            await event.reply("Use of /rules will send the rules to the user's PM.")
+        else:
+            await event.reply(
+                f"All /rules commands will send the rules to {event.chat.title}."
+            )
+    elif args in pos:
+        await event.reply("Use of /rules will send the rules to the user's PM.")
+        sql.set_private_rules(event.chat_id, True)
+    elif args in neg:
+        await event.reply(
+            f"All /rules commands will send the rules to {event.chat.title}."
+        )
+        sql.set_private_rules(event.chat_id, False)
+    else:
+        await event.reply("I only understand the following: yes/no/on/off")
+
 
 # soon
