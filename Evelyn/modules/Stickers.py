429b1abff9b6cf2a7b438b5ac1f1779bd24c9a6d
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-06-10 14:17:52 +0000

    RoseLoverX: auto fixes

--- Evelyn/modules/Stickers.py
+++ Evelyn/modules/Stickers.py
@@ -22,60 +22,99 @@ sticker_sets = db.sticker_sets
 
 @Cbot(pattern="^/(kang|kamg) ?(.*)")
 async def kang(event):
- try:
-    if not event.reply_to_msg_id:
-        return await event.reply("Please reply to a sticker, or image to kang it!")
-    msg = await event.get_reply_message()
-    if not msg.sticker and not isinstance(event.media, MessageMediaPhoto):
-        return await event.reply("Yeah, I can't kang that.")
     try:
-        emoji = msg.media.document.attributes[1].alt
-    except:
-        emoji = "😂"
-    if event.pattern_match.group(2):
-        emoji = event.pattern_match.group(2)[0]
-    if msg.sticker:
-      mime_type = msg.media.document.mime_type
-      if "application/x-tgsticker" in mime_type:
-        animated = True
-      else:
-        animated = False
-      sticker_id_id = msg.media.document.id
-      access_hash_id = msg.media.document.access_hash
-      file_reference = msg.media.document.file_reference
-      if animated:
-         return
-    elif msg.media.photo:
-         ok = msg.media.photo
-         sticker_id_id = ok.id
-         access_hash_id = ok.access_hash
-         file_reference = ok.file_reference
-    short_name = f"e{event.sender_id}_by_MissCarla_Bot"
-    user_id = OWNER_ID
-    if event.sender.first_name:
-        title = f"{event.sender.first_name}'s Kang pack"
-    else:
-        title = f"{event.sender_id}'s Kang pack"
-    if str((sticker_sets.find({"id": event.sender_id})).distinct("sticker_id")) == "[]":
+        if not event.reply_to_msg_id:
+            return await event.reply("Please reply to a sticker, or image to kang it!")
+        msg = await event.get_reply_message()
+        if not msg.sticker and not isinstance(event.media, MessageMediaPhoto):
+            return await event.reply("Yeah, I can't kang that.")
+        try:
+            emoji = msg.media.document.attributes[1].alt
+        except:
+            emoji = "😂"
+        if event.pattern_match.group(2):
+            emoji = event.pattern_match.group(2)[0]
+        if msg.sticker:
+            mime_type = msg.media.document.mime_type
+            if "application/x-tgsticker" in mime_type:
+                animated = True
+            else:
+                animated = False
+            sticker_id_id = msg.media.document.id
+            access_hash_id = msg.media.document.access_hash
+            file_reference = msg.media.document.file_reference
+            if animated:
+                return
+        elif msg.media.photo:
+            ok = msg.media.photo
+            sticker_id_id = ok.id
+            access_hash_id = ok.access_hash
+            file_reference = ok.file_reference
+        short_name = f"e{event.sender_id}_by_MissCarla_Bot"
+        user_id = OWNER_ID
+        if event.sender.first_name:
+            title = f"{event.sender.first_name}'s Kang pack"
+        else:
+            title = f"{event.sender_id}'s Kang pack"
+        if (
+            str((sticker_sets.find({"id": event.sender_id})).distinct("sticker_id"))
+            == "[]"
+        ):
+            try:
+                result = await tbot(
+                    CreateStickerSetRequest(
+                        user_id=user_id,
+                        title=title,
+                        short_name=short_name,
+                        stickers=[
+                            InputStickerSetItem(
+                                document=InputDocument(
+                                    id=sticker_id_id,
+                                    access_hash=access_hash_id,
+                                    file_reference=file_reference,
+                                ),
+                                emoji=emoji,
+                                mask_coords=MaskCoords(n=42, x=7.13, y=7.13, zoom=7.13),
+                            )
+                        ],
+                        masks=False,
+                        animated=False,
+                    )
+                )
+            except StickerPngNopngError:
+                return
+            except StickerEmojiInvalidError:
+                return await event.reply(
+                    f"Invalid emoji provided, '{event.pattern_match.group(1)}' is not an emoji."
+                )
+            except Exception as e:
+                return await event.reply(str(e))
+            txt = f"Sticker successfully added to <a href='http://t.me/addstickers/{short_name}'>pack</a>\nEmoji is: {emoji}"
+            await event.reply(txt, parse_mode="html", link_preview=False)
+            return sticker_sets.insert_one(
+                {
+                    "id": event.sender_id,
+                    "sticker_id": result.set.id,
+                    "access_hash": result.set.access_hash,
+                }
+            )
+        user_st = sticker_sets.find({"id": event.sender_id})
+        sticker_id = user_st.distinct("sticker_id")[0]
+        access_hash = user_st.distinct("access_hash")[0]
         try:
             result = await tbot(
-                CreateStickerSetRequest(
-                    user_id=user_id,
-                    title=title,
-                    short_name=short_name,
-                    stickers=[
-                        InputStickerSetItem(
-                            document=InputDocument(
-                                id=sticker_id_id,
-                                access_hash=access_hash_id,
-                                file_reference=file_reference,
-                            ),
-                            emoji=emoji,
-                            mask_coords=MaskCoords(n=42, x=7.13, y=7.13, zoom=7.13),
-                        )
-                    ],
-                    masks=False,
-                    animated=False,
+                AddStickerToSetRequest(
+                    stickerset=InputStickerSetID(
+                        id=sticker_id, access_hash=access_hash
+                    ),
+                    sticker=InputStickerSetItem(
+                        document=InputDocument(
+                            id=sticker_id_id,
+                            access_hash=access_hash_id,
+                            file_reference=file_reference,
+                        ),
+                        emoji=emoji,
+                    ),
                 )
             )
         except StickerPngNopngError:
@@ -85,45 +124,11 @@ async def kang(event):
                 f"Invalid emoji provided, '{event.pattern_match.group(1)}' is not an emoji."
             )
         except Exception as e:
-            return await event.reply(str(e))
-        txt = f"Sticker successfully added to <a href='http://t.me/addstickers/{short_name}'>pack</a>\nEmoji is: {emoji}"
+            return await event.respond(str(e))
+        txt = f"Sticker successfully added to <a href='http://t.me/addstickers/{result.set.short_name}'>pack</a>\nEmoji is: {emoji}"
         await event.reply(txt, parse_mode="html", link_preview=False)
-        return sticker_sets.insert_one(
-            {
-                "id": event.sender_id,
-                "sticker_id": result.set.id,
-                "access_hash": result.set.access_hash,
-            }
-        )
-    user_st = sticker_sets.find({"id": event.sender_id})
-    sticker_id = user_st.distinct("sticker_id")[0]
-    access_hash = user_st.distinct("access_hash")[0]
-    try:
-        result = await tbot(
-            AddStickerToSetRequest(
-                stickerset=InputStickerSetID(id=sticker_id, access_hash=access_hash),
-                sticker=InputStickerSetItem(
-                    document=InputDocument(
-                        id=sticker_id_id,
-                        access_hash=access_hash_id,
-                        file_reference=file_reference,
-                    ),
-                    emoji=emoji,
-                ),
-            )
-        )
-    except StickerPngNopngError:
-        return
-    except StickerEmojiInvalidError:
-        return await event.reply(
-            f"Invalid emoji provided, '{event.pattern_match.group(1)}' is not an emoji."
-        )
     except Exception as e:
-        return await event.respond(str(e))
-    txt = f"Sticker successfully added to <a href='http://t.me/addstickers/{result.set.short_name}'>pack</a>\nEmoji is: {emoji}"
-    await event.reply(txt, parse_mode="html", link_preview=False)
- except Exception as e:
-   await event.reply(str(e))
+        await event.reply(str(e))
 
 
 # work on animated sticker
