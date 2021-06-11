a53e86fcb44f6c8feb7690cb52efb5eceefd96a6
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-11 17:46:43 +0530

    Create Song.py

---
+++ Evelyn/modules/Song.py
@@ -0,0 +1,15 @@
+from Evelyn import tbot, ubot
+from Evelyn.events import Cbot
+
+@Cbot(pattern="^/music ?(.*)")
+async def music(event):
+ music = event.pattern_math.group(1)
+ if not music:
+   return
+ async with ubot.conversation("@EvelynMusic_Bot) as conv:
+   await conv.send_message(music)
+   res = await conv.get_response()
+   if "Sorry" in res.raw_text:
+      return await event.reply("Failed to find the song!")
+   await res.click(0)
+   muzic = await conv.get_response()
