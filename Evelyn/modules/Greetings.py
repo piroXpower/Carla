71b03cf97d74cab4ef765de1f5ad5e8aebc13ee6
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-12 14:44:31 +0530

    Update Greetings.py

--- Evelyn/modules/Greetings.py
+++ Evelyn/modules/Greetings.py
@@ -195,9 +195,6 @@ async def kek(event):
                 event.channel_id, f"Hey **{first_name}**, How are you."
             )
         custom_welcome = cws.custom_welcome_message
-        count = 0
-        if "{count}" in custom_welcome:
-            count = len(await tbot.get_participants(event.channel_id))
         if cas.get_mode(channel_id) == True:
             chat_info = channel_id
             if channel.username:
@@ -218,7 +215,6 @@ async def kek(event):
             full_name=full_name,
             title=title,
             id=user_id,
-            count=count,
         )
         if cas.get_mode(channel_id) == True:
             if not bot:
