65a84cf3ac33c6b2cd25106b43b63f9171c77c5d
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-13 23:12:08 +0530

    Update Reporting.py

--- Evelyn/modules/Reporting.py
+++ Evelyn/modules/Reporting.py
@@ -4,7 +4,7 @@ from Evelyn import OWNER_ID, tbot
 from Evelyn.events import Cbot
 from Evelyn.modules.sql import reporting_sql as sql
 
-from . import ELITES, can_change_info, is_admin
+from . import ELITES, can_change_info, is_admin, get_user
 
 Ron = """
 Reports are currently enabled in this chat.
@@ -23,10 +23,9 @@ To change this setting, try this command again, with one of the following args: 
 async def _(event):
     if event.is_private:
         return  # connect
-    if not event.sender_id == OWNER_ID or event.sender_id in ELITES:
-        k = await can_change_info(event, event.sender_id)
-        if not k:
-            return
+    if event.from_id:
+       if not await can_change_info(event, evebt.sender_id):
+          return
     args = event.pattern_match.group(1)
     chat = event.chat_id
     if args:
@@ -56,33 +55,19 @@ async def _(event):
         return
     if await is_admin(event.chat_id, event.sender_id):
         return
-    if event.reply_to_msg_id:
-        msg = await event.get_reply_message()
-        id = msg.sender_id
-        if await is_admin(event.chat_id, id):
-            return
-        name = msg.sender.first_name
-    elif event.pattern_match.group(1):
-        args = event.pattern_match.group(1)
-        args = args.split()
-        user = args[0]
-        try:
-            user = await tbot.get_entity(user)
-        except:
-            return await event.reply("Reported to admins.​")
-        id = user.id
-        if await is_admin(event.chat_id, user.id):
-            return
-        name = user.first_name
-        if len(args) == 2:
-            args[1]
-    else:
-        return await event.reply("Reported to admins.​")
-    text = f'Reported <a href="tg://user?id={id}">{name}</a> to admins.'
-    for users in await tbot.get_participants(chat_id, filter=ChannelParticipantsAdmins):
-        text += f'<a href="tg://user?id={users.id}">&#8203;</a>'
-    await event.reply(text, parse_mode="html")
-
+    user = None
+    try:
+     user, extra = await get_user(event)
+    except TypeError:
+     pass
+    if not user:
+      return
+    if await is_admin(event.chat_id, event.sender_id):
+      return
+    print("soon")
+# soon
+# afk
+# gn
 
 @Cbot(pattern="^@admin ?(.*)")
 async def I(event):
