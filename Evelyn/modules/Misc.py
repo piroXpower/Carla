5b45f5f96a028332be41988dd86420335b781e47
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-15 13:25:03 +0530

    Update Misc.py

--- Evelyn/modules/Misc.py
+++ Evelyn/modules/Misc.py
@@ -813,3 +813,12 @@ async def st(event):
         await event.reply(file=file)
     os.remove(f)
     os.remove(file)
+
+slap_strings = (
+ "{name_u} pokes {name_r} with a pen!",
+ "{name_r} was struck by lightning.",
+ "{name_u} best is what's for dinner!",
+)
+@Cbot(pattern="^/slap ?(.*)")
+async def slap(event):
+ r = random.choice(slap_strings)
