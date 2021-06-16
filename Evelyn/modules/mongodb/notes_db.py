952a46bf011afbdd9824781a28cfec002cc87fdb
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-06-16 12:28:12 +0000

    RoseLoverX: auto fixes

--- Evelyn/modules/mongodb/notes_db.py
+++ Evelyn/modules/mongodb/notes_db.py
@@ -2,14 +2,12 @@ from Evelyn.modules import db
 
 notes = db.notes
 
-def save_note(chat_id, name, note):
- _note = notes.find_one({"chat_id": chat_id})
- if not _note:
-   _notes = {}
- else:
-  _notes = _note["notes"]
- _notes[name] = note
- notes.update_one({"chat_id": chat_id}, {"$set": {"notes": _notes}}, upsert=True)
 
- 
- 
+def save_note(chat_id, name, note):
+    _note = notes.find_one({"chat_id": chat_id})
+    if not _note:
+        _notes = {}
+    else:
+        _notes = _note["notes"]
+    _notes[name] = note
+    notes.update_one({"chat_id": chat_id}, {"$set": {"notes": _notes}}, upsert=True)
