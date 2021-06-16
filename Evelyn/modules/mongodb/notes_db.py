57ce508ee87bc4da740800c1e9a64bd66c4e8389
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-06-16 12:58:07 +0000

    RoseLoverX: auto fixes

--- Evelyn/modules/mongodb/notes_db.py
+++ Evelyn/modules/mongodb/notes_db.py
@@ -25,28 +25,29 @@ def delete_note(chat_id, name):
         del _notes[name]
         notes.update_one({"chat_id": chat_id}, {"$set": {"notes": _notes}}, upsert=True)
 
+
 def get_note(chat_id, name):
- name = name.strip().lower()
- _note = notes.find_one({"chat_id": chat_id})
- if not _note:
+    name = name.strip().lower()
+    _note = notes.find_one({"chat_id": chat_id})
+    if not _note:
         _notes = {}
- else:
+    else:
         _notes = _note["notes"]
- if name in _notes:
-   return _notes[name]
- return False
+    if name in _notes:
+        return _notes[name]
+    return False
+
 
 def get_all_notes(chat_id):
- _note = notes.find_one({"chat_id": chat_id})
- if note:
-   return _note["notes"]
- return None
+    _note = notes.find_one({"chat_id": chat_id})
+    if note:
+        return _note["notes"]
+    return None
+
 
 def delete_all_notes(chat_id):
- _note = notes.find_one({"chat_id": chat_id})
- if note:
-    notes.delete_one({"chat_id": chat_id})
-    return True
- return False
-
-
+    _note = notes.find_one({"chat_id": chat_id})
+    if note:
+        notes.delete_one({"chat_id": chat_id})
+        return True
+    return False
