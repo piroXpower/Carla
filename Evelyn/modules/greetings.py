9ddedfd3bebdf414f5a4a9da0e5966c93c5974e6
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-17 13:18:31 +0530

    Update and rename greetings to greetings.py

--- Evelyn/modules/greetings.py
+++ Evelyn/modules/greetings.py
@@ -46,3 +46,40 @@ async def set_welxome(event):
         r_text = event.text.split(None, 1)[1]
     await event.reply("The new welcome message has been saved!")
     db.set_welcome(event.chat_id, r_text, id, hash, ref, type, True)
+
+@Cbot(pattern="^/resetwelcome")
+async def rw(event):
+ if event.is_private:
+        return await event.reply("This command is made to used in group chats!")
+ if not event.from_id:
+      return await a_welcome(event, 'resetwelcome')
+ if event.is_group:
+        if not await can_change_info(event, event.sender_id):
+            return
+ await event.reply("The welcome message has been reset to default!")
+ db.reset_welcome(event.chat_id)
+
+w_str = """
+I am currently welcoming users: {}
+I am currently deleting old welcomes: {}
+I am currently deleting service messages: {}
+CAPTCHAs are {}.
+Welcome message:
+"""
+
+@Cbot(pattern="^/welcome ?(.*)")
+async def welfome(event):
+ if event.is_private:
+        return await event.reply("This command is made to used in group chats!")
+ if not event.from_id:
+      return await a_welcome(event, 'welcome')
+ if event.is_group:
+        if not await can_change_info(event, event.sender_id):
+            return
+ settings = event.pattern_match.group(1)
+ if not settings:
+   chat_s = db.get_welcome(event.chat_id)
+   if chat_s:
+     await event.reply(str(chat_s))
+ 
+
