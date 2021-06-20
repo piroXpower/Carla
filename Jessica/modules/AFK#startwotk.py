5ace717f789f184dec85ad707d800e259ff41b00
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-06-20 10:49:50 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/AFK#startwotk.py
+++ Jessica/modules/AFK#startwotk.py
@@ -1,30 +1,32 @@
-from Jessica.modules.sql import afk_sql as sql
-from Jessica.events import Cbot
 import random
 
+from Jessica.events import Cbot
+from Jessica.modules.sql import afk_sql as sql
+
 options = [
-                "{} is here!",
-                "{} is back!",
-                "{} is now in the chat!",
-                "{} is awake!",
-                "{} is back online!",
-                "{} is finally here!",
-                "Welcome back! {}",
-                "Where is {}?\nIn the chat!",
-                "Pro {}, is back alive!",
-            ]
+    "{} is here!",
+    "{} is back!",
+    "{} is now in the chat!",
+    "{} is awake!",
+    "{} is back online!",
+    "{} is finally here!",
+    "Welcome back! {}",
+    "Where is {}?\nIn the chat!",
+    "Pro {}, is back alive!",
+]
+
 
 @Cbot(pattern=r"(.*?)")
 async def afk(e):
- if sql.is_afk(sender.id):
-   sql.rm_afk(sender.id)
-   return await event.reply((random.choice(options)).format(event.sender.first_name))
- for x in [".afk", "/afk", "!afk", "?afk" "brb"]:
-  if (event.text.lower()).startswith(x):
-    reason = event.text.split(None, 1)[1]
-    fname = event.sender.first_name
-    user_id = event.sender_id
-    sql.set_afk(user_id, reason, fname)
-    await event.reply(
-           "<b>{}</b> is now AFK !".format(fname),
-           parse_mode="html")
+    if sql.is_afk(sender.id):
+        sql.rm_afk(sender.id)
+        return await event.reply(
+            (random.choice(options)).format(event.sender.first_name)
+        )
+    for x in [".afk", "/afk", "!afk", "?afk" "brb"]:
+        if (event.text.lower()).startswith(x):
+            reason = event.text.split(None, 1)[1]
+            fname = event.sender.first_name
+            user_id = event.sender_id
+            sql.set_afk(user_id, reason, fname)
+            await event.reply("<b>{}</b> is now AFK !".format(fname), parse_mode="html")
