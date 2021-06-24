0217b2f5b6a7107ac8e43d6aae74c90f1262bdca
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-24 13:33:24 +0530

    Create filters_db.py

---
+++ Jessica/modules/mongodb/filters_db.py
@@ -0,0 +1,58 @@
+from Jessica.modules import db
+
+filters = db.filter_s
+
+def save_filter(chat_id, name, reply, id=None, hash=None, reference=None, type=None):
+    name = name.lower().strip()
+    _filters = filters.find_one({"chat_id": chat_id})
+    if not _filters:
+        _filter = {}
+    else:
+        _filter = _filters["filters"]
+    _filter[name] = {
+        "reply": reply,
+        "id": id,
+        "hash": hash,
+        "ref": reference,
+        "mtype": type,
+    }
+    filters.update_one({"chat_id": chat_id}, {"$set": {"filters": _filters}}, upsert=True)
+
+
+def delete_filter(chat_id, name):
+    name = name.strip().lower()
+    _filters = filters.find_one({"chat_id": chat_id})
+    if not _filters:
+        _filter = {}
+    else:
+        _filter = _filters["filters"]
+    if name in _filter:
+        del _filter[name]
+        filters.update_one({"chat_id": chat_id}, {"$set": {"filters": _filter}}, upsert=True)
+
+
+def get_filter(chat_id, name):
+    name = name.strip().lower()
+    _filters = filters.find_one({"chat_id": chat_id})
+    if not _filters:
+        _filter = {}
+    else:
+        _filter = _filters["filters"]
+    if name in _filter:
+        return _filter[name]
+    return False
+
+
+def get_all_filters(chat_id):
+    _filters = filtes.find_one({"chat_id": chat_id})
+    if _filters:
+        return _filters["filters"]
+    return None
+
+
+def delete_all_filters(chat_id):
+    _filters = filters.find_one({"chat_id": chat_id})
+    if _filters:
+        filters.delete_one({"chat_id": chat_id})
+        return True
+    return False
