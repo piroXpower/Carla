26d1e37b4b5a7d428acfb545ae5ab46824b0c772
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-06-24 13:02:08 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/mongodb/locks_db.py
+++ Jessica/modules/mongodb/locks_db.py
@@ -1,18 +1,19 @@
-from Jessica.modules import db
 locks = locks.db
 
+
 def add_lock(chat_id, type):
- _locks = locks.find_one({"chat_id": chat_id})
- if _locks:
-   _lock = _locks["locked"]
- else:
-   _lock = []
- _lock.append(type)
- new_lock = list(set(_lock))
- locks.update_one({"chat_id": chat_id}, {"$set": {"locked": new_lock}}, upsert=True)
- 
+    _locks = locks.find_one({"chat_id": chat_id})
+    if _locks:
+        _lock = _locks["locked"]
+    else:
+        _lock = []
+    _lock.append(type)
+    new_lock = list(set(_lock))
+    locks.update_one({"chat_id": chat_id}, {"$set": {"locked": new_lock}}, upsert=True)
+
+
 def get_locks(chat_id):
- _locks = locks.find_one({"chat_id": chat_id})
- if _locks:
-   return _locks["locked"]
- return None
+    _locks = locks.find_one({"chat_id": chat_id})
+    if _locks:
+        return _locks["locked"]
+    return None
