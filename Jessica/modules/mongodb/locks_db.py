97d43da6cec6485e8ffc35e8a26b9c5d3d1a7046
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-24 18:57:28 +0530

    Update locks_db.py

--- Jessica/modules/mongodb/locks_db.py
+++ Jessica/modules/mongodb/locks_db.py
@@ -19,3 +19,43 @@ def get_locks(chat_id):
     if _locks:
         return _locks["locked"]
     return None
+
+def remove_lock(chat_id, type):
+ _locks = locks.find_one({"chat_id": chat_id})
+ if _locks:
+        _lock = _locks["locked"]
+ else:
+        return
+ if type in _lock:
+   _lock.remove(type)
+   locks.update_one({"chat_id": chat_id}, {"$set": {"locked": _lock}}, upsert=True)
+
+all_locks = [media,
+            audio,
+            video,
+            sticker,
+            gif,
+            inline,
+            all,
+            emojigame,
+            phone,
+            photo,
+            url,
+            location,
+            invitelink,
+            forward,
+            document,
+            contact,
+            command,
+            button,
+            bot,
+            email,
+            game,
+            text,
+            voice,
+            videonote,
+            poll,
+        ]
+
+def lock_all(chat_id):
+ locks.update_one({"chat_id": chat_id}, {"$set": {"locked": all_locks}}, upsert=True)
