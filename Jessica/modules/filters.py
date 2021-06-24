5b885a1a0b8f531cd2cecd6e578af17e7ab95415
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-06-24 08:26:36 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/filters.py
+++ Jessica/modules/filters.py
@@ -1,8 +1,10 @@
-from telethon import events, Button, types
-from Jessica import tbot
-from Jessica.events import Cbot, Cinline
+from telethon import types
+
 import Jessica.modules.mongodb.filters_db as db
-from . import can_change_info, get_reply_msg_btns_text, button_parser
+from Jessica.events import Cbot
+
+from . import can_change_info, get_reply_msg_btns_text
+
 
 def file_ids(msg):
     if isinstance(msg.media, types.MessageMediaDocument):
@@ -24,43 +26,47 @@ def file_ids(msg):
         return None, None, None, None
     return file_id, access_hash, file_reference, type
 
+
 file_id = file_reference = access_hash = type = None
+
 
 @Cbot(pattern="^/filter ?(.*)")
 async def add_filter(event):
- if (
+    if (
         event.text.startswith("!filters")
         or event.text.startswith("/filters")
         or event.text.startswith("?filters")
         or event.text.startswith(".filters")
     ):
         return
- if event.from_id:
-    if not isinstance(event.from_id, types.PeerUser):
-      return
- if event.is_group and event.from_id:
-     if not await can_change_info(event, event.sender_id):
-        return
- if not event.reply_to and not event.pattern_match.group(1):
-   return await event.reply("You need to give the filter a name!")
- elif event.reply_to:
-   name = event.pattern_match.group(1)
-   if not name:
-     return await event.reply("You need to give the filter a name!")
-   reply_msg = await event.get_reply_message()
-   if reply_msg.media:
-      file_id, access_hash, file_reference, type = file_ids(reply_msg)
-   if not r_msg.text and not r_msg.media:
-                return await event.reply("you need to give the filter some content!")
-   reply = reply_msg.text or "Nil"
-   if reply_msg.reply_markup:
-      reply = reply + get_reply_msg_btns_text(reply_msg)
- elif event.pattern_match.group(1):
-   _total = event.text.split(None, 1)[1]
-   _t = _total.split(None, 1)
-   if len(_t) == 1:
-     return await event.reply("You need to give the filter some content!")
-   name = _t[0]
-   reply = _t[1]
- await event.reply("Saved filter '{}'.".format(name))
- db.save_filter(event.chat_id, name, reply, file_id, access_hash, file_reference, type)
+    if event.from_id:
+        if not isinstance(event.from_id, types.PeerUser):
+            return
+    if event.is_group and event.from_id:
+        if not await can_change_info(event, event.sender_id):
+            return
+    if not event.reply_to and not event.pattern_match.group(1):
+        return await event.reply("You need to give the filter a name!")
+    elif event.reply_to:
+        name = event.pattern_match.group(1)
+        if not name:
+            return await event.reply("You need to give the filter a name!")
+        reply_msg = await event.get_reply_message()
+        if reply_msg.media:
+            file_id, access_hash, file_reference, type = file_ids(reply_msg)
+        if not r_msg.text and not r_msg.media:
+            return await event.reply("you need to give the filter some content!")
+        reply = reply_msg.text or "Nil"
+        if reply_msg.reply_markup:
+            reply = reply + get_reply_msg_btns_text(reply_msg)
+    elif event.pattern_match.group(1):
+        _total = event.text.split(None, 1)[1]
+        _t = _total.split(None, 1)
+        if len(_t) == 1:
+            return await event.reply("You need to give the filter some content!")
+        name = _t[0]
+        reply = _t[1]
+    await event.reply("Saved filter '{}'.".format(name))
+    db.save_filter(
+        event.chat_id, name, reply, file_id, access_hash, file_reference, type
+    )
