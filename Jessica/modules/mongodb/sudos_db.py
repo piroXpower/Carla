297628f632c56ff9a54ef70821f2f7d7c20134e3
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-06-24 14:36:26 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/mongodb/sudos_db.py
+++ Jessica/modules/mongodb/sudos_db.py
@@ -2,24 +2,24 @@ from Jessica.modules import db
 
 sudo_m = db.sudo_m
 
+
 def add_sudo(user_id: int, name: str):
-  sudos = sudo_m.find_one({"type": "staffs"})
-  if sudos:
-     sudos = sudos["sudo"]
-     sudos[user_id] = name
-  else:
-     sudos = {}
-     sudos[user_id] = name
-  sudo_m.update_one({"type": "staffs"}, {"$set": {"sudo": devs}}, upsert=True)
- 
+    sudos = sudo_m.find_one({"type": "staffs"})
+    if sudos:
+        sudos = sudos["sudo"]
+        sudos[user_id] = name
+    else:
+        sudos = {}
+        sudos[user_id] = name
+    sudo_m.update_one({"type": "staffs"}, {"$set": {"sudo": devs}}, upsert=True)
+
 
 def add_dev(user_id: int, name: str):
- devs = sudo_m.find_one({"type": "staffs"})
- if devs:
-   devs = devs["dev"]
-   devs[user_id] = name
- else:
-   devs = {}
-   devs[user_id] = name
- sudo_m.update_one({"type": "staffs"}, {"$set": {"dev": devs}}, upsert=True)
- 
+    devs = sudo_m.find_one({"type": "staffs"})
+    if devs:
+        devs = devs["dev"]
+        devs[user_id] = name
+    else:
+        devs = {}
+        devs[user_id] = name
+    sudo_m.update_one({"type": "staffs"}, {"$set": {"dev": devs}}, upsert=True)
