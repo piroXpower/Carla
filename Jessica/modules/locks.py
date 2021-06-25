f029ddbd5797e0a00b6843adbe1d0bfcf1d1136a
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-25 15:55:35 +0530

    Update locks.py

--- Jessica/modules/locks.py
+++ Jessica/modules/locks.py
@@ -1,7 +1,9 @@
 import Jessica.modules.mongodb.locks_db as db
 from Jessica.events import Cbot
 
-from . import can_change_info
+from . import can_change_info, is_admin
+from telethon import events
+from Jessica import tbot
 
 
 @Cbot(pattern="^/lock ?(.*)")
@@ -145,3 +147,123 @@ async def unlock_item(event):
             await tbot.edit_permissions(event.chat_id, send_inline=True)
         except:
             pass
+
+@tbot.on(events.NewMessage())
+async def locks(event):
+ if not event.chat.admin_rights.delete_messages:
+        return
+ locked = db.get_locks
+ if len(locked) == 0:
+    return
+ trigg = await lock_check(event, locked)
+ print(trigg)
+
+async def lock_check(event, locked):
+    trigg = False
+    if "sticker" in locked:
+        if event.sticker:
+            trigg = True
+    if "gif" in locked:
+        if event.gif:
+            trigg = True
+    if "document" in locked:
+        if event.media:
+            if isinstance(event.media, MessageMediaDocument):
+                if not event.media.document.mime_type in [
+                    "image/webp",
+                    "application/x-tgsticker",
+                    "image/jpeg",
+                    "audio/ogg",
+                    "audio/m4a",
+                    "audio/mp3",
+                    "video/mp4",
+                ]:
+                    trigg = True
+    if "location" in locked:
+        if event.media:
+            if isinstance(event.media, MessageMediaGeo):
+                trigg = True
+    if "phone" in locked:
+        if event.message.entities:
+            if isinstance(event.message.entities[0], MessageEntityPhone):
+                trigg = True
+    if "email" in locked:
+        if event.message.entities:
+            if isinstance(event.message.entities[0], MessageEntityEmail):
+                trigg = True
+    if "command" in locked:
+        if event.message.entities:
+            if isinstance(event.message.entities[0], MessageEntityBotCommand):
+                trigg = True
+    if "url" in locked:
+        if event.message.entities:
+            if isinstance(event.message.entities[0], MessageEntityUrl):
+                trigg = True
+    if "invitelink" in locked:
+        if event.text:
+            if "t.me/" in event.text:
+                trigg = True
+    if "poll" in locked:
+        if event.media:
+            if isinstance(event.media, MessageMediaPoll):
+                trigg = True
+    if "photo" in locked:
+        if event.media:
+            if isinstance(event.media, MessageMediaPhoto):
+                trigg = True
+    if "videonote" in locked:
+        if event.media:
+            if isinstance(event.media, MessageMediaDocument):
+                if event.media.document.mime_type == "video/mp4":
+                    trigg = True
+    if "video" in locked:
+        if event.media:
+            if isinstance(event.media, MessageMediaDocument):
+                if isinstance(
+                    event.media.document.attributes[0], DocumentAttributeVideo
+                ):
+                    trigg = True
+    if "voice" in locked:
+        if event.media:
+            if isinstance(event.media, MessageMediaDocument):
+                if isinstance(
+                    event.media.document.attributes[0], DocumentAttributeAudio
+                ):
+                    if event.media.document.attributes[0].voice:
+                        trigg = True
+    if "audio" in locked:
+        if event.media:
+            if isinstance(event.media, MessageMediaDocument):
+                if isinstance(
+                    event.media.document.attributes[0], DocumentAttributeAudio
+                ):
+                    trigg = True
+    if "bot" in locked:
+        if event.sender.bot:
+            trigg = True
+    if "button" in locked:
+        if event.reply_markup:
+            trigg = True
+    if "game" in locked:
+        if event.media:
+            if isinstance(event.media, MessageMediaGame):
+                trigg = True
+    if "contact" in locked:
+        if event.media:
+            if isinstance(event.media, MessageMediaContact):
+                trigg = True
+    if "forward" in locked:
+        if event.fwd_from:
+            trigg = True
+    if "emojigame" in locked:
+        if event.media:
+            if isinstance(event.media, MessageMediaDice):
+                trigg = True
+    if "forwardchannel" in locked:
+        if event.fwd_from:
+          if event.fwd_from.from_id:
+           if isinstance(event.fwd_from.from_id, PeerChannel):
+                trigg = True
+    return trigg
+    
+ 
