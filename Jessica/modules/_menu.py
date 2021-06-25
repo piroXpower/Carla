cf14e7d861bd0cfc012fb00afde9dbff9f891a6d
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-06-25 13:01:56 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/_menu.py
+++ Jessica/modules/_menu.py
@@ -1,5 +1,6 @@
-from Jessica.events import Cbot
 from telethon import Button
+
+from Jessica.events import Cbot
 
 from . import db
 
@@ -12,20 +13,21 @@ Hit /help to find out more about how to use me to my full potential.
 You can checkout more about me via following buttons.
 """
 
+
 @Cbot(pattern="^/start$")
 async def start(event):
     if event.is_group or event.is_channel:
         await event.reply("Hi there, I'm online ^_^")
     elif event.is_private:
-        buttons=[
-                [
-                    Button.inline("Advanced", data="soon"),
-                    Button.inline("Commands", data="help_menu"),
-                ],
-                  [
-                    Button.url(
-                        "Add Me To Your Group!", "t.me/missneko_bot?startgroup=true"
-                    ),
-                ],
-            ]
+        buttons = [
+            [
+                Button.inline("Advanced", data="soon"),
+                Button.inline("Commands", data="help_menu"),
+            ],
+            [
+                Button.url(
+                    "Add Me To Your Group!", "t.me/missneko_bot?startgroup=true"
+                ),
+            ],
+        ]
         await event.reply(pm_caption, buttons=buttons, file=dp_1)
