3aa2b0afac7c8cfc36bd4fa8dee0c463549ac079
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-25 15:01:21 +0530

    Update locks.py

--- Jessica/modules/locks.py
+++ Jessica/modules/locks.py
@@ -6,6 +6,17 @@ from . import can_change_info
 
 @Cbot(pattern="^/lock ?(.*)")
 async def lock_item(event):
+    if (
+        event.text.startswith(".locks")
+        or event.text.startswith("/locks")
+        or event.text.startswith("!locks")
+        or event.text.startswith("?locks")
+        or event.text.startswith(".locktypes")
+        or event.text.startswith("/locktypes")
+        or event.text.startswith("?locktypes")
+        or event.text.startswith("!locktypes")
+    ):
+        return
     if event.is_private:
         return await event.reply("This command is made to be used in group chats.")
     if event.is_group and event.from_id:
@@ -61,3 +72,18 @@ async def lock_types(event):
     for x in av_locks:
         main_txt = main_txt + "\n- " + x
     await event.replt(main_txt)
+
+@Cbot(pattern="^/locks")
+async def locks(event):
+ if not await can_change_info(event, event.sender_id):
+     return
+ av_locks = db.all_locks
+ _final = "These are the current lock settings:"
+ locked = db.get_locks(event.chat_id)
+ for x in av_locks:
+   _mode = "false"
+   if x in locked:
+     _mode = "true"
+   _final = _final + "\n- " + x  + " = " + _mode
+ await event.reply(_final)
+
