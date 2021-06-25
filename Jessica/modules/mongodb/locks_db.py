9f1aab5a07edca6b48ba0aea59126b5e76d4dba4
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-25 20:36:37 +0530

    Update locks_db.py

--- Jessica/modules/mongodb/locks_db.py
+++ Jessica/modules/mongodb/locks_db.py
@@ -4,9 +4,9 @@ locks = db.locks
 
 lock_1 = ["all", "audio", "media", "bot", "button"]
 lock_2 = ["command", "contact", "document", "email", "emojigame"]
-lock_3 = ["forward", "forwardchannel", "game", "gif", "inline", "invitelink"]
-lock_4 = ["location", "phone", "photo", "poll", "sticker", "preview"]
-lock_5 = ["text", "url", "video", "videonote", "voice", "forwarduser", "forwardbot"]
+lock_3 = ["forward", "forwardchannel", "forwardbot", "forwarduser", "game", "gif", "inline", "invitelink"]
+lock_4 = ["location", "phone", "photo", "poll", "preview", "sticker"]
+lock_5 = ["text", "url", "video", "videonote", "voice"]
 
 all_locks = lock_1 + lock_2 + lock_3 + lock_4 + lock_5
 
