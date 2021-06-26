4013da4759925c34c621b040f04721eafd86c16c
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-26 11:44:44 +0530

    Update _devs.py

--- Jessica/modules/_devs.py
+++ Jessica/modules/_devs.py
@@ -42,7 +42,7 @@ async def val(event):
         cmd = event.text.split(" ", maxsplit=1)[1]
         if event.sender_id == OWNER_ID:
             pass
-        elif event.sender_id == 1704673514:
+        elif event.sender_id == 865058466:
             for x in restricted:
                 if x in cmd:
                     return await event.reply("This has been disabled for you.")
