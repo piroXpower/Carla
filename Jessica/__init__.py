daa0d66882fea78c24380151cfa59fa1efb070e7
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-26 12:49:59 +0530

    Update __init__.py

--- Jessica/__init__.py
+++ Jessica/__init__.py
@@ -14,7 +14,7 @@ CMD_HELP = {}
 LOAD_PLUG = {}
 
 logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.DEBUG
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 LOGGER = logging.getLogger(__name__)
 ENV = bool(os.environ.get("ENV", True))
