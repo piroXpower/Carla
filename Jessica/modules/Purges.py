3fdba67420744162dfbb11b19eea3e0682ce1dde
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-27 09:39:38 +0530

    Update Purges.py

--- Jessica/modules/Purges.py
+++ Jessica/modules/Purges.py
@@ -3,6 +3,7 @@ import asyncio
 from telethon import Button, events, functions
 from telethon.errors import UserAlreadyParticipantError
 from telethon.errors.rpcerrorlist import MessageDeleteForbiddenError
+from telethon.errors import MultiError
 from telethon.tl.functions.messages import ExportChatInviteRequest
 
 from Jessica import BOT_ID, tbot, ubot
