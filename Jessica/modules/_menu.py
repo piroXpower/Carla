17cd637b7bbc73017035511295a8ece25ce8bb8a
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-06-27 15:18:57 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/_menu.py
+++ Jessica/modules/_menu.py
@@ -107,7 +107,9 @@ def paginate_help(event, page_number, loaded_plugins, prefix):
     to_check = page.find_one({"id": event.sender_id})
     page.update_one({"id": event.chat_id}, {"$set": {"page": page_number}}, upsert=True)
     helpable_plugins = sorted(plugins)
-    modules = [Button.inline(x, data=f"us_plugin_{x.lower()}") for x in helpable_plugins]
+    modules = [
+        Button.inline(x, data=f"us_plugin_{x.lower()}") for x in helpable_plugins
+    ]
     pairs = list(
         zip(
             modules[::3],
