fec2019529ac9f1f94a9916017b476ddcda538fc
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-27 14:00:59 +0530

    Update _menu.py

--- Jessica/modules/_menu.py
+++ Jessica/modules/_menu.py
@@ -45,7 +45,8 @@ async def start(event):
 
 @Cbot(pattern="^/help ?(.*)")
 async def help(event):
-    print("#")
+    if event.is_group:
+     await event.reply("Contact me in PM for help!", buttons=Button.url("Click me for help!", "https://t.me/MissNeko_Bot?start=_help"))
 
 
 @Cinline(pattern="help_menu")
