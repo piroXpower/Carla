5984e71fad45445dc4f3bc509dd31e7cb4618171
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-29 14:30:02 +0530

    Update CAPTCHA.py

--- Jessica/modules/CAPTCHA.py
+++ Jessica/modules/CAPTCHA.py
@@ -315,5 +315,5 @@ async def text_captcha(event, chat_id):
    if len(bt) == 3:
      btns.append(bt)
      bt = []
-   bt.append(Button.inline(x, data=f"txtc_{x}")
+   bt.append(Button.inline(x, data=f"txtc_{x}"))
  await event.respond(file=captcha_pic, buttons=buttons)
