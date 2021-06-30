91f1b0cd76abbc4544e25bf465c8feb8477e7163
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-06-30 14:23:47 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/Ai_test.py
+++ Jessica/modules/Ai_test.py
@@ -1,12 +1,13 @@
-from Jessica.events import Cbot
 from Jessica import ubot
+from Jessica.events import Cbot
+
 
 @Cbot(pattern="^/test ?(*.)")
 async def cb(e):
- q = event.pattern_match.group(1)
- if not q:
-   return
- async with ubot.conversation("@KukiAI_bot") as chat:
-   await chat.send_message(str(q))
-   res = await chat.get_response()
-   await res.forward_message(event.chat_id)
+    q = event.pattern_match.group(1)
+    if not q:
+        return
+    async with ubot.conversation("@KukiAI_bot") as chat:
+        await chat.send_message(str(q))
+        res = await chat.get_response()
+        await res.forward_message(event.chat_id)
