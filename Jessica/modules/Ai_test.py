9fe63ad22f2bfe8f660dda0312a4a9d4605efb3b
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-30 21:13:25 +0530

    Update Ai_test.py

--- Jessica/modules/Ai_test.py
+++ Jessica/modules/Ai_test.py
@@ -1,17 +1,26 @@
-from Jessica import ubot
+from Jessica import ubot, BOT_ID
 from Jessica.events import Cbot
 
 
-@Cbot(pattern="^/c ?(.*)")
+@tbot.on(events.NewMessage())
 async def cb(e):
-    q = e.pattern_match.group(1)
-    if not q:
-        return
+    if e.media or e.fwd_from:
+       return
+    if e.reply_to:
+       r_e = await e.get_reply_message()
+       if r_e.sender_id == BOT_ID:
+          pass
+       else:
+          return
+    else:
+       return
+    q = e.text
     async with ubot.conversation("@KukiAI_bot") as chat:
         await chat.send_message(str(q))
         res = await chat.get_response()
         if res.text:
             await e.reply(res.text)
         elif res.media:
-            re_re = await ubot.send_message(file=res.media)
+            re_re = await ubot.send_message("@MissNeko_Bot", file=res.media)
             await e.reply(file=re_re.media)
+            await re_re.delete()
