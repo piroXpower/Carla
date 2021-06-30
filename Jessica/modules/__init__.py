30f6a78a6dbbe45fede0dc23f5a3a821cc85e86b
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-30 10:44:35 +0530

    Update __init__.py

--- Jessica/modules/__init__.py
+++ Jessica/modules/__init__.py
@@ -546,3 +546,45 @@ def gen_captcha(mode="text"):
         captcha_total = generator.gen_math_captcha_image(2, False, True, False)
         (captcha_total["image"]).save("captcha.png")
         return "captcha.png", captcha_total["equation_result"]
+
+def generate_captcha():
+    def gen_letter():
+        return chr(randint(65, 90))
+
+    def rndColor():
+        return (randint(64, 255), randint(64, 255), randint(64, 255))
+
+    def rndColor2():
+        return (randint(32, 127), randint(32, 127), randint(32, 127))
+
+    def gen_wrong_answer():
+        word = ""
+        for _ in range(4):
+            word += gen_letter()
+        return word
+
+    wrong_answers = []
+    for _ in range(8):
+        wrong_answers.append(gen_wrong_answer())
+
+    width = 80 * 4
+    height = 100
+    correct_answer = ""
+    font = ImageFont.truetype("./Jessica/modules/sql/DroidSans.ttf", 55)
+    file = f"{randint(1000, 9999)}.jpg"
+    image = Image.new("RGB", (width, height), (255, 255, 255))
+    draw = ImageDraw.Draw(image)
+
+    for x in range(width):
+        for y in range(height):
+            draw.point((x, y), fill=rndColor())
+
+    for t in range(4):
+        letter = gen_letter()
+        correct_answer += letter
+        draw.text(
+            (60 * t + 50, 15), letter, font=font, fill=rndColor2()
+        )
+    image = image.filter(ImageFilter.BLUR)
+    image.save(file, "jpeg")
+    return [file, correct_answer, wrong_answers]
