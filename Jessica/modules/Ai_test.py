e37c317ccf569edc91ce77e4b7dba8f966cd1543
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-30 21:16:39 +0530

    Update Ai_test.py

--- Jessica/modules/Ai_test.py
+++ Jessica/modules/Ai_test.py
@@ -1,10 +1,35 @@
 from telethon import events
-
+from Jessica.events import Cbot
 from Jessica import BOT_ID, tbot, ubot
+import Jessica.modules.sql.chatbot_sql as sql
+
+@Cbot(pattern="^/chatbot ?(.*)")
+async def cb(event):
+    if event.is_group:
+        if not await can_change_info(event, event.sender_id):
+            return
+    args = event.pattern_match.group(1)
+    if not args:
+        mode = sql.chatbot_mode(event.chat_id)
+        if mode:
+            await event.reply("AI chatbot is currently **enabled** for this chat.")
+        else:
+            await event.reply("AI chatbot is currently **disabled** for this chat.")
+    elif args in ["on", "y", "yes"]:
+        await event.reply("**Enabled** AI chatbot for this chat.")
+        sql.set_chatbot_mode(event.chat_id, True)
+    elif args in ["off", "n", "no"]:
+        await event.reply("**Disabled** AI chatbotfor this chat.")
+        sql.set_chatbot_mode(event.chat_id, False)
+    else:
+        await event.reply("Your input was not recognised as one of: yes/no/y/n/on/off")
+
 
 
 @tbot.on(events.NewMessage())
 async def cb(e):
+    if not sql.chatbot_mode(e.chat_id):
+        return
     if e.media or e.fwd_from:
         return
     if e.reply_to:
