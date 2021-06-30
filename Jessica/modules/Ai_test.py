feb42a10d67fffe53bd2dd6a5fb541641f230aef
Amarnath C <amarnathcharichilil@gmail.com>
2021-06-30 20:31:51 +0530

    Update Ai_test.py

--- Jessica/modules/Ai_test.py
+++ Jessica/modules/Ai_test.py
@@ -2,7 +2,7 @@ from Jessica import ubot
 from Jessica.events import Cbot
 
 
-@Cbot(pattern="^/test ?(.*)")
+@Cbot(pattern="^/c ?(.*)")
 async def cb(e):
     q = e.pattern_match.group(1)
     if not q:
@@ -10,5 +10,9 @@ async def cb(e):
     async with ubot.conversation("@KukiAI_bot") as chat:
         await chat.send_message(str(q))
         res = await chat.get_response()
+        if res.media:
+            @ubot.on(events.NewMessage(from_users=["@KukiAI_bot"])
+            async def _(ev):
+               await e.reply(ev.text)
         if res.text:
             await e.reply(res.text)
