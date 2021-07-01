650688dc664a3ed29eb16df313e8e939c3bcef72
Amarnath C <amarnathcharichilil@gmail.com>
2021-07-01 16:58:14 +0530

    Create rules_db.py

---
+++ Jessica/modules/mongodb/rules_db.py
@@ -0,0 +1,16 @@
+from . import db
+rules = db.rules
+
+def set_rules(chat_id: int, rules: str):
+ rules.update_one({"chat_id": chat_id}, {"$set": {"rules": rules}}, upsert=True)
+
+def get_rules(chat_id: int):
+ _rules = rules.find_one({"chat_id": chat_id})
+ if _rules:
+   return _rules["rules"]
+ return False
+
+def del_rules(chat_id: int):
+ _rules = rules.find_one({"chat_id": chat_id})
+ if _rules:
+   rules.delete_one({"chat_id": chat_id})
