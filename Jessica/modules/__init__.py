76f553ca8f4880f7ba6c0633967ebe254c6b9ea7
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-07-01 15:28:09 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/__init__.py
+++ Jessica/modules/__init__.py
@@ -323,17 +323,18 @@ async def extract_time(message, time_val):
         )
         return False
 
+
 def g_time(time: int):
- if time >= 86400:
-   time = int(time/(60*60*24))
-   unit = "days"
- elif time >= 3600 < 86400:
-   time = int(time/(60*60))
-   unit = "hours"
- elif time >= 60 < 3600:
-   time = int(time/60)
-   unit = "minutes"
- return " {} {}".format(time, unit)
+    if time >= 86400:
+        time = int(time / (60 * 60 * 24))
+        unit = "days"
+    elif time >= 3600 < 86400:
+        time = int(time / (60 * 60))
+        unit = "hours"
+    elif time >= 60 < 3600:
+        time = int(time / 60)
+        unit = "minutes"
+    return " {} {}".format(time, unit)
 
 
 BTN_URL_REGEX = re.compile(
