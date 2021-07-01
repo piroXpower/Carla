d45ce603455328706db92c7a19985c1ddf4b9309
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-07-01 11:28:42 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/mongodb/rules_db.py
+++ Jessica/modules/mongodb/rules_db.py
@@ -1,16 +1,20 @@
 from . import db
+
 rules = db.rules
 
+
 def set_rules(chat_id: int, rules: str):
- rules.update_one({"chat_id": chat_id}, {"$set": {"rules": rules}}, upsert=True)
+    rules.update_one({"chat_id": chat_id}, {"$set": {"rules": rules}}, upsert=True)
+
 
 def get_rules(chat_id: int):
- _rules = rules.find_one({"chat_id": chat_id})
- if _rules:
-   return _rules["rules"]
- return False
+    _rules = rules.find_one({"chat_id": chat_id})
+    if _rules:
+        return _rules["rules"]
+    return False
+
 
 def del_rules(chat_id: int):
- _rules = rules.find_one({"chat_id": chat_id})
- if _rules:
-   rules.delete_one({"chat_id": chat_id})
+    _rules = rules.find_one({"chat_id": chat_id})
+    if _rules:
+        rules.delete_one({"chat_id": chat_id})
