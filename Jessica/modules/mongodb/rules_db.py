1cbd2fefc8f2f3cc3689e44ea95fbb2af7c2f792
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-07-01 12:15:44 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/mongodb/rules_db.py
+++ Jessica/modules/mongodb/rules_db.py
@@ -4,7 +4,11 @@ rules = db.rules
 
 
 def set_rules(chat_id: int, rule_s: str):
-    rules.update_one({"chat_id": chat_id}, {"$set": {"rules": rule_s, "private": False, "button": "Rules"}}, upsert=True)
+    rules.update_one(
+        {"chat_id": chat_id},
+        {"$set": {"rules": rule_s, "private": False, "button": "Rules"}},
+        upsert=True,
+    )
 
 
 def get_rules(chat_id: int):
@@ -19,15 +23,21 @@ def del_rules(chat_id: int):
     if _rules:
         rules.delete_one({"chat_id": chat_id})
 
+
 def set_private_rules(chat_id: int, mode):
     _rules = rules.find_one({"chat_id": chat_id})
     if _rules:
-     rule_s = _rules["rules"]
-     button = _rules["button"]
+        rule_s = _rules["rules"]
+        button = _rules["button"]
     else:
-     rule_s = ""
-     button = "Rules"
-    rules.update_one({"chat_id": chat_id}, {"$set": {"rules": rule_s, "private": mode, "button": button}}, upsert=True)
+        rule_s = ""
+        button = "Rules"
+    rules.update_one(
+        {"chat_id": chat_id},
+        {"$set": {"rules": rule_s, "private": mode, "button": button}},
+        upsert=True,
+    )
+
 
 def get_private_rules(chat_id: int):
     _rules = rules.find_one({"chat_id": chat_id})
@@ -35,19 +45,24 @@ def get_private_rules(chat_id: int):
         return _rules["private"]
     return False
 
+
 def set_rules_button(chat_id, button):
     _rules = rules.find_one({"chat_id": chat_id})
     if _rules:
-     rule_s = _rules["rules"]
-     p = _rules["private"]
+        rule_s = _rules["rules"]
+        p = _rules["private"]
     else:
-     rule_s = ""
-     p = False
-    rules.update_one({"chat_id": chat_id}, {"$set": {"rules": rule_s, "private": p, "button": button}}, upsert=True)
+        rule_s = ""
+        p = False
+    rules.update_one(
+        {"chat_id": chat_id},
+        {"$set": {"rules": rule_s, "private": p, "button": button}},
+        upsert=True,
+    )
+
 
 def get_rules_button(chat_id: int):
     _rules = rules.find_one({"chat_id": chat_id})
     if _rules:
         return _rules["button"]
     return "Rules"
-
