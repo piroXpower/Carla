b39f87827879857ebfa242b64c0874d6240f2c79
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-07-05 11:46:23 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/antibetreyal_system.py
+++ Jessica/modules/antibetreyal_system.py
@@ -1,10 +1,12 @@
-from . import db
 # from ..events import Cbot, Cinline
 from telethon import events
 from telethon.tl.types import ChannelParticipantBanned, UpdateChannelParticipant
 
 from .. import tbot
+from . import db
+
 x_b = db.betreyal
+
 
 @tbot.on(events.Raw(UpdateChannelParticipant))
 async def x(e):
@@ -17,17 +19,23 @@ async def x(e):
     else:
         return
     if not e.new_participant:
-       return
+        return
     x = (str(e)).replace("UpdateChannelParticipant", "")
     chat_id = int(str(-100) + str(e.channel_id))
     chance = 1
-    prev_db = x_b.find_one({"chat_id": e.chat_id, "user_id": e.new_participant.kicked_by})
+    prev_db = x_b.find_one(
+        {"chat_id": e.chat_id, "user_id": e.new_participant.kicked_by}
+    )
     if prev_db:
-      chance = prev_db["chance"] + 1
-    x_b.update_one({"chat_id": chat_id, "user_id": e.new_participant.kicked_by}, {"$set": {"chance": chance}}, upsert=True)
+        chance = prev_db["chance"] + 1
+    x_b.update_one(
+        {"chat_id": chat_id, "user_id": e.new_participant.kicked_by},
+        {"$set": {"chance": chance}},
+        upsert=True,
+    )
     if chance >= 2:
-      try:
-        await tbot.edit_admin(
+        try:
+            await tbot.edit_admin(
                 chat_id,
                 e.new_participant.kicked_by,
                 is_admin=False,
@@ -39,10 +47,10 @@ async def x(e):
                 change_info=False,
                 invite_users=False,
             )
-      except:
-        pass
+        except:
+            pass
     user = await tbot.get_entity(e.new_participant.kicked_by)
-    chat = await tbot.get_entity(chat_id)
+    await tbot.get_entity(chat_id)
     x = f"""
 Warning !!
   Betreyer Detected.
@@ -50,4 +58,3 @@ User: [{user.first_name}](tg://user?id={user.id})
 Banned Count: {chance}
 """
     await tbot.send_message(chat_id, x)
-    
