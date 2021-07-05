eb4e25bc52c9fcee64456f700e2a2ea657676cc7
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-07-05 15:15:22 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/mongodb/afk_db.py
+++ Jessica/modules/mongodb/afk_db.py
@@ -1,17 +1,29 @@
-from .. import db
 import time
+
+from .. import db
+
 afk = db.afk
 
+
 def set_afk(user_id: int, first_name: stt, reason: str):
- afk.insert_one({"user_id": user_id, "first_name": first_name, "reason": reason, "time": time.time()})
+    afk.insert_one(
+        {
+            "user_id": user_id,
+            "first_name": first_name,
+            "reason": reason,
+            "time": time.time(),
+        }
+    )
+
 
 def unset_afk(user_id):
- _afk = afk.find_one({"user_id"})
- if _afk:
-  afk.delete_one({"user_id"})
+    _afk = afk.find_one({"user_id"})
+    if _afk:
+        afk.delete_one({"user_id"})
+
 
 def get_afk(user_id):
- _afk = afk.find_one({"user_id"})
- if _afk:
-   return True
- return False
+    _afk = afk.find_one({"user_id"})
+    if _afk:
+        return True
+    return False
