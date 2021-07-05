0474aaff76e83ac45b09a08304af8e63cf3756be
Amarnath C <amarnathcharichilil@gmail.com>
2021-07-05 20:50:37 +0530

    Update afk_db.py

--- Jessica/modules/mongodb/afk_db.py
+++ Jessica/modules/mongodb/afk_db.py
@@ -17,13 +17,13 @@ def set_afk(user_id: int, first_name="User", reason=None):
 
 
 def unset_afk(user_id):
-    _afk = afk.find_one({"user_id"})
+    _afk = afk.find_one({"user_id": user_id})
     if _afk:
-        afk.delete_one({"user_id"})
+        afk.delete_one({"user_id": user_id})
 
 
 def get_afk(user_id):
-    _afk = afk.find_one({"user_id"})
+    _afk = afk.find_one({"user_id": user_id})
     if _afk:
         return True
     return False
