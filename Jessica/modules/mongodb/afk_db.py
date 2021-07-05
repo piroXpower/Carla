095a0516b4dd0e3b4aa992c5bb9d3b0c00e77758
Amarnath C <amarnathcharichilil@gmail.com>
2021-07-05 20:44:49 +0530

    Create afk_db.py

---
+++ Jessica/modules/mongodb/afk_db.py
@@ -0,0 +1,17 @@
+from .. import db
+import time
+afk = db.afk
+
+def set_afk(user_id: int, first_name: stt, reason: str):
+ afk.insert_one({"user_id": user_id, "first_name": first_name, "reason": reason, "time": time.time()})
+
+def unset_afk(user_id):
+ _afk = afk.find_one({"user_id"})
+ if _afk:
+  afk.delete_one({"user_id"})
+
+def get_afk(user_id):
+ _afk = afk.find_one({"user_id"})
+ if _afk:
+   return True
+ return False
