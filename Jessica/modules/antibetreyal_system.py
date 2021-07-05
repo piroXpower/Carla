20701edee77135cbf8bb3710b21f049604e1c6be
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-07-05 11:18:09 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/antibetreyal_system.py
+++ Jessica/modules/antibetreyal_system.py
@@ -1,17 +1,19 @@
 # from . import db
 # from ..events import Cbot, Cinline
-from .. import tbot, CMD_HELP
 from telethon.tl.types import ChannelParticipantBanned, UpdateChannelParticipant
+
+from .. import tbot
+
 
 @tbot.events.Raw(UpdateChannelParticipant)
 async def x(e):
- if not e.prev_participant:
+    if not e.prev_participant:
         return
- if isinstance(e.prev_participant, ChannelParticipantBanned):
+    if isinstance(e.prev_participant, ChannelParticipantBanned):
         return
- if e.channel_id == 1486931338:
-   pass
- else:
-   return
- chat_id = int((-100) + str(e.channel_id ))
- await tbot.send_message(chat_id, "AntiBetreyal_Sys Test.")
+    if e.channel_id == 1486931338:
+        pass
+    else:
+        return
+    chat_id = int((-100) + str(e.channel_id))
+    await tbot.send_message(chat_id, "AntiBetreyal_Sys Test.")
