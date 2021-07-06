8d615b0f02248a237249d91bf759bbbfe0ebb056
Amarnath C <amarnathcharichilil@gmail.com>
2021-07-06 20:13:04 +0530

    Update zip_file.py

--- Jessica/modules/zip_file.py
+++ Jessica/modules/zip_file.py
@@ -200,6 +200,6 @@ async def zip_back(e):
     try:
         zip_files = zip_back_files_db[x_name]
     except KeyError:
-        return
+        return await event.answer("fimx" + "|" + x_name, alert=True)
     buttons = paginate_zip(0, zip_files, x_name)
     await e.edit(buttons=buttons)
