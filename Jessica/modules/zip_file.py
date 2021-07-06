3cf193986b833ca43e2f2d1fc003ff20b79c4992
Amarnath C <amarnathcharichilil@gmail.com>
2021-07-06 15:06:53 +0530

    Update zip_file.py

--- Jessica/modules/zip_file.py
+++ Jessica/modules/zip_file.py
@@ -26,36 +26,42 @@ async def e_unzip(event):
             return await event.reply(
                 "File size limit exceeds, The maximum file size allowed is 5MB."
             )
-        x_r = await event.reply("Started the file download!")
-        zip_f = await tbot.download_media(zip_file)
-    else:
-        return
-    if zip_f:
-        with zipfile.ZipFile(zip_f, "r") as zip_r:
-            zip_r.extractall("./zip")
-    unzip_dir = "zip/" + str(zip_f).replace(".zip", "")
-    await x_r.edit(str(os.listdir(unzip_dir)))
-
-
-@Cbot(pattern="^/unzip_menu")
-async def e_unzip_menu(e):
-    x_text = """
+        x_text = """
 Choose appropriate action 
 
 🗃 = Normal files 
 🔓 = Password protected files 
 ❌ = Cancel Process
 """
-    x_buttons = [
+        x_buttons = [
         [
-            Button.inline("Unzip🗃", data="unzip_f"),
-            Button.inline("Password🔓", data="password"),
+            Button.inline("Unzip🗃", data=f"unzip_{event.id}"),
+            Button.inline("Password🔓", data=f"password_{event.id}"),
         ],
         [Button.inline("Cancel ❌", data="unzip_cancel")],
     ]
-    await e.reply(x_text, buttons=x_buttons)
-
+        await e.reply(x_text, buttons=x_buttons)
+        zip_f = await tbot.download_media(zip_file)
+        zip_db[event.id] = zip_f
+        
+    else:
+        return
 
 @Cinline(pattern="unzip_cancel")
 async def unzip_cancel_cb(e):
     await e.delete()
+
+@Cinline(pattern="unzip(\_(.*)")
+async def unzip_e(e):
+ e_id = int(((e.pattern_match.group(1)).decode()).split("_", 1)[1])
+ try:
+   zip_f = zip_db[e_id]
+ except KeyError:
+   await e.edit("File not found‼️")
+ if zip_f:
+        with zipfile.ZipFile(zip_f, "r") as zip_r:
+            zip_r.extractall("./zip")
+ unzip_dir = "zip/" + str(zip_f).replace(".zip", "")
+ await x_r.edit(str(os.listdir(unzip_dir)))
+
+
