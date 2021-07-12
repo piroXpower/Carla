d7dbe7a9bb1027c3d9739d1759b91f2d3221d7ae
Amarnath C <amarnathcharichilil@gmail.com>
2021-07-12 20:11:47 +0530

    Update feds_db.py

--- Jessica/modules/mongodb/feds_db.py
+++ Jessica/modules/mongodb/feds_db.py
@@ -25,7 +25,7 @@ def new_fed(owner_id: int, fed_id, fedname):
 def del_fed(fed_id):
     feds.delete_one({"fed_id": fed_id})
     fbans.delete_one({"fed_id": fed_id})
-
+    fsubs.delete_one({"fed_id": fed_id})
 
 def transfer_fed(owner_id: int, user_id: int):
     _fed = feds.find_one({"owner_id": owner_id})
