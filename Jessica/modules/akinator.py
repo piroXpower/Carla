67d2dac104ce2472b4ceb8595b69510b12ac7938
Amarnath C <amarnathcharichilil@gmail.com>
2021-07-16 17:00:41 +0530

    Update and rename spamdetect.py to akinator.py

---
+++ Jessica/modules/akinator.py
@@ -0,0 +1,12 @@
+from telethon import Button
+from ..events import Cbot, Cinline
+from akinator import Akinator
+
+buttons = [[Button.inline("Yes", data="aki_yes"), Button.inline("No", data="aki_no"), Button.inline("Probably", data="aki_probably")], [Button.inline("I don't Know", data="aki_idk"), Button.inline("Probably not", data="aki_probably_not")], [Button.inline("Go Back", data="aki_back")]]
+
+@Cbot(pattern="^/akinator ?(.*)")
+async def akinator(e):
+ f = await e.respond("Loading...")
+ q = Akinator()
+ first_q = q.start_game()
+ await e.respond(first_q, buttons=buttons)
