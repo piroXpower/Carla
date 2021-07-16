617e8d63ce29c5c0f6c5044eddcd75346ff19f50
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-07-16 11:31:12 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/akinator.py
+++ Jessica/modules/akinator.py
@@ -1,12 +1,25 @@
-from telethon import Button
-from ..events import Cbot, Cinline
 from akinator import Akinator
+from telethon import Button
 
-buttons = [[Button.inline("Yes", data="aki_yes"), Button.inline("No", data="aki_no"), Button.inline("Probably", data="aki_probably")], [Button.inline("I don't Know", data="aki_idk"), Button.inline("Probably not", data="aki_probably_not")], [Button.inline("Go Back", data="aki_back")]]
+from ..events import Cbot
+
+buttons = [
+    [
+        Button.inline("Yes", data="aki_yes"),
+        Button.inline("No", data="aki_no"),
+        Button.inline("Probably", data="aki_probably"),
+    ],
+    [
+        Button.inline("I don't Know", data="aki_idk"),
+        Button.inline("Probably not", data="aki_probably_not"),
+    ],
+    [Button.inline("Go Back", data="aki_back")],
+]
+
 
 @Cbot(pattern="^/akinator ?(.*)")
 async def akinator(e):
- f = await e.respond("Loading...")
- q = Akinator()
- first_q = q.start_game()
- await e.respond(first_q, buttons=buttons)
+    await e.respond("Loading...")
+    q = Akinator()
+    first_q = q.start_game()
+    await e.respond(first_q, buttons=buttons)
