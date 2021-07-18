ed90b0d53f13bd8cda0ed7b7bcc22ec62e05a63b
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-07-18 09:17:24 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/Inline.py
+++ Jessica/modules/Inline.py
@@ -352,7 +352,7 @@ async def imdb_data_(e):
     for x in range(0, 3):
         genr_e += " " + genre[x].text + ","
         if x == 2:
-          genr_e += " " + genre[x].text
+            genr_e += " " + genre[x].text
     text = f"**[{title}]**({img})\n**Ratings:** `{rating}/10`{genr_e}\n\n`{desc}`"
     await e.edit(
         text,
