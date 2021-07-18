3a39fa7bee914bdf7a9213d866ac6410bab2d18a
Amarnath C <amarnathcharichilil@gmail.com>
2021-07-18 19:20:19 +0530

    Update Inline.py

--- Jessica/modules/Inline.py
+++ Jessica/modules/Inline.py
@@ -65,7 +65,7 @@ async def gen_help(event, thumb):
             Button.switch_inline("Torrent Search", query="torrent ", same_peer=True),
             Button.switch_inline("IMDb Search", query="imdb ", same_peer=True),
         ],
-        [Button.switch_inline("Wallpaper Search", query="wall ", same_peer=True),
+        [Button.switch_inline("Wallpaper Search", query="wall ", same_peer=True),]
     ]
     return await event.builder.article(
         title="Inline Query Help Menu.",
