9d19bb3c3e0e2dfe11c0f253b558034f17a339fa
Amarnath C <amarnathcharichilil@gmail.com>
2021-07-19 21:32:39 +0530

    Update Inline.py

--- Jessica/modules/Inline.py
+++ Jessica/modules/Inline.py
@@ -6,7 +6,7 @@ from telethon import Button, events
 from telethon.tl.types import InputWebDocument
 from tpblite import TPB
 from youtubesearchpython import SearchVideos
-
+from geniuses import GeniusClient
 from Jessica import tbot
 from Jessica.events import Cinline, Cquery
 
