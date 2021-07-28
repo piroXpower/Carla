e667ce1904c436ac7bb13201e7785afb8c34a2f3
Amarnath C <amarnathcharichilil@gmail.com>
2021-07-28 10:06:21 +0530

    Update Purges.py

--- Jessica/modules/Purges.py
+++ Jessica/modules/Purges.py
@@ -12,11 +12,6 @@ from . import can_del_msg, db, is_owner
 
 purgex = db.purge
 
-
-def get_id(id):
-    return purgex.find_one({"id": id})
-
-
 @Cbot(pattern="^/purge ?(.*)")
 async def pugre(event):
     if (
@@ -70,23 +65,7 @@ async def lil(event):
         return await event.reply("Reply to a message to let me know what to delete.")
     reply_msg = await event.get_reply_message()
     msg_id = reply_msg.id
-    chats = purgex.find({})
-    for c in chats:
-        if event.chat_id == c["id"]:
-            to_check = get_id(id=event.chat_id)
-            purgex.update_one(
-                {
-                    "_id": to_check["_id"],
-                    "id": to_check["id"],
-                    "msg_id": to_check["msg_id"],
-                },
-                {"$set": {"msg_id": msg_id}},
-            )
-            return await event.respond(
-                "Message marked for deletion. Reply to another message with /purgeto to delete all messages in between.",
-                reply_to=msg_id,
-            )
-    purgex.insert_one({"id": event.chat_id, "msg_id": msg_id})
+    purgex.update_one({"id": event.chat_id}, {"$set": {"msg_id": msg_id}}, upsert=True)
     await event.respond(
         "Message marked for deletion. Reply to another message with /purgeto to delete all messages in between.",
         reply_to=msg_id,
@@ -101,12 +80,11 @@ async def lilz(event):
     if not event.reply_to_msg_id:
         return await event.reply("Reply to a message to let me know what to delete.")
     reply_msg = await event.get_reply_message()
-    reply_msg.id
-    msg_id = None
-    chats = purgex.find({})
-    for c in chats:
-        if event.chat_id == c["id"]:
-            msg_id = c["msg_id"]
+    purge = purgex.find_one({"id": event.chat_id})
+    if purge = None:
+        msg_id = None
+    else:
+        msg_id = purge.get("msg_id")
     if msg_id == None:
         return await event.reply(
             "You can only use this command after having used the /purgefrom command."
@@ -125,16 +103,14 @@ async def lilz(event):
         return await event.reply("I can't delete messages that are too old!")
     except MessageDeleteForbiddenError:
         return await event.reply("I can't delete messages that are too old!")
-    chats = purgex.find({})
-    for c in chats:
-        if event.chat_id == c["id"]:
-            purgex.delete_one({"id": event.chat_id})
-
+    purgex.delete_one({"id": event.chat_id})
+    await event.respond("Purge complete!")
 
 @Cbot(pattern="^/del")
 async def deve(event):
     if event.from_id:
-        if not await can_del_msg(event, event.sender_id):
+        if event.is_group:
+         if not await can_del_msg(event, event.sender_id):
             return
         if not event.reply_to:
             return await event.reply(
@@ -222,15 +198,18 @@ async def ki(event):
     except Exception as e:
         return await event.edit(str(e))
     link = (link.link).replace("https://t.me/joinchat/", "")
+    JOINED = False
     try:
-        result = await ubot(functions.messages.ImportChatInviteRequest(hash=link))
+        result = await ubot(functions.channels.JoinChannelRequest(link)
+        JOINED = True
     except UserAlreadyParticipantError:
         pass
     except Exception as e:
         return await event.edit(str(e))
-    await tbot.edit_admin(
+    try:
+     await tbot.edit_admin(
         event.chat_id,
-        1763477650,
+        "@RoseLoverX",
         manage_call=False,
         add_admins=False,
         pin_messages=True,
@@ -238,8 +217,13 @@ async def ki(event):
         ban_users=True,
         change_info=True,
         invite_users=True,
-        title="delall",
+        title="delall_helper",
     )
+    except Exception as e:
+      if await can_del_msg(event, "RoseLoverX"):
+          pass
+      else:
+          return await event.edit(str(e))
     msg_id = event.id
     for msg_id in range(1, msg_id + 1):
         messages.append(msg_id)
@@ -247,9 +231,10 @@ async def ki(event):
             await ubot.delete_messages(event.chat_id, messages)
             messages = []
     await ubot.delete_messages(event.chat_id, messages)
-    try:
-        await tbot.kick_participant(event.chat_id, 1763477650)
-    except:
+    if JOINED:
+     try:
+        await tbot.kick_participant(event.chat_id, "RoseLoverX")
+     except:
         pass
     k = await event.edit("Cleaning Process Completed.")
     await asyncio.sleep(4)
