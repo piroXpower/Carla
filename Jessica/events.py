5859e3505c8b59ad1895e02ba800068a85ce1e21
Amarnath C <amarnathcharichilil@gmail.com>
2021-07-29 11:36:22 +0530

    Update events.py

--- Jessica/events.py
+++ Jessica/events.py
@@ -12,7 +12,7 @@ from Jessica import CMD_LIST, tbot
 
 def Cbot(**args):
     pattern = args.get("pattern", None)
-    r_pattern = r"^[/?!.]"
+    r_pattern = r"^[/?!+]"
     if pattern is not None and not pattern.startswith("(?i)"):
         args["pattern"] = "(?i)" + pattern
     args["pattern"] = pattern.replace("^/", r_pattern, 1)
