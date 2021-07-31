92618f164f68f100ab16a564384b767b75374378
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-07-31 15:53:18 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/mongodb/warns_db.py
+++ Jessica/modules/mongodb/warns_db.py
@@ -17,33 +17,40 @@ def warn_user(user_id, chat_id, reason=""):
         upsert=True,
     )
 
+
 def remove_warn(user_id, chat_id):
     _warn = warns.find_one({"chat_id": chat_id, "user_id": user_id})
     if _warn and _warn["num_warns"] > 0:
         warns.update_one(
-        {"chat_id": chat_id, "user_id": user_id},
-        {"$set": {"num_warns": _warn["num_warns"] - 1}},
-        upsert=True)
+            {"chat_id": chat_id, "user_id": user_id},
+            {"$set": {"num_warns": _warn["num_warns"] - 1}},
+            upsert=True,
+        )
         return True
     return False
+
 
 def reset_warns(user_id, chat_id):
     _warn = warns.find_one({"chat_id": chat_id, "user_id": user_id})
     if _warn:
         warns.update_one(
-        {"chat_id": chat_id, "user_id": user_id},
-        {"$set": {"num_warns": 0}},
-        upsert=True)
+            {"chat_id": chat_id, "user_id": user_id},
+            {"$set": {"num_warns": 0}},
+            upsert=True,
+        )
         return True
     return False
+
 
 def get_warns(user_id, chat_id):
     _warn = warns.find_one({"chat_id": chat_id, "user_id": user_id})
     if _warn:
-        return _warn['num_warns'], _warn['reasons']
+        return _warn["num_warns"], _warn["reasons"]
     return None
+
 
 def reset_all_warns(chat_id):
     warns.delete_one({"chat_id": chat_id})
-    
-# soon 
+
+
+# soon
