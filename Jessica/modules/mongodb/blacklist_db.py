92548df6668e1beb95ce29bd26498fd819380222
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-08-02 05:05:13 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/mongodb/blacklist_db.py
+++ Jessica/modules/mongodb/blacklist_db.py
@@ -2,26 +2,33 @@ from .. import db
 
 blacklist = db.blacklist
 
+
 def add_to_blacklist(chat_id, trigger):
- _bl = blacklist.find_one({'chat_id': chat_id})
- if _bl:
-   bl = _bl['blacklists']
-   mode = _bl['mode']
-   time = _bl['time']
- else:
-   bl = []
-   mode = 'nothing'
-   time = 0
- bl.append(trigger)
- blacklist.update_one({'chat_id': chat_id}, {'$set': {'blacklists': bl, 'mode': mode, 'time': time}}, upsert=True)
+    _bl = blacklist.find_one({"chat_id": chat_id})
+    if _bl:
+        bl = _bl["blacklists"]
+        mode = _bl["mode"]
+        time = _bl["time"]
+    else:
+        bl = []
+        mode = "nothing"
+        time = 0
+    bl.append(trigger)
+    blacklist.update_one(
+        {"chat_id": chat_id},
+        {"$set": {"blacklists": bl, "mode": mode, "time": time}},
+        upsert=True,
+    )
+
 
 def rm_from_blacklist(chat_id, trigger):
-  _bl = blacklist.find_one({'chat_id': chat_id})
-  if _bl:
-   bl = _bl['blacklists']
-  else:
-   return False
-  if trigger in bl:
-   bl.remove(trigger)
-  blacklist.update_one({'chat_id': chat_id}, {'$set': {'blacklists': bl}}, upsert=True)
-
+    _bl = blacklist.find_one({"chat_id": chat_id})
+    if _bl:
+        bl = _bl["blacklists"]
+    else:
+        return False
+    if trigger in bl:
+        bl.remove(trigger)
+    blacklist.update_one(
+        {"chat_id": chat_id}, {"$set": {"blacklists": bl}}, upsert=True
+    )
