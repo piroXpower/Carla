558422eea51dff42317bb2cadc62980c6df9454a
Amarnath C <amarnathcharichilil@gmail.com>
2021-08-03 21:11:43 +0530

    Update Antiflood.py

--- Jessica/modules/Antiflood.py
+++ Jessica/modules/Antiflood.py
@@ -2,7 +2,7 @@ from telethon import events
 
 from .. import tbot
 from . import can_change_info, extract_time
-from .mongodb.antiflood_db import update_flood
+from .mongodb import antiflood_db as db
 from .sql import antiflood_sql as sql
 
 badtime = """
@@ -33,21 +33,21 @@ async def _(event):
             f"Unknown type '{options}'. Please use one of: ban/kick/mute/tban/tmute"
         )
     if options[0] == "ban":
-        sql.set_flood_strength(event.chat_id, 1, "0")
+        db.set_flood_strength(event.chat_id, 'ban')
         txt = "Banned"
     elif options[0] == "kick":
-        sql.set_flood_strength(event.chat_id, 2, "0")
+        db.set_flood_strength(event.chat_id, 'kick')
         txt = "Kicked"
     elif options[0] == "mute":
-        sql.set_flood_strength(event.chat_id, 3, "0")
+        db.set_flood_strength(event.chat_id, 'mute')
         txt = "Muted"
     elif options[0] == "tban":
         if len(options) == 1:
             return await event.reply(badtime)
         time = await extract_time(event, options[1])
         if not time:
-            return
-        sql.set_flood_strength(event.chat_id, 4, str(time))
+               return
+        db.set_flood_strength(event.chat_id, 'tban', time)
         txt = "temporarly Banned for {}".format(options[1])
     elif options[0] == "tmute":
         if len(options) == 1:
@@ -55,7 +55,7 @@ async def _(event):
         time = await extract_time(event, options[1])
         if not time:
             return
-        sql.set_flood_strength(event.chat_id, 5, str(time))
+        db.set_flood_strength(event.chat_id, 'tmute', time)
         txt = "temporarly Muted for {}".format(options[1])
     await event.respond(
         "Updated antiflood reaction in {} to: **{}**".format(event.chat.title, txt)
@@ -68,6 +68,7 @@ async def _(event):
         event.text.startswith("/setfloodmode")
         or event.text.startswith("!setfloodmode")
         or event.text.startswith("?setfloodmode")
+        or event.text.startswith("+setfloodmode")
     ):
         return
     if event.is_private:
@@ -78,103 +79,34 @@ async def _(event):
     if not args:
         return await event.reply(badargs)
     if args == "off":
-        sql.set_flood(event.chat_id, 0)
+        db.set_flood(event.chat_id, 0)
         text = "Antiflood has been disabled."
     elif args == "∞":
-        sql.set_flood(event.chat_id, 0)
+        db.set_flood(event.chat_id, 0)
         text = "Antiflood has been disabled."
     elif args == "0":
-        sql.set_flood(event.chat_id, 0)
+        db.set_flood(event.chat_id, 0)
         text = "Antiflood has been disabled."
     elif args.isdigit():
         if not int(args):
             return await event.reply(f"{args} is not a valid integer.")
         elif int(args) <= 0:
-            sql.set_flood(event.chat_id, 0)
+            db.set_flood(event.chat_id, 0)
             text = "Antiflood has been disabled."
         else:
-            sql.set_flood(event.chat_id, int(args))
+            db.set_flood(event.chat_id, int(args))
             text = f"Antiflood settings for {event.chat.title} have been updated to {int(args)}."
     else:
         return await event.reply(f"{args}is not a valid integer.")
     await event.reply(text)
 
 
-"""
-@tbot.on(events.NewMessage(pattern=None))
-async def flood(event):
-    if event.is_private:
-        return  # connect
-    if not event.from_id:
-        return
-    if (
-        event.sender_id in ELITES
-        or event.sender_id == OWNER_ID
-        or await is_admin(event.chat_id, event.sender_id)
-    ):
-        return sql.update_flood(event.chat_id, None)
-    should_ban = sql.update_flood(event.chat_id, event.sender_id)
-    if not should_ban:
-        return
-    await event.delete()
-    mode, getvalue = sql.get_flood_setting(event.chat_id)
-    text = f"Yeah, I don't like yout flooding.\n**{event.sender.first_name}** has been "
-    if mode == 1:
-        text += "banned."
-        await event.respond(text)
-        await tbot.edit_permissions(event.chat_id, event.sender_id, view_messages=False)
-    elif mode == 2:
-        text += "Kicked."
-        await event.respond(text)
-        await tbot.kick_participant(event.chat_id, event.sender_id)
-    elif mode == 3:
-        text += "muted."
-        await event.respond(text)
-        await tbot.edit_permissions(event.chat_id, event.sender_id, send_messages=False)
-    elif mode == 4:
-        if int(getvalue) < 3600:
-            value = int(getvalue) // 60
-            iid = "{} Minutes".format(value)
-        elif int(getvalue) > 3600 < 86400:
-            value = int(getvalue) // (60 * 60)
-            iid = "{} Hours".format(value)
-        elif int(getvalue) > 86400:
-            value = int(getvalue) // (60 * 60 * 24)
-            iid = "{} Days".format(value)
-        text += f"Banned for {iid}."
-        await event.respond(text)
-        await tbot.edit_permissions(
-            event.chat_id,
-            event.sender_id,
-            until_date=time.time() + int(getvalue),
-            view_messages=False,
-        )
-    elif mode == 5:
-        if int(getvalue) < 3600:
-            value = int(getvalue) // 60
-            iid = "{} Minutes".format(value)
-        elif int(getvalue) > 3600 < 86400:
-            value = int(getvalue) // (60 * 60)
-            iid = "{} Hours".format(value)
-        elif int(getvalue) > 86400:
-            value = int(getvalue) // (60 * 60 * 24)
-            iid = "{} Days".format(value)
-        text += f"Muted for {iid}."
-        await event.respond(text)
-        await tbot.edit_permissions(
-            event.chat_id,
-            event.sender_id,
-            until_date=time.time() + int(getvalue),
-            send_messages=False,
-        )
-"""
-
 
 @Cbot(pattern="^/flood")
 async def _(event):
     if event.is_private:
         return  # connect
-    limit = sql.get_flood_limit(event.chat_id)
+    limit = db.get_flood_limit(event.chat_id)
     if limit == 0:
         text = "This chat is not currently enforcing flood control."
     else:
@@ -189,7 +121,7 @@ async def _(fx):
             return
         if not fx.from_id:
             return
-        d = update_flood(fx.chat_id, fx.sender_id)
+        d = db.update_flood(fx.chat_id, fx.sender_id)
         if d:
             text = f"Yeah, I don't like yout flooding.\n**{fx.sender.first_name}** has been banned!"
             await fx.respond(text)
