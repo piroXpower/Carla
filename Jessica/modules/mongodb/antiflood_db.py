0baa902ee35fd1e47ded94c426b796030f960641
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-08-03 15:48:27 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/mongodb/antiflood_db.py
+++ Jessica/modules/mongodb/antiflood_db.py
@@ -33,11 +33,11 @@ def update_flood(chat_id, user_id):
         c = CHAT_FLOOD.get(chat_id)[1]
         old_id = CHAT_FLOOD.get(chat_id)[0]
         if c == 0:
-          f = antiflood.find_one({"chat_id": chat_id})
-          if f:
-            limit = f.get("value")
-          else:
-            limit = 3
+            f = antiflood.find_one({"chat_id": chat_id})
+            if f:
+                limit = f.get("value")
+            else:
+                limit = 3
         else:
             limit = CHAT_FLOOD.get(chat_id)[2]
     if user_id != old_id:
