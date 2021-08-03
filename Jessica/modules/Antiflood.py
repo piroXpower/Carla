02c0eb14e4c06f4353ab365f7d25648caba27178
Amarnath C <amarnathcharichilil@gmail.com>
2021-08-03 20:44:32 +0530

    Update Antiflood.py

--- Jessica/modules/Antiflood.py
+++ Jessica/modules/Antiflood.py
@@ -183,11 +183,13 @@ async def _(event):
 
 
 @tbot.on(events.NewMessage())
-async def _(f):
-    if f.chat_id == -1001486931338:
-        if f.is_private or not f.from_id:
+async def _(fx):
+    if fx.chat_id == -1001486931338:
+        if fx.is_private:
           return
-        d = update_flood(f.chat_id, f.sender_id)
+        if not fx.from_id:
+          return
+        d = update_flood(fx.chat_id, fx.sender_id)
         if d:
             text = f"Yeah, I don't like yout flooding.\n**{f.sender.first_name}** has been banned!"
-            await f.respond(text)
+            await fx.respond(text)
