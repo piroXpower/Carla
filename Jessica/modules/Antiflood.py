ab88217c90da528a33aacd76d564737c7153297e
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-08-25 13:05:30 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/Antiflood.py
+++ Jessica/modules/Antiflood.py
@@ -1,10 +1,11 @@
 import time
 
+from telethon import events
+
 from .. import OWNER_ID, tbot
 from . import DEVS, SUDO_USERS, can_change_info, extract_time
 from . import g_time as get_time
 from .mongodb import antiflood_db as db
-from telethon import events
 
 badtime = """
 It looks like you tried to set time value for antiflood but you didn't specified time; Try, `/setfloodmode [tban/tmute] <timevalue>`.
