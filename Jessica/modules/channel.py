b0915dbdf3666a06997c20d50abcd720d5f2f4bd
Amarnath C <41010786@sfcollege.edu>
2021-08-26 20:05:26 +0530

    Create channel.py

---
+++ Jessica/modules/channel.py
@@ -0,0 +1,7 @@
+from ..events import Cbot
+
+@Cbot(pattern="^/setfooter ?(.*)")
+async def set_footer_channel____(e):
+ if not e.is_channel or e.is_group:
+   return await e.reply("This command is made to be used in channels, not in pm!")
+ print('x')
