2e38688b9b61ffd01cfb95b41f8048ab8b60504c
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-08-26 14:36:02 +0000

    RoseLoverX: auto fixes

--- Jessica/modules/channel.py
+++ Jessica/modules/channel.py
@@ -1,7 +1,8 @@
 from ..events import Cbot
 
+
 @Cbot(pattern="^/setfooter ?(.*)")
 async def set_footer_channel____(e):
- if not e.is_channel or e.is_group:
-   return await e.reply("This command is made to be used in channels, not in pm!")
- print('x')
+    if not e.is_channel or e.is_group:
+        return await e.reply("This command is made to be used in channels, not in pm!")
+    print("x")
