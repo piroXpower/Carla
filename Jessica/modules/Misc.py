19e841d8b44b1482cc2f1831c2158a0844ecf1d7
Amarnath C <41010786@sfcollege.edu>
2021-08-29 10:46:52 +0530

    Update Misc.py

--- Jessica/modules/Misc.py
+++ Jessica/modules/Misc.py
@@ -823,7 +823,7 @@ async def paste(e):
     space_bin = "https://spaceb.in/api/v1/documents"
     pasty_bin = "https://pasty.lus.pm/api/v1/pastes"
     if sp_bin == "h":
-        r = post(haste_bin, data=paste_text)
+        r = post(haste_bin, data=paste_text, timeout=2)
         if r.ok and r.status_code == 200:
             try:
                 key = r.json()["key"]
