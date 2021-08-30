bbe4c5445158b92dd2f0997a8a0917770f2c173a
Amarnath C <41010786@sfcollege.edu>
2021-08-30 23:30:25 +0530

    Update events.py

--- Jessica/events.py
+++ Jessica/events.py
@@ -3,7 +3,7 @@ import logging
 import sys
 from pathlib import Path
 
-from frompyrate_limiter import BucketFullException
+from from pyrate_limiter import BucketFullException
 from telethon import events
 
 from . import Limit, tbot
