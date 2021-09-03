17bb285361c8fb4e537421f5be528879a1b5f5b6
Amarnath C <41010786@sfcollege.edu>
2021-09-03 21:42:56 +0530

    Delete Jessica directory

--- Jessica/modules/channel.py
+++
@@ -1,8 +0,0 @@
-from ..utils import Cbot
-
-
-@Cbot(pattern="^/setfooter ?(.*)")
-async def set_footer_channel____(e):
-    if not e.is_channel or e.is_group:
-        return await e.reply("This command is made to be used in channels, not in pm!")
-    print("x")
