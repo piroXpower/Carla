0317c4ce90f5051be5e57952037392301b9a0065
Amarnath C <41010786@sfcollege.edu>
2021-09-03 20:16:51 +0530

    Update __init__.py

--- Jessica/__init__.py
+++ Jessica/__init__.py
@@ -1,5 +1,5 @@
 import logging
-import os
+from os import environ as e
 import sys
 import time
 from logging import INFO, basicConfig, getLogger
@@ -23,27 +23,24 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 LOGGER = logging.getLogger(__name__)
-MAINTENANCE = os.environ.get("MAINTENANCE", False)
-TOKEN = os.environ.get("TOKEN", None)
-OWNER_ID = int(os.environ.get("OWNER_ID", 1221693726))
-GBAN_LOGS = os.environ.get("GBAN_LOGS", -100)
-OWNER_USERNAME = os.environ.get("OWNER_USERNAME", None)
-API_KEY = os.environ.get("API_KEY", None)
-API_HASH = os.environ.get("API_HASH", None)
-DB_URI = os.environ.get("DATABASE_URL", None)
-WOLFRAM_ID = os.environ.get("WOLFRAM_ID", None)
+MAINTENANCE = e.get("MAINTENANCE", False)
+TOKEN = e.get("TOKEN", None)
+OWNER_ID = int(e.get("OWNER_ID", 1221693726))
+GBAN_LOGS = e.get("GBAN_LOGS", -100)
+OWNER_USERNAME = e.get("OWNER_USERNAME", None)
+API_KEY = e.get("API_KEY", None)
+API_HASH = e.get("API_HASH", None)
+DB_URI = e.get("DATABASE_URL", None)
+WOLFRAM_ID = e.get("WOLFRAM_ID", None)
 tbot = TelegramClient(None, API_KEY, API_HASH)
-STRING_SESSION = os.environ.get("STRING_SESSION", None)
+STRING_SESSION = e.get("STRING_SESSION", None)
 MONGO_DB_URI = "mongodb://neko:neko1234@iad2-c7-2.mongo.objectrocket.com:52584,iad2-c7-0.mongo.objectrocket.com:52584,iad2-c7-1.mongo.objectrocket.com:52584/neko?replicaSet=25a8afdada8f49d39f2c94edadce9dca"
-HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
-HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME", None)
-UPSTREAM_REPO_URL = os.environ.get(
+HEROKU_API_KEY = e.get("HEROKU_API_KEY", None)
+HEROKU_APP_NAME = e.get("HEROKU_APP_NAME", None)
+UPSTREAM_REPO_URL = e.get(
     "UPSTREAM_REPO_URL", "https://github.com/amarnathcjd/cerina"
 )
-BOT_ID = int(os.environ.get("BOT_ID"))
-if MAINTENANCE == "True":
-    print("Maintenance Mode Active.")
-    sys.exit(0)
+BOT_ID = int(e.get("BOT_ID"))
 ubot = None
 if STRING_SESSION:
     ubot = TelegramClient(StringSession(STRING_SESSION), API_KEY, API_HASH)
@@ -52,10 +49,9 @@ else:
 try:
     ubot.start()
 except BaseException:
-    print("Invalid STRING SESSION!")
+    print("[INFO]: Failed to start userbot client.")
 
 spam = {}
-
 
 def spam_check(user_id):
     x = spam.get(user_id)
