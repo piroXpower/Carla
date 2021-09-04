4b6baf96e59a33dbccf4894b05a0962d58bad9ce
Amarnath C <41010786@sfcollege.edu>
2021-09-04 22:53:53 +0530

    Update test.py

--- neko/modules/test.py
+++ neko/modules/test.py
@@ -30,6 +30,7 @@ async def play_video(e):
         vid = yts.get("formats")[9].get("url")
         if not aud:
             return await e.reply("No Search Result Found for Your Query.")
+        await e.reply("Playing {}".format(yts.get("formats")[0].get("title")))
         if not db.get(e.chat_id):
             call = GroupCallFactory(
                 ubot, GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON
