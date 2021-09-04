806d1da4c9e8efaf97ad22695bfb1ffd808f81e4
Amarnath C <41010786@sfcollege.edu>
2021-09-04 22:40:42 +0530

    Update test.py

--- neko/modules/test.py
+++ neko/modules/test.py
@@ -13,7 +13,7 @@ async def play_video(e):
    return await e.reply("No Query.")
  with youtube_dl.YoutubeDL(ops) as yt:
    yts = yt.extract_info(q, download=False)
- url = yts.get('formats')[0].get('url'):
+ url = yts.get('formats')[0].get('url')
  if not url:
    return await e.reply("No Search Result Found for Your Query.")
  call = GroupCallFactory (ubot, GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON).get_group_call()
