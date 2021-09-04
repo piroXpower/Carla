192498a0256d3e5c234af8724607b64fff8aac08
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-09-04 17:19:44 +0000

    RoseLoverX: auto fixes

--- neko/modules/test.py
+++ neko/modules/test.py
@@ -16,29 +16,29 @@ db = {}
 
 @Cbot(pattern="^/playvideo ?(.*)")
 async def play_video(e):
- try:
-    global p
-    if not e.sender_id in p:
-        return
     try:
-        q = e.text.split(None, 1)[1]
-    except IndexError:
-        return await e.reply("No Query.")
-    with youtube_dl.YoutubeDL(ops) as yt:
-        yts = yt.extract_info(q, download=False)
-    aud = yts.get("formats")[0].get("url")
-    vid = yts.get("formats")[6].get("url")
-    if not url:
-        return await e.reply("No Search Result Found for Your Query.")
-    if not db.get(e.chat_id):
-        call = GroupCallFactory(
-            ubot, GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON
-        ).get_group_call()
-    else:
-        call = db.get(e.chat_id)
-    await call.join(e.chat_id)
-    await call.start_video(vid, repeat=False, with_audio=False)
-    await call.start_audio(aud, repeat=False)
-    db[e.chat_id] = call
- except Exception as ep:
-    await e.reply(str(ep))
+        global p
+        if not e.sender_id in p:
+            return
+        try:
+            q = e.text.split(None, 1)[1]
+        except IndexError:
+            return await e.reply("No Query.")
+        with youtube_dl.YoutubeDL(ops) as yt:
+            yts = yt.extract_info(q, download=False)
+        aud = yts.get("formats")[0].get("url")
+        vid = yts.get("formats")[6].get("url")
+        if not url:
+            return await e.reply("No Search Result Found for Your Query.")
+        if not db.get(e.chat_id):
+            call = GroupCallFactory(
+                ubot, GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON
+            ).get_group_call()
+        else:
+            call = db.get(e.chat_id)
+        await call.join(e.chat_id)
+        await call.start_video(vid, repeat=False, with_audio=False)
+        await call.start_audio(aud, repeat=False)
+        db[e.chat_id] = call
+    except Exception as ep:
+        await e.reply(str(ep))
