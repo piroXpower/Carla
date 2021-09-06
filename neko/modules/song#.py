2301a699db876117b80b9ff78a9b3a93d96496cf
Amarnath C <amarnathcharichilil@gmail.com>
2021-09-06 21:52:39 +0530

    Update song#.py

--- neko/modules/song#.py
+++ neko/modules/song#.py
@@ -2,45 +2,42 @@ import os
 
 from telethon.tl.types import DocumentAttributeAudio
 from youtubesearchpython import VideosSearch
-
+import yt_dlp
 from ..utils import Cbot
 from . import runcmd
 
-
+opts = {
+            'format':
+           'bestaudio',
+            'addmetadata':
+            True,
+            'key':
+            'FFmpegMetadata',
+            'writethumbnail':
+            True,
+            'prefer_ffmpeg':
+            True,
+            'geo_bypass':
+            True,
+            'nocheckcertificate':
+            True,
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '320',
+            }],
+            'outtmpl':
+            '%(id)s.mp3',
+            'quiet':
+            True,
+            'logtostderr':
+            False
+        }
 @Cbot(pattern="^/song ?(.*)")
-async def song(event):
-    return
-    try:
-        q = event.pattern_match.group(1)
-        if not q:
-            return await event.reply("Please provide the name of the song!")
-        st_r = await event.reply("`Processing...`")
-        search = VideosSearch(q, limit=1)
-        if not search:
-            return await event.reply(f"Song Not Found With Name {q}.")
-        r = (search.result())["result"]
-        url = r[0]["url"]
-        x_u = await st_r.edit(f"`Preparing to upload song:` **{str(r[0]['title'])}**")
-        await runcmd(
-            'yt-dlp {} -x --audio-quality 0 -o "~/YouTube/%(id)s.%(ext)s" -q --default-search ytsearch --no-cache-dir --geo-bypass -title'.format(
-                url
-            )
-        )
-        du_s = (str(r[0]["duration"])).split(":", 1)
-        du = (int(du_s[0]) * 60) + int(du_s[1])
-        fil_e = f'/root/YouTube/{r[0]["id"]}.opus'
-        await event.respond(
-            file=fil_e,
-            attributes=[
-                DocumentAttributeAudio(
-                    duration=int(du),
-                    title=str(r[0]["title"]),
-                    performer="YouTube",
-                    waveform="320",
-                )
-            ],
-        )
-        await x_u.delete()
-        os.remove(fil_e)
-    except Exception:
-        await event.reply(str(e))
+async def song(e):
+ try:
+  q = e.text.split(None, 1)[1]
+ except IndexError:
+  return await e.reply("The song query was not provided!")
+ with yt_dlp.YoutubeDL(ops) as yt:
+  print(yt)
