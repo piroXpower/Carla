af20b5a7af46551ef6734611ae61a16e52d83a22
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-09-06 16:23:09 +0000

    RoseLoverX: auto fixes

--- neko/modules/song#.py
+++ neko/modules/song#.py
@@ -1,43 +1,33 @@
-import os
-
-from telethon.tl.types import DocumentAttributeAudio
-from youtubesearchpython import VideosSearch
 import yt_dlp
+
 from ..utils import Cbot
-from . import runcmd
 
 opts = {
-            'format':
-           'bestaudio',
-            'addmetadata':
-            True,
-            'key':
-            'FFmpegMetadata',
-            'writethumbnail':
-            True,
-            'prefer_ffmpeg':
-            True,
-            'geo_bypass':
-            True,
-            'nocheckcertificate':
-            True,
-            'postprocessors': [{
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '320',
-            }],
-            'outtmpl':
-            '%(id)s.mp3',
-            'quiet':
-            True,
-            'logtostderr':
-            False
+    "format": "bestaudio",
+    "addmetadata": True,
+    "key": "FFmpegMetadata",
+    "writethumbnail": True,
+    "prefer_ffmpeg": True,
+    "geo_bypass": True,
+    "nocheckcertificate": True,
+    "postprocessors": [
+        {
+            "key": "FFmpegExtractAudio",
+            "preferredcodec": "mp3",
+            "preferredquality": "320",
         }
+    ],
+    "outtmpl": "%(id)s.mp3",
+    "quiet": True,
+    "logtostderr": False,
+}
+
+
 @Cbot(pattern="^/song ?(.*)")
 async def song(e):
- try:
-  q = e.text.split(None, 1)[1]
- except IndexError:
-  return await e.reply("The song query was not provided!")
- with yt_dlp.YoutubeDL(ops) as yt:
-  print(yt)
+    try:
+        e.text.split(None, 1)[1]
+    except IndexError:
+        return await e.reply("The song query was not provided!")
+    with yt_dlp.YoutubeDL(ops) as yt:
+        print(yt)
