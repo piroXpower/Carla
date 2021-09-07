e24c42a6a6f7b05929717e78b7eaf55071b81391
Amarnath C <amarnathcharichilil@gmail.com>
2021-09-07 10:34:02 +0530

    Update song#.py

--- neko/modules/song#.py
+++ neko/modules/song#.py
@@ -1,7 +1,8 @@
 import yt_dlp
-
+import os
+from telethon.tl.types import DocumentAttributeAudio
 from ..utils import Cbot
-
+from youtubesearchpython import VideosSearch as vs
 opts = {
     "format": "bestaudio",
     "addmetadata": True,
@@ -26,8 +27,21 @@ opts = {
 @Cbot(pattern="^/song ?(.*)")
 async def song(e):
     try:
-        e.text.split(None, 1)[1]
+        q = e.text.split(None, 1)[1]
     except IndexError:
         return await e.reply("The song query was not provided!")
+    try:
+      v = vs(q, limit=1).result()["result"][0]
+    except (IndexError, KeyError, TypeError):
+      return await e.reply("No song result found for your query!")
+    axe = await e.reply("Preparing to upload **{}** by {}".format(v.get("title"), v.get("channel").get("name") or "Channel"))
+    duration = int(v["duration"].split(":")[0]) * 60 + int(v["duration"].split(":")[1])
     with yt_dlp.YoutubeDL(ops) as yt:
-        print(yt)
+      try:
+        yt.extract_info(v["link"])
+      except Exception as bx:
+        return await axe.edit(str(bx))
+    await e.client.send_file(e.chat_id, v["id"] + ".mp3",
+        supports_streaming=False, force_document=False, allow_cache=False, 
+        attributes=[DocumentAttributeAudio(duration=duration, title=v["title"], performer=v["channel"]["name"])])
+    os.remove(v["id"] + ".mp3")
