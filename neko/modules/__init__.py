fadfb6180e0caef731f1cecaa24f55c616764857
Amarnath C <amarnathcharichilil@gmail.com>
2021-09-07 12:16:36 +0530

    Update __init__.py

--- neko/modules/__init__.py
+++ neko/modules/__init__.py
@@ -272,7 +272,12 @@ async def is_admin(chat_id, user_id):
 
 
 async def get_user(event):
-    args = event.pattern_match.group(1).split(" ", 1)
+    try:
+     args = event.text.split(" ", 1)[1].split(" ")
+    except IndexError:
+     return await event.reply(
+                "I don't know who you're talking about, you're going to need to specify a user...!"
+            )
     if event.reply_to_msg_id:
         previous_message = await event.get_reply_message()
         user_obj = await tbot.get_entity(previous_message.sender_id)
