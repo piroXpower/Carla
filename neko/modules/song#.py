ca00a2eda8bb745ac61ac7b3a5f5538561358e99
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-09-07 05:04:27 +0000

    RoseLoverX: auto fixes

--- neko/modules/song#.py
+++ neko/modules/song#.py
@@ -1,8 +1,11 @@
-import yt_dlp
 import os
+
+import yt_dlp
 from telethon.tl.types import DocumentAttributeAudio
-from ..utils import Cbot
 from youtubesearchpython import VideosSearch as vs
+
+from ..utils import Cbot
+
 opts = {
     "format": "bestaudio",
     "addmetadata": True,
@@ -31,17 +34,30 @@ async def song(e):
     except IndexError:
         return await e.reply("The song query was not provided!")
     try:
-      v = vs(q, limit=1).result()["result"][0]
+        v = vs(q, limit=1).result()["result"][0]
     except (IndexError, KeyError, TypeError):
-      return await e.reply("No song result found for your query!")
-    axe = await e.reply("Preparing to upload **{}** by {}".format(v.get("title"), v.get("channel").get("name") or "Channel"))
+        return await e.reply("No song result found for your query!")
+    axe = await e.reply(
+        "Preparing to upload **{}** by {}".format(
+            v.get("title"), v.get("channel").get("name") or "Channel"
+        )
+    )
     duration = int(v["duration"].split(":")[0]) * 60 + int(v["duration"].split(":")[1])
     with yt_dlp.YoutubeDL(ops) as yt:
-      try:
-        yt.extract_info(v["link"])
-      except Exception as bx:
-        return await axe.edit(str(bx))
-    await e.client.send_file(e.chat_id, v["id"] + ".mp3",
-        supports_streaming=False, force_document=False, allow_cache=False, 
-        attributes=[DocumentAttributeAudio(duration=duration, title=v["title"], performer=v["channel"]["name"])])
+        try:
+            yt.extract_info(v["link"])
+        except Exception as bx:
+            return await axe.edit(str(bx))
+    await e.client.send_file(
+        e.chat_id,
+        v["id"] + ".mp3",
+        supports_streaming=False,
+        force_document=False,
+        allow_cache=False,
+        attributes=[
+            DocumentAttributeAudio(
+                duration=duration, title=v["title"], performer=v["channel"]["name"]
+            )
+        ],
+    )
     os.remove(v["id"] + ".mp3")
