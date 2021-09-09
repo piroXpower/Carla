bf631ffe6c10c99f8ba5ae2d91f454c7e9e4bee3
Amarnath C <amarnathcharichilil@gmail.com>
2021-09-09 12:37:35 +0530

    Update quotly.py

--- neko/modules/quotly.py
+++ neko/modules/quotly.py
@@ -34,129 +34,128 @@ colors = [
 
 @Cbot(pattern="^/q ?(.*)")
 async def qoutly_api(e):
+ try:
+    if not e.reply_to:
+        return await e.reply("Command must be sent as a reply to a message.")
+    msg = await e.get_reply_message()
+    color = "#1b1429"
     try:
-        if not e.reply_to:
-            return await e.reply("Command must be sent as a reply to a message.")
-        msg = await e.get_reply_message()
-        color = "#1b1429"
-        try:
-            d = e.text.split(" ", 1)[1]
-        except IndexError:
-            d = ""
-        for c in colors:
-            if c in d:
-                q_without_color = (d).replace(c, "")
-                color = c
-        num = [int(x) for x in d.split() if x.isdigit()]
-        if not num:
-            if "r" in q_without_color and msg.reply_to:
-                reply = await msg.get_reply_message()
-                if isinstance(reply.sender, Channel):
-                    reply_msg = {
-                        "chatId": e.chat_id,
-                        "first_name": reply.chat.title,
-                        "last_name": "",
-                        "username": reply.chat.username,
-                        "text": reply.text,
-                        "name": reply.chat.title,
-                    }
-                elif reply.sender:
-                    name = reply.sender.first_name
-                    name = (
-                        name + " " + reply.sender.last_name
-                        if reply.sender.last_name
-                        else name
-                    )
-                    reply_msg = {
-                        "chatId": e.chat_id,
-                        "first_name": reply.sender.first_name,
-                        "last_name": "reply.sender.last_name",
-                        "username": reply.sender.username,
-                        "text": reply.text,
-                        "name": name,
-                    }
-                else:
-                    reply_msg = {}
+        d = e.text.split(" ", 1)[1]
+    except IndexError:
+        d = ""
+    q_without_color= d
+    for c in colors:
+        if c in d:
+            q_without_color = (d).replace(c, "")
+            color = c
+    num = [int(x) for x in d.split() if x.isdigit()]
+    if not num:
+        if "r" in q_without_color and msg.reply_to:
+            reply = await msg.get_reply_message()
+            if isinstance(reply.sender, Channel):
+                reply_msg = {
+                    "chatId": e.chat_id,
+                    "first_name": reply.chat.title,
+                    "last_name": "",
+                    "username": reply.chat.username,
+                    "text": reply.text,
+                    "name": reply.chat.title,
+                }
+            elif reply.sender:
+                name = reply.sender.first_name
+                name = (
+                    name + " " + reply.sender.last_name
+                    if reply.sender.last_name
+                    else name
+                )
+                reply_msg = {
+                    "chatId": e.chat_id,
+                    "first_name": reply.sender.first_name,
+                    "last_name": "reply.sender.last_name",
+                    "username": reply.sender.username,
+                    "text": reply.text,
+                    "name": name,
+                }
             else:
                 reply_msg = {}
-            if isinstance(msg.sender, User):
-                _name = msg.sender.first_name
-                _name = (
-                    _name + " " + msg.sender.last_name
-                    if msg.sender.last_name
-                    else _name
-                )
-                if msg.fwd_from and msg.fwd_from.from_name:
-                    _name = msg.fwd_from.from_name
-                _first = msg.sender.first_name
-                _last = msg.sender.last_name
-                _username = msg.sender.username
-                _id = msg.sender_id
-                _title = "Admin"
-            elif isinstance(msg.sender, Channel):
-                _name = msg.chat.title
-                _first = _last = msg.chat.title
-                _username = msg.chat.username
-                _id = msg.chat_id
-                _title = "Channel"
-            else:
-                _name = "Anonymous Admin"
-                _first = _last = "Anon"
-                _username = "GroupAnonymousBot"
-                _id = 1087968824
-                _title = "Gey"
-            if msg.sticker:
-                media = [
-                    {
-                        "file_id": msg.file.id,
-                        "file_size": msg.file.size,
-                        "height": msg.file.height,
-                        "width": msg.file.width,
-                    }
-                ]
-            elif msg.photo:
-                media = [
-                    {
-                        "file_id": msg.file.id,
-                        "file_size": msg.file.size,
-                        "height": msg.file.height,
-                        "width": msg.file.width,
-                    }
-                ]
-            if msg.text:
-                _text = msg.text
-            else:
-                _text = ""
-            data = {
-                "type": "quote",
-                "backgroundColor": color,
-                "width": 512,
-                "height": 768,
-                "scale": 2,
-                "messages": [
-                    {
-                        "entities": [],
-                        "chatId": e.chat_id,
-                        "avatar": True,
-                        "from": {
-                            "id": _id,
-                            "first_name": _first,
-                            "last_name": _last,
-                            "username": _username,
-                            "language_code": "en",
-                            "title": _title,
-                            "photo": {},
-                            "type": "group",
-                            "name": _name,
-                        },
-                        "text": _text,
-                        "replyMessage": reply_msg,
-                    }
-                ],
-            }
-        await e.reply(str(data))
-    except Exception as r:
-        await e.reply(str(type(r)) + str(r))
+        else:
+            reply_msg = {}
+        if isinstance(msg.sender, User):
+            _name = msg.sender.first_name
+            _name = (
+                _name + " " + msg.sender.last_name if msg.sender.last_name else _name
+            )
+            if msg.fwd_from and msg.fwd_from.from_name:
+                _name = msg.fwd_from.from_name
+            _first = msg.sender.first_name
+            _last = msg.sender.last_name
+            _username = msg.sender.username
+            _id = msg.sender_id
+            _title = "Admin"
+        elif isinstance(msg.sender, Channel):
+            _name = msg.chat.title
+            _first = _last = msg.chat.title
+            _username = msg.chat.username
+            _id = msg.chat_id
+            _title = "Channel"
+        else:
+            _name = "Anonymous Admin"
+            _first = _last = "Anon"
+            _username = "GroupAnonymousBot"
+            _id = 1087968824
+            _title = "Gey"
+        if msg.sticker:
+            media = [
+                {
+                    "file_id": msg.file.id,
+                    "file_size": msg.file.size,
+                    "height": msg.file.height,
+                    "width": msg.file.width,
+                }
+            ]
+        elif msg.photo:
+            media = [
+                {
+                    "file_id": msg.file.id,
+                    "file_size": msg.file.size,
+                    "height": msg.file.height,
+                    "width": msg.file.width,
+                }
+            ]
+        if msg.text:
+            _text = msg.text
+        else:
+            _text = ""
+        data = {
+            "type": "quote",
+            "backgroundColor": color,
+            "width": 512,
+            "height": 768,
+            "scale": 2,
+            "messages": [
+                {
+                    "entities": [],
+                    "chatId": e.chat_id,
+                    "avatar": True,
+                    "from": {
+                        "id": _id,
+                        "first_name": _first,
+                        "last_name": _last,
+                        "username": _username,
+                        "language_code": "en",
+                        "title": _title,
+                        "photo": {},
+                        "type": "group",
+                        "name": _name,
+                    },
+                    "text": _text,
+                    "replyMessage": reply_msg,
+                }
+            ],
+        }
+    await e.reply(str(data))
+ except Exception as r:
+    await e.reply(str(type(r)) + str(r))
 
 
 """
