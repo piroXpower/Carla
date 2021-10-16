022fc30b981a249631c8e5aae7f7042838a9929b
AmarnathCjd <72609355+AmarnathCjd+@users.noreply.github.com>
2021-10-16 15:25:38 +0000

    RoseLoverX: auto fixes

--- neko/__init__.py
+++ neko/__init__.py
@@ -1,9 +1,9 @@
 import logging
+import os
 import sys
 import time
 from logging import INFO, basicConfig, getLogger
 from os import environ as e
-import os
 
 from telethon import TelegramClient
 from telethon.sessions import StringSession
@@ -33,7 +33,10 @@ API_HASH = os.environ.get("API_HASH", "9ff85074c961b349e6dad943e9b20f54")
 DB_URI = "postgres://sqlgzrfkyljzma:6b1a2997b31e0120390f4ff1c208753ed8003f86402ca2bbf27cd256306e533f@ec2-52-19-170-215.eu-west-1.compute.amazonaws.com:5432/d6n5ris8lr007u"
 tbot = TelegramClient(None, API_KEY, API_HASH)
 STRING_SESSION = "1BVtsOMQBu7EC7pxQ-iHfjwUFpjG4YIUuZ2QqzexjefOKGwS8RSUsyrsU_tR6TMxnAbc6ZAnRkq2KghEYNb0YtQ-v3I5ijU03M-BVYy_w1SslF-5lH2XbF7sSRl5TWa2Jar256brsZvZZNnaEbDTBenTIFBt87hUdLBV5H-O0EeSd96b0a76ULgc91QsRJKZO76GewtsuEP8OrAxKkaP7hPrTAYtXYl7hJTRJ1aKGoWNt5iRr3ViaoeLuEW5PfNutiswKllgub_bDGw9grMLsPHtIaGEeCQrlwXKodWRbTZzhb3ycpabFDzX3bI1DXnGBQ7wI-LDqs37eMHQKDzKgm9fz_L-HFNw="
-MONGO_DB_URI = os.environ.get("MONGO_DB_URI", "mongodb+srv://Simpmodz:Simpmodz@rexmodz.d2sxt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+MONGO_DB_URI = os.environ.get(
+    "MONGO_DB_URI",
+    "mongodb+srv://Simpmodz:Simpmodz@rexmodz.d2sxt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+)
 BOT_ID = 1839462992
 ubot = None
 if STRING_SESSION:
