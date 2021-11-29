45d7a019ec2e29fa37d54627baf910ebc61ad5b3
Amarnath C <41010786@sfcollege.edu>
2021-11-29 15:50:45 +0530

    Update Inline.py

--- neko/modules/Inline.py
+++ neko/modules/Inline.py
@@ -10,7 +10,7 @@ from requests import get
 from researchacademic import ResearchAcademic
 from telethon import Button, events
 from telethon.tl.types import InputWebDocument
-from tpblite import TPB
+# from tpblite import TPB not working idk why
 from youtubesearchpython import SearchVideos
 
 from .. import tbot
